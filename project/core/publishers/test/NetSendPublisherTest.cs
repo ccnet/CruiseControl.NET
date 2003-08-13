efc57ccfe0cd65de032665ca0ce4641ededd3b96
Mike Roberts <mike.b.roberts@gmail.com>
2003-08-13 02:43:44 +0000

    Needed to comment out this namespace for compiling in VS 2003 - don't know why...

--- project/core/publishers/test/NetSendPublisherTest.cs
+++ project/core/publishers/test/NetSendPublisherTest.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Text;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
 using NMock;
