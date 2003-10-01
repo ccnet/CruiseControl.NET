a8211c09cfd6549d19fe60ce12646413ce2a2139
Drew Noakes <web@drewnoakes.com>
2003-10-01 09:38:33 +0000

    Added some documentation to try and force a build.  I've previously added a file, which I thought should trigger the build process, but nothing seems to have happened, and I don't have access to the box.

--- project/core/ITask.cs
+++ project/core/ITask.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace tw.ccnet.core
 {
+	/// <summary>
+	/// Defines a task that may be run.
+	/// </summary>
 	public interface ITask
 	{
+		/// <summary>
+		/// Runs the task, given the specified <see cref="IntegrationResult"/>.
+		/// </summary>
+		/// <param name="result"></param>
 		void Run(IntegrationResult result);
 	}
 }
