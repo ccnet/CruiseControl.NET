ec00d086a667fe14d9fa7fe90f8a9ddd4df7fcf3
Drew Noakes <web@drewnoakes.com>
2003-10-01 09:17:26 +0000

    Missed this file from previous checkin!

---
+++ project/ccnet.remote/BuildCondition.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace tw.ccnet.remote
+{
+	/// <summary>
+	/// Enumeration containing possible conditions for the execution of the
+	/// build phase of project integration.
+	/// </summary>
+	public enum BuildCondition
+	{
+		/// <summary>
+		/// A build should not occur.
+		/// </summary>
+		NoBuild,
+
+		/// <summary>
+		/// A build should occur only if modifications exist.
+		/// </summary>
+		IfModificationExists,
+
+		/// <summary>
+		/// A build should be forced, regardless of whether
+		/// modifications exist or not.
+		/// </summary>
+		ForceBuild
+	}
+}
