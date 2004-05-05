2d4b0c0ea42a3ee7dbdd6dada0ff1d0f8f26e246
Garrett M. Smith <gsmith@thoughtworks.com>
2004-05-05 01:39:32 +0000

    Added comment about VSS labelling issue

--- project/core/label/ILabeller.cs
+++ project/core/label/ILabeller.cs
@@ -4,6 +4,15 @@ namespace ThoughtWorks.CruiseControl.Core
 {
 	public interface ILabeller : ITask
 	{
+		/// <summary>
+		/// Returns the label to apply.
+		/// </summary>
+		/// <remarks>
+		/// There is a known issue with implementations for VSS that return labels that are the same
+		/// as a previous label.  See http://confluence.public.thoughtworks.org/display/CCNET/VSS.
+		/// </remarks>
+		/// <param name="previousLabel">state information for determining the label</param>
+		/// <returns>the label to apply</returns>
 		string Generate(IntegrationResult previousLabel);
 	}
 }
