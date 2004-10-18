6ffafc925f692c28ea705957556772f74e37c6c7
Owen Rogers <exortech@gmail.com>
2004-10-18 13:30:04 +0000

    forgot to add these tests

---
+++ project/UnitTests/Xsl/HeaderStylesheetTest.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.UnitTests.Xsl
+{
+	[TestFixture]
+	public class HeaderStylesheetTest : StylesheetTestFixture
+	{
+		protected override string Stylesheet
+		{
+			get { return @"xsl\header.xsl"; }
+		}
+
+		[Test]
+		public void ShouldOutputBuildConditionForForcedBuilds()
+		{
+			string input = @"<cruisecontrol><build buildcondition=""ForceBuild""/></cruisecontrol>";
+
+			string actualXml = LoadStylesheetAndTransformInput(input);
+			CustomAssertion.AssertContains("Forced Build", actualXml);
+		}
+
+		[Test]
+		public void ShouldOutputBuildConditionForModificationsDetected()
+		{
+			string input = @"<cruisecontrol><build buildcondition=""IfModificationExists""/></cruisecontrol>";
+
+			string actualXml = LoadStylesheetAndTransformInput(input);
+			CustomAssertion.AssertContains("Modifications Detected", actualXml);
+		}
+	}
+}
---
+++ project/UnitTests/Xsl/StylesheetTestFixture.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using NUnit.Framework;
+
+namespace ThoughtWorks.CruiseControl.UnitTests.Xsl
+{
+	public abstract class StylesheetTestFixture
+	{
+		private XslTransform transform;
+
+		protected abstract string Stylesheet { get; }
+
+		protected string LoadStylesheetAndTransformInput(string input)
+		{
+			XPathDocument document = new XPathDocument(new StringReader(input));
+			XmlReader output = transform.Transform(document, null);
+
+			XmlDocument outputDocument = new XmlDocument();
+			outputDocument.Load(output);
+			return outputDocument.OuterXml;
+		}
+
+		[TestFixtureSetUp]
+		public void LoadStyleSheet()
+		{
+			transform = new XslTransform();
+			transform.Load(Stylesheet);
+		}
+	}
+}
