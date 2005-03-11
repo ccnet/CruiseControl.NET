dd3fa710e05a9302e4c8c303f27597a12f1218b3
Owen Rogers <exortech@gmail.com>
2005-03-11 14:35:39 +0000

    fixing CCNET-159 -- making NoSuchProjectException serialisable

--- project/Remote/NoSuchProjectException.cs
+++ project/Remote/NoSuchProjectException.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Serialization;
 using ThoughtWorks.CruiseControl.Core;
 
 namespace ThoughtWorks.CruiseControl.Remote
@@ -8,14 +9,28 @@ namespace ThoughtWorks.CruiseControl.Remote
 	{
 		private readonly string requestedProject;
 
+		public NoSuchProjectException() : base(ExceptionMessage(string.Empty)) {}
+
+		public NoSuchProjectException(string requestedProject) : base(ExceptionMessage(requestedProject))
+		{
+			this.requestedProject = requestedProject;
+		}
+		
+		public NoSuchProjectException(string requestedProject, Exception e) : base(ExceptionMessage(requestedProject), e)
+		{
+			this.requestedProject = requestedProject;			
+		}
+		
+		public NoSuchProjectException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
 		public string RequestedProject
 		{
 			get { return requestedProject; }
 		}
 
-		public NoSuchProjectException(string requestedProject) : base ()
+		private static string ExceptionMessage(string project)
 		{
-			this.requestedProject = requestedProject;
+			return string.Format("The project: {0} does not exist on the CCNet server.", project);
 		}
 	}
 }
