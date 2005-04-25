7cd888b1a2fac6189ed84ce6f531ca0fb3192bda
Owen Rogers <exortech@gmail.com>
2005-04-25 02:04:35 +0000

    forgot to add ProcessTaskResult

---
+++ project/core/tasks/ProcessTaskResult.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Xml;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.Core.Tasks
+{
+	public class ProcessTaskResult : ITaskResult
+	{
+		protected readonly ProcessResult result;
+
+		public ProcessTaskResult(ProcessResult result)
+		{
+			this.result = result;
+		}
+
+		public virtual string Data
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public virtual void WriteTo(XmlWriter writer)
+		{
+			writer.WriteStartElement("task");
+			if (result.Failed) writer.WriteAttributeString("failed", true.ToString());
+			if (result.TimedOut) writer.WriteAttributeString("timedout", true.ToString());
+			writer.WriteElementString("standardOutput", result.StandardOutput);
+			writer.WriteElementString("standardError", result.StandardError);
+			writer.WriteEndElement();
+		}
+
+		public bool Succeeded()
+		{
+			return ! Failed();
+		}
+
+		public bool Failed()
+		{
+			return result.Failed;
+		}
+	}
+}
