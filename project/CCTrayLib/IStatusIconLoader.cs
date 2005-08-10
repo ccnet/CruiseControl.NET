3de1f2d2b938e7daccb3e16c4828de13ebc3660e
Graham Tackley <graham@tackley.net>
2005-08-10 18:54:16 +0000

    CCNET-496: remove old cctray code

--- project/CCTrayLib/IStatusIconLoader.cs
+++
@@ -1,9 +0,0 @@
-using ThoughtWorks.CruiseControl.Remote;
-
-namespace ThoughtWorks.CruiseControl.CCTrayLib
-{
-	public interface IStatusIconLoader
-	{
-		StatusIcon LoadIcon (ProjectStatus status);
-	}
-}
