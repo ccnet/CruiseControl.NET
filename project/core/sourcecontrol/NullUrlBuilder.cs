efbd1c0ceeea4bb913cb8479976ecd162b4ee1f2
Owen Rogers <exortech@gmail.com>
2005-09-27 22:56:19 +0000

    forgot to commit nullurl builder

---
+++ project/core/sourcecontrol/NullUrlBuilder.cs
@@ -0,0 +1,8 @@
+namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
+{
+	public class NullUrlBuilder : IModificationUrlBuilder
+	{
+		public void SetupModification(Modification[] modifications)
+		{}
+	}
+}
