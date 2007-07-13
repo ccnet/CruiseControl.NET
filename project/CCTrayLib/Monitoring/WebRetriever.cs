97dc0d03781f2581b48f274cc3a067c9bacdd49e
Sidu Ponnappa <sidu@c42.in>
2007-07-13 07:40:52 +0000

    CCNET-934: Fixed by ignoring the certificate provided by the server

--- project/CCTrayLib/Monitoring/WebRetriever.cs
+++ project/CCTrayLib/Monitoring/WebRetriever.cs
@@ -4,19 +4,20 @@ using System.Net;
 
 namespace ThoughtWorks.CruiseControl.CCTrayLib.Monitoring
 {
-	public class WebRetriever : IWebRetriever
-	{
-		public string Get(Uri uri)
-		{
-			WebRequest request = WebRequest.Create(uri);
-			using (WebResponse response = request.GetResponse())
-			{
-				using (Stream responseStream = response.GetResponseStream())
-				{
-					StreamReader streamReader = new StreamReader(responseStream);
-					return streamReader.ReadToEnd();
-				}
-			}
-		}
-	}
+    public class WebRetriever : IWebRetriever
+    {
+        public string Get(Uri uri)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            WebRequest request = WebRequest.Create(uri);
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(responseStream);
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
 }
