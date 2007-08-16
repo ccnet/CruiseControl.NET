955f9b0326de796dd4ad59e695076b8dfd239d69
Ashish Kumar <ashish_kgp@users.sourceforge.net>
2007-08-16 16:18:53 +0000

    User friendly message when someone tries to view statistics when it has not been collected yet, instead of showing a stacktrace.

--- project/WebDashboard/Plugins/Statistics/ProjectStatisticsPlugin.cs
+++ project/WebDashboard/Plugins/Statistics/ProjectStatisticsPlugin.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
-using System.IO;
 using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
 using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
 using ThoughtWorks.CruiseControl.Core.Util;
 using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
@@ -12,57 +12,69 @@ using ThoughtWorks.CruiseControl.WebDashboard.ServerConnection;
 
 namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.Statistics
 {
-	[ReflectorType("projectStatisticsPlugin")]
-	public class ProjectStatisticsPlugin : ICruiseAction, IPlugin
-	{
-		private readonly IFarmService farmService;
-		private readonly IPhysicalApplicationPathProvider pathProvider;
-		public static readonly string ACTION_NAME = "ViewStatisticsReport";
-		private string xslFileName;
-		private ITransformer transformer;
+    [ReflectorType("projectStatisticsPlugin")]
+    public class ProjectStatisticsPlugin : ICruiseAction, IPlugin
+    {
+        public static readonly string ACTION_NAME = "ViewStatisticsReport";
+        private readonly IFarmService farmService;
+        private readonly IPhysicalApplicationPathProvider pathProvider;
+        private readonly ITransformer transformer;
+        private string xslFileName;
 
-		public ProjectStatisticsPlugin(IFarmService farmService, IPhysicalApplicationPathProvider pathProvider)
-		{
-			this.farmService = farmService;
-			this.pathProvider = pathProvider;
-			transformer = new XslTransformer();
-		}
+        public ProjectStatisticsPlugin(IFarmService farmService, IPhysicalApplicationPathProvider pathProvider)
+        {
+            this.farmService = farmService;
+            this.pathProvider = pathProvider;
+            transformer = new XslTransformer();
+        }
 
-		[ReflectorProperty("xslFileName")]
-		public string XslFileName
-		{
-			get
-			{
-				return xslFileName;
-			}
-			set
-			{
-				xslFileName = value;
-			}
-		}
+        [ReflectorProperty("xslFileName")]
+        public string XslFileName
+        {
+            get { return xslFileName; }
+            set { xslFileName = value; }
+        }
 
-		public INamedAction[] NamedActions
-		{
-			get { return new INamedAction[]{new ImmutableNamedAction(ACTION_NAME, this)}; }
-		}
+        #region ICruiseAction Members
 
-		public IResponse Execute(ICruiseRequest cruiseRequest)
-		{
-			if (xslFileName == null)
-			{
-				throw new ApplicationException("XSL File Name has not been set for XSL Report Action");
-			}
-			Hashtable xsltArgs = new Hashtable();
-			xsltArgs["applicationPath"] = cruiseRequest.Request.ApplicationPath;
+        public IResponse Execute(ICruiseRequest cruiseRequest)
+        {
+            if (xslFileName == null)
+            {
+                throw new ApplicationException("XSL File Name has not been set for XSL Report Action");
+            }
+            Hashtable xsltArgs = new Hashtable();
+            xsltArgs["applicationPath"] = cruiseRequest.Request.ApplicationPath;
 
-			string xslFile = pathProvider.GetFullPathFor(XslFileName);
-			string statisticsDocument = farmService.GetStatisticsDocument(cruiseRequest.ProjectSpecifier);
-			return new HtmlFragmentResponse(transformer.Transform(statisticsDocument, xslFile, xsltArgs));
-		}
+            string xslFile = pathProvider.GetFullPathFor(XslFileName);
+            string statisticsDocument = farmService.GetStatisticsDocument(cruiseRequest.ProjectSpecifier);
+            Log.Debug(statisticsDocument);
+            string htmlFragment;
+            try
+            {
+                htmlFragment = transformer.Transform(statisticsDocument, xslFile, xsltArgs);
+            }
+            catch (CruiseControlException)
+            {
+                htmlFragment = "Missing/Invalid statistics reports. Please check if you have enabled the Statistics Publisher, and statistics have been collected atleast once after that.";
+            }
+            return new HtmlFragmentResponse(htmlFragment);
+        }
 
-		public string LinkDescription
-		{
-			get { return "View Statistics"; }
-		}
-	}
+        #endregion
+
+        #region IPlugin Members
+
+        public INamedAction[] NamedActions
+        {
+            get { return new INamedAction[] {new ImmutableNamedAction(ACTION_NAME, this)}; }
+        }
+
+        public string LinkDescription
+        {
+            get { return "View Statistics"; }
+        }
+
+        #endregion
+    }
 }
