16abbf0430ecd7dd93b5ed0ab99d4caf2bb1a5cf
Ross Patterson <ross.patterson@gmail.com>
2007-08-20 18:14:18 +0000

    Add unit tests to verify default values and to check ability to load from configuration XML.

--- project/UnitTests/Core/Label/FileLabellerTest.cs
+++ project/UnitTests/Core/Label/FileLabellerTest.cs
@@ -1,3 +1,5 @@
+using System;
+using Exortech.NetReflector;
 using NUnit.Framework;
 using ThoughtWorks.CruiseControl.Core;
 using ThoughtWorks.CruiseControl.Core.Label;
@@ -9,7 +11,65 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
     public class FileLabellerTest : IntegrationFixture
     {
         [Test]
-        public void ShouldGeneratedLabelWithPrefix()
+        public void VerifyDefaultValues()
+        {
+            FileLabeller labeller = new FileLabeller(new TestFileReader("001")); 
+            Assert.AreEqual(string.Empty, labeller.Prefix);
+            Assert.AreEqual(string.Empty, labeller.LabelFilePath);
+            Assert.AreEqual(true, labeller.AllowDuplicateSubsequentLabels);
+        }
+
+        [Test]
+        public void ShouldPopulateCorrectlyFromXml()
+        {
+            FileLabeller labeller = new FileLabeller(new TestFileReader("001"));
+            string xml = @"<fileLabeller prefix=""foo"" labelFilePath=""label.txt"" allowDuplicateSubsequentLabels=""false"" />";
+            NetReflector.Read(xml, labeller);
+            Assert.AreEqual("foo", labeller.Prefix);
+            Assert.AreEqual("label.txt", labeller.LabelFilePath);
+            Assert.AreEqual(false, labeller.AllowDuplicateSubsequentLabels);
+        }
+
+        [Test]
+        public void ShouldPopulateCorrectlyFromMinimalXml()
+        {
+            FileLabeller labeller = new FileLabeller(new TestFileReader("001"));
+            string xml = @"<fileLabeller labelFilePath=""label.txt"" />";
+            NetReflector.Read(xml, labeller);
+            Assert.AreEqual("", labeller.Prefix);
+            Assert.AreEqual("label.txt", labeller.LabelFilePath);
+            Assert.AreEqual(true, labeller.AllowDuplicateSubsequentLabels);
+        }
+
+        [Test]
+        public void ShouldFailToPopulateFromConfigurationMissingRequiredFields()
+        {
+            FileLabeller labeller = new FileLabeller(new TestFileReader("001"));
+            string xml = @"<fileLabeller prefix=""foo"" allowDuplicateSubsequentLabels=""false"" />";
+            string expectedExceptionText = "Missing Xml node (labelFilePath) for required member (ThoughtWorks.CruiseControl.Core.Label.FileLabeller.LabelFilePath).";
+            try
+            {
+                NetReflector.Read(xml, labeller);
+                Assert.Fail("Should have received a NetReflectorException with message: {0}", expectedExceptionText);
+            }
+            catch (AssertionException)
+            {
+                throw;
+            }
+            catch (NetReflectorException e)
+            {
+                Assert.AreEqual(expectedExceptionText, e.Message,
+                                "Received an unexpected NetReflector exception");
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(typeof (NetReflectorException), e,
+                                        "Received an unexpected exception type");
+            }
+        }
+
+        [Test]
+        public void ShouldGenerateLabelWithPrefix()
         {
             FileLabeller labeller = new FileLabeller(new TestFileReader("001"));
             labeller.Prefix = "V0-";
