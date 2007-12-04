261829155230debaa1b7e147be0f207e642e9a41
Ross Patterson <ross.patterson@gmail.com>
2007-12-04 19:36:23 +0000

    Document the ProcessArgumentBuilder.

--- project/core/util/ProcessArgumentBuilder.cs
+++ project/core/util/ProcessArgumentBuilder.cs
@@ -6,7 +6,14 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 	{
 		private StringBuilder builder = new StringBuilder();
 
-		public void AppendArgument(string format, string value)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list, separating it from the rest of the list
+        /// with a space, formatting the value with the specified format string <i>a la</i>
+        /// <see cref="StringBuilder.AppendFormat(string, object[])"/>.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="value">The argument value.</param>
+        public void AppendArgument(string format, string value)
 		{
 			if (StringUtil.IsBlank(value)) return;
 
@@ -14,7 +21,12 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 			builder.AppendFormat(format, value);
 		}
 
-		public void AppendArgument(string value)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list, separating it from the rest of the list
+        /// with a space.
+        /// </summary>
+        /// <param name="value">The argument value.</param>
+        public void AppendArgument(string value)
 		{
 			if (StringUtil.IsBlank(value)) return;
 
@@ -22,34 +34,68 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 			builder.Append(value);
 		}
 
+        /// <summary>
+        /// Add a space to the end of the argument list if it is not empty.
+        /// </summary>
 		private void AppendSpaceIfNotEmpty()
 		{
 			if (builder.Length > 0) builder.Append(" ");
 		}
 
-		public void AppendIf(bool condition, string value)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list if the specified condition is true, separating
+        /// it from the rest of the list with a space.
+        /// </summary>
+        /// <param name="condition">True if the method should append, false otherwise.</param>
+        /// <param name="value">The argument value.</param>
+        public void AppendIf(bool condition, string value)
 		{
 			if (condition) AppendArgument(value);
 		}
 
-		public void AppendIf(bool condition, string format, string argument)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list if the specified condition is true, separating 
+        /// it from the rest of the list with a space, formatting the value with the specified format string <i>a la</i>
+        /// <see cref="StringBuilder.AppendFormat(string, object[])"/>.
+        /// </summary>
+        /// <param name="condition">True if the method should append, false otherwise.</param>
+        /// <param name="format">The format string.</param>
+        /// <param name="argument">The argument value.</param>
+        public void AppendIf(bool condition, string format, string argument)
 		{
 			if (condition) AppendArgument(format, argument);
 		}
 
+        /// <summary>
+        /// Add the specified text to the end of the argument list exactly as it is.
+        /// </summary>
+        /// <param name="text">The text to add.</param>
 		public void Append(string text)
 		{
 			builder.Append(text);
 		}
 
-//		public string Separator = ":";
-
-		public void AddArgument(string arg, string value)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list, separating it from the rest of the list
+        /// with a space, separating the name and value with a space, and enquoting the value if the value contains 
+        /// any spaces.  If the value is an empty string or null, nothing is appended. 
+        /// </summary>
+        /// <param name="arg">The name of the argument to add.</param>
+        /// <param name="value">The value of the argument to add.</param>
+        public void AddArgument(string arg, string value)
 		{
 			AddArgument(arg, " ", value);
 		}
 
-		public void AddArgument(string arg, string separator, string value)
+        /// <summary>
+        /// Add the specified argument to the end of the argument list, separating it from the rest of the list
+        /// with a space, separating the name and value with the specified separator, and enquoting the value 
+        /// if the value contains any spaces.  If the value is an empty string or null, nothing is appended. 
+        /// </summary>
+        /// <param name="arg">The name of the argument to add.</param>
+        /// <param name="separator">The separator to place between the name and value.</param>
+        /// <param name="value">The value of the argument to add.</param>
+        public void AddArgument(string arg, string separator, string value)
 		{
 			if (StringUtil.IsBlank(value)) return;
 			AppendSpaceIfNotEmpty();
@@ -57,6 +103,12 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 			builder.Append(string.Format("{0}{1}{2}", arg, separator, SurroundInQuotesIfContainsSpace(value)));
 		}
 
+        /// <summary>
+        /// Add the specified argument to the end of the argument list, separating it from the rest of the list
+        /// with a space, and enquoting it if it contains any spaces.  If the argument is an empty string or null,
+        /// nothing is appended. 
+        /// </summary>
+        /// <param name="value">The argument to add.</param>
 		public void AddArgument(string value)
 		{
 			if (StringUtil.IsBlank(value)) return;
@@ -65,6 +117,15 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 			builder.Append(SurroundInQuotesIfContainsSpace(value));			
 		}
 		
+        /// <summary>
+        /// Add a leading and trailing quote to the input value if it contains any spaces, and return the result.
+        /// </summary>
+        /// <param name="value">The value to enquote.</param>
+        /// <returns>The enquoted value.</returns>
+        /// <remarks>
+        /// Note: This method does not do anything special to quotes contained in the value (<i>i.e.</i>, <code>A"B</code> 
+        /// becomes <code>"A"B"</code>, not <code>"A""B"</code> or <code>"A\"B"</code>.
+        /// </remarks>
 		private string SurroundInQuotesIfContainsSpace(string value)
 		{
 			if (! StringUtil.IsBlank(value) && value.IndexOf(' ') >= 0)
@@ -72,6 +133,10 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 			return value;
 		}
 
+        /// <summary>
+        /// Return the argument list, converted to a string.
+        /// </summary>
+        /// <returns>The argument list, converted to a string.</returns>
 		public override string ToString()
 		{
 			return builder.ToString();
