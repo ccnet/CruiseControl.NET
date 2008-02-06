93fae6884d7ad45c68b80d3b16375d32a91acea4
Ross Patterson <ross.patterson@gmail.com>
2008-02-06 16:04:57 +0000

    CCNET-1075 - Unable to kill processes on Windows 2000 because taskkill.exe doesn't exist.

--- project/core/util/KillUtil.cs
+++ project/core/util/KillUtil.cs
@@ -6,6 +6,11 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 {
     class KillUtil
     {
+        /// <summary>
+        /// Default installation directory for the "Windows 2000 Service Pack 4 Support Tools" package.
+        /// </summary>
+        public const string WIN2K_SUPPORT_TOOLS_DIR = @"C:\\Program Files\\Support Tools";
+
         public static void KillPid(int pid)
         {
             Process process = new Process();
@@ -13,10 +18,23 @@ namespace ThoughtWorks.CruiseControl.Core.Util
             switch (Environment.OSVersion.Platform)
             {
                 case PlatformID.Win32NT:
-                    process.StartInfo.FileName = string.Format("{0}\\taskkill.exe",
-                            Environment.GetFolderPath(Environment.SpecialFolder.System));
-                    process.StartInfo.Arguments = string.Format("/pid {0} /t /f", pid);
-                    break;
+                    if (Environment.OSVersion.Version.Minor == 0)
+                    {
+                        // Windows 2000 doesn't have taskkill.exe, so use kill.exe from the 
+                        // "Windows 2000 Service Pack 4 Support Tools" package from Microsoft's download center
+                        // (http://www.microsoft.com/Downloads/details.aspx?FamilyID=f08d28f3-b835-4847-b810-bb6539362473&displaylang=en)
+                        // instead.  It may not exist, but if it doesn't, at least if can be obtained.
+                        process.StartInfo.FileName = string.Format("{0}\\kill.exe", WIN2K_SUPPORT_TOOLS_DIR);
+                        process.StartInfo.Arguments = string.Format("-f {0}", pid);
+                        break;
+                    }
+                    else
+                    {
+                        process.StartInfo.FileName = string.Format("{0}\\taskkill.exe",
+                                Environment.GetFolderPath(Environment.SpecialFolder.System));
+                        process.StartInfo.Arguments = string.Format("/pid {0} /t /f", pid);
+                        break;
+                    }
 
                 case PlatformID.Unix:
                     process.StartInfo.FileName = "/usr/bin/pkill";
