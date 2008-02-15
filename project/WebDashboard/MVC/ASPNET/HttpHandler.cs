6265bdf8b45ee809231b30740e13397b4c92fd20
Ross Patterson <ross.patterson@gmail.com>
2008-02-15 17:03:02 +0000

    CCNET-1089 Add CCNet version to dashboard HTTP responses

--- project/WebDashboard/MVC/ASPNET/HttpHandler.cs
+++ project/WebDashboard/MVC/ASPNET/HttpHandler.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Web;
 using Objection;
 using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
@@ -15,6 +16,10 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.MVC.ASPNET
 				new CachingImplementationResolver(new NMockAwareImplementationResolver(), new CachedTypeMap(context.Cache, RESOLVED_TYPE_MAP)), 
                 new MaxLengthConstructorSelectionStrategy());
 			ObjectSource objectSource = new CruiseObjectSourceInitializer(objectionStore).SetupObjectSourceForRequest(context);
+
+            context.Response.AppendHeader("X-CCNet-Version", 
+                string.Format("CruiseControl.NET/{0}", Assembly.GetExecutingAssembly().GetName().Version));
+		    Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
 			IResponse response = ((RequestController) objectSource.GetByType(typeof (RequestController))).Do();
 			response.Process(context.Response);
