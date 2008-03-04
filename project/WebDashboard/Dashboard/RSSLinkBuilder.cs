482864efdff6c35ce68157c2a71b93522781b67b
Ross Patterson <ross.patterson@gmail.com>
2008-03-04 21:10:04 +0000

    CCNET-1080 - URL generated by RSS link may be incorrect.

--- project/WebDashboard/Dashboard/RSSLinkBuilder.cs
+++ project/WebDashboard/Dashboard/RSSLinkBuilder.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
 
 namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
@@ -14,14 +11,16 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
 
             if (System.Web.HttpContext.Current == null ) 
             {
-                MachineName = "localhost/";
+                MachineName = "localhost";
             }
             else
             {
-                MachineName  = System.Web.HttpContext.Current.Server.MachineName;
+                MachineName = System.Web.HttpContext.Current.Request.Url.Host;
+                if (!System.Web.HttpContext.Current.Request.Url.IsDefaultPort)
+                    MachineName = MachineName + ":" + System.Web.HttpContext.Current.Request.Url.Port;
             }
 
-            return  new GeneralAbsoluteLink("RSS",string.Format("http://{0}{1}",
+            return  new GeneralAbsoluteLink("RSS",string.Format("http://{0}/{1}",
                          MachineName,  
                          linkFactory.CreateProjectLink(projectSpecifier, WebDashboard.Plugins.RSS.RSSFeed.ACTION_NAME).Url));
        }
