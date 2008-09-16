5d714ad72528c8c5b7484176cc2831dae163069c
Ruben Willems <ruben.willems@gmail.com>
2008-09-16 20:15:07 +0000

    CCNET-1248 add a postfix in the default labeler

--- project/UnitTests/Core/Label/DefaultLabellerTest.cs
+++ project/UnitTests/Core/Label/DefaultLabellerTest.cs
@@ -40,6 +40,15 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 			Assert.AreEqual("Sample" + DefaultLabeller.INITIAL_LABEL.ToString(), labeller.Generate(InitialIntegrationResult()));
 		}
 
+
+        [Test]
+        public void GenerateInitialPostfixedLabel()
+        {
+            labeller.LabelPostfix = "QA_Approved";
+            Assert.AreEqual(DefaultLabeller.INITIAL_LABEL.ToString()+ "QA_Approved", labeller.Generate(InitialIntegrationResult()));
+        }
+
+
 		[Test]
 		public void GeneratePrefixedLabelWhenLastBuildSucceeded()
 		{
@@ -47,12 +56,51 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 			Assert.AreEqual("Sample36", labeller.Generate(SuccessfulResult("35")));
 		}
 
-		[Test]
+
+        [Test]
+        public void GeneratePostfixedLabelWhenLastBuildSucceeded()
+        {
+            labeller.LabelPostfix = "Sample";
+            Assert.AreEqual("36Sample", labeller.Generate(SuccessfulResult("35")));
+        }
+
+
+        [Test]
+        public void GeneratePreAndPostfixedLabelWhenLastBuildSucceeded()
+        {
+            labeller.LabelPrefix = "Sample";
+            labeller.LabelPostfix = "QA_OK";
+            Assert.AreEqual("Sample36QA_OK", labeller.Generate(SuccessfulResult("35")));
+            
+        }
+
+
+        [Test]
+        public void GeneratePreAndPostfixedLabelWhenLastBuildSucceededPreAndPostFixContainingNumericParts()
+        {
+            labeller.LabelPrefix = "Numeric55Sample";
+            labeller.LabelPostfix = "QA11OK";
+            Assert.AreEqual("Numeric55Sample36QA11OK", labeller.Generate(SuccessfulResult("35")));
+
+        }
+
+        
+        [Test]
 		public void GeneratePrefixedLabelWhenLastBuildFailed()
 		{
 			labeller.LabelPrefix = "Sample";
 			Assert.AreEqual("23", labeller.Generate(FailedResult("23")));
 		}
+
+
+        [Test]
+        public void GeneratePostFixedLabelWhenLastBuildFailed()
+        {
+            labeller.LabelPostfix = "Sample";
+            Assert.AreEqual("23", labeller.Generate(FailedResult("23")));
+        }
+
+
 
 		[Test]
 		public void GeneratePrefixedLabelWhenLastBuildSucceededAndHasLabelWithPrefix()
@@ -61,6 +109,15 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 			Assert.AreEqual("Sample24", labeller.Generate(SuccessfulResult("Sample23")));
 		}
 
+
+        [Test]
+        public void GeneratePostfixedLabelWhenLastBuildSucceededAndHasLabelWithPostfix()
+        {
+            labeller.LabelPostfix = "Sample";
+            Assert.AreEqual("24Sample", labeller.Generate(SuccessfulResult("23Sample")));
+        }
+
+
 		[Test]
 		public void GeneratePrefixedLabelWhenPrefixAndLastIntegrationLabelDontMatch()
 		{
@@ -68,21 +125,50 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 			Assert.AreEqual("Sample24", labeller.Generate(SuccessfulResult("SomethingElse23")));
 		}
 
-		[Test]
+
+        [Test]
+        public void GeneratePostfixedLabelWhenPostfixAndLastIntegrationLabelDontMatch()
+        {
+            labeller.LabelPostfix = "Sample";
+            Assert.AreEqual("24Sample", labeller.Generate(SuccessfulResult("23Dummy")));
+        }
+
+        
+        [Test]
 		public void GeneratePrefixedLabelWhenPrefixIsNumeric()
 		{
 			labeller.LabelPrefix = "R3SX";
 			Assert.AreEqual("R3SX24", labeller.Generate(SuccessfulResult("R3SX23")));
 		}
 
+
         [Test]
-        public void GenerateInitialFormattedLabel()
+        public void GeneratePrefixedLabelWhenPostfixIsNumeric()
+        {
+            labeller.LabelPostfix = "R3";
+            Assert.AreEqual("24R3", labeller.Generate(SuccessfulResult("23R3")));
+        }
+
+
+
+        [Test]
+        public void GenerateInitialFormattedLabelWithPrefix()
         {
             labeller.LabelPrefix = "Sample";
             labeller.LabelFormat = "000";
             Assert.AreEqual("Sample" + DefaultLabeller.INITIAL_LABEL.ToString("000"), labeller.Generate(InitialIntegrationResult()));
         }
 
+
+        [Test]
+        public void GenerateInitialFormattedLabelWithPostfix()
+        {
+            labeller.LabelPostfix = "Sample";
+            labeller.LabelFormat = "000";
+            Assert.AreEqual(DefaultLabeller.INITIAL_LABEL.ToString("000") + "Sample" , labeller.Generate(InitialIntegrationResult()));
+        }
+
+        
         [Test]
         public void GenerateFormattedLabelWhenLastBuildSucceeded()
         {
@@ -92,12 +178,22 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
         }
 
         [Test]
-        public void GenerateFormattedLabelWhenLastBuildFailed()
+        public void GenerateFormattedLabelWhenLastBuildFailedWithPrefix()
         {
             labeller.LabelPrefix = "Sample";
             labeller.LabelFormat = "000";
             Assert.AreEqual("23", labeller.Generate(FailedResult("23")));
         }
+
+
+        [Test]
+        public void GenerateFormattedLabelWhenLastBuildFailedWithPostFix()
+        {
+            labeller.LabelPostfix = "Sample";
+            labeller.LabelFormat = "000";
+            Assert.AreEqual("23", labeller.Generate(FailedResult("23")));
+        }
+
 
 
 		[Test]
@@ -107,13 +203,17 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 			Assert.AreEqual("24", labeller.Generate(FailedResult("23")));
 		}
 
+
+
+
 		[Test]
 		public void PopulateFromConfiguration()
 		{
-			string xml = @"<defaultLabeller prefix=""foo"" incrementOnFailure=""true"" />";
+            string xml = @"<defaultLabeller prefix=""foo"" incrementOnFailure=""true"" postfix=""bar"" />";
 			NetReflector.Read(xml, labeller);
 			Assert.AreEqual("foo", labeller.LabelPrefix);
 			Assert.AreEqual(true, labeller.IncrementOnFailed);
+            Assert.AreEqual("bar", labeller.LabelPostfix);
 		}
 
 		[Test]
@@ -121,6 +221,7 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 		{
 			Assert.AreEqual(string.Empty, labeller.LabelPrefix);
 			Assert.AreEqual(false, labeller.IncrementOnFailed);
+            Assert.AreEqual(string.Empty, labeller.LabelPostfix);
 		}
 	}
 }
--- project/core/label/DefaultLabeller.cs
+++ project/core/label/DefaultLabeller.cs
@@ -4,53 +4,100 @@ using ThoughtWorks.CruiseControl.Remote;
 
 namespace ThoughtWorks.CruiseControl.Core.Label
 {
-	[ReflectorType("defaultlabeller")]
-	public class DefaultLabeller : ILabeller
-	{
-		public const int INITIAL_LABEL = 1;
-        
-        [ReflectorProperty("prefix", Required=false)]
-		public string LabelPrefix = string.Empty;
+    [ReflectorType("defaultlabeller")]
+    public class DefaultLabeller : ILabeller
+    {
+        public const int INITIAL_LABEL = 1;
 
-		[ReflectorProperty("incrementOnFailure", Required=false)]
-		public bool IncrementOnFailed = false;
+        [ReflectorProperty("prefix", Required = false)]
+        public string LabelPrefix = string.Empty;
+
+        [ReflectorProperty("postfix", Required = false)]
+        public string LabelPostfix = string.Empty;
+
+
+        [ReflectorProperty("incrementOnFailure", Required = false)]
+        public bool IncrementOnFailed = false;
 
         [ReflectorProperty("labelFormat", Required = false)]
         public string LabelFormat = "0";
 
-		public virtual string Generate(IIntegrationResult integrationResult)
-		{
-			IntegrationSummary lastIntegration = integrationResult.LastIntegration;
-			if (integrationResult == null || lastIntegration.IsInitial())
-			{
-				return LabelPrefix + INITIAL_LABEL.ToString(LabelFormat);
-			}
-			else if (ShouldIncrementLabel(lastIntegration))
-			{
-				return LabelPrefix + IncrementLabel(lastIntegration.Label);
-			}
-			else
-			{
-				return integrationResult.LastIntegration.Label;
-			}
-		}
+        public virtual string Generate(IIntegrationResult integrationResult)
+        {
+            IntegrationSummary lastIntegration = integrationResult.LastIntegration;
+            if (integrationResult == null || lastIntegration.IsInitial())
+            {
+                return LabelPrefix + INITIAL_LABEL.ToString(LabelFormat) + LabelPostfix;
+            }
+            else if (ShouldIncrementLabel(lastIntegration))
+            {
+                return LabelPrefix + IncrementLabel(lastIntegration.Label) + LabelPostfix;
+            }
+            else
+            {
+                return integrationResult.LastIntegration.Label;
+            }
+        }
 
-		private bool ShouldIncrementLabel(IntegrationSummary previousResult)
-		{
-			return previousResult.Status == IntegrationStatus.Success || IncrementOnFailed;
-		}
+        private bool ShouldIncrementLabel(IntegrationSummary previousResult)
+        {
+            return previousResult.Status == IntegrationStatus.Success || IncrementOnFailed;
+        }
 
-		public void Run(IIntegrationResult result)
-		{
-			result.Label = Generate(result);
-		}
+        public void Run(IIntegrationResult result)
+        {
+            result.Label = Generate(result);
+        }
 
-		private string IncrementLabel(string label)
-		{
-			string numericLabel = Regex.Replace(label, @".*?(\d+$)", "$1");
-			int newLabel = int.Parse(numericLabel);
-			newLabel++;
-			return newLabel.ToString(LabelFormat);
-		}
-	}
+        private string IncrementLabel(string label)
+        {
+            if (LabelPostfix.Length == 0 && LabelPrefix.Length > 0)
+            {
+                string numericLabel = Regex.Replace(label, @".*?(\d+$)", "$1");
+                int newLabel = int.Parse(numericLabel);
+                newLabel++;
+                return newLabel.ToString(LabelFormat);
+
+            }
+
+            if (LabelPrefix.Length == 0 && LabelPostfix.Length >= 0)
+            {
+                string numericLabel = Regex.Replace(label, @"\D*?(\d{1,9}).*", "$1");
+                int newLabel = int.Parse(numericLabel);
+                newLabel++;
+                return newLabel.ToString(LabelFormat);
+
+            }
+
+            // prefix and postfix are present, do some extra checking
+            MatchCollection NumericParts = Regex.Matches(label, @"\D*?(\d{1,9})\D*");
+
+            if (NumericParts.Count == 1)
+            {
+                int newLabel = int.Parse(NumericParts[0].ToString());
+                newLabel++;
+                return newLabel.ToString(LabelFormat);
+            }
+
+
+            // multiple numeric parts found, remove pre and postfix from the label
+            // and scan for numeric parts
+            // if only 1 part found, that should be the label
+            // otherwise it is impossible to identify the label
+
+            label = label.Replace(LabelPrefix, "");
+            label = label.Replace(LabelPostfix, "");
+
+            NumericParts = Regex.Matches(label, @"\D*?(\d{1,9})\D*");
+
+            if (NumericParts.Count == 1)
+            {
+                int newLabel = int.Parse(NumericParts[0].ToString());
+                newLabel++;
+                return newLabel.ToString(LabelFormat);
+            }
+
+            throw new CruiseControlException("Unable to determine numeric part in label, pre and postfix may not contain multiple numeric parts");
+        }
+    }
 }
