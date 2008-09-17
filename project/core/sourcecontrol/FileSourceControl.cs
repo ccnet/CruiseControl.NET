b8584c953c2cdd19ebd403766e425fd46282d573
Ruben Willems <ruben.willems@gmail.com>
2008-09-17 11:46:21 +0000

    CCNet-1109 FileSourceControl should also detect new files as modifications

--- project/core/sourcecontrol/FileSourceControl.cs
+++ project/core/sourcecontrol/FileSourceControl.cs
@@ -6,93 +6,108 @@ using ThoughtWorks.CruiseControl.Core.Util;
 
 namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
 {
-	[ReflectorType("filesystem")]
-	public class FileSourceControl : ISourceControl
-	{
-		private readonly IFileSystem fileSystem;
+    [ReflectorType("filesystem")]
+    public class FileSourceControl : ISourceControl
+    {
+        private readonly IFileSystem fileSystem;
 
-		public FileSourceControl() : this(new SystemIoFileSystem())
-		{}
+        public FileSourceControl()
+            : this(new SystemIoFileSystem())
+        { }
 
-		public FileSourceControl(IFileSystem fileSystem)
-		{
-			this.fileSystem = fileSystem;
-		}
+        public FileSourceControl(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
 
-		[ReflectorProperty("repositoryRoot")]
-		public string RepositoryRoot;
+        [ReflectorProperty("repositoryRoot")]
+        public string RepositoryRoot;
 
-		[ReflectorProperty("ignoreMissingRoot", Required=false)]
-		public bool IgnoreMissingRoot;
+        [ReflectorProperty("ignoreMissingRoot", Required = false)]
+        public bool IgnoreMissingRoot;
 
-		[ReflectorProperty("autoGetSource", Required = false)]
-		public bool AutoGetSource = false;
+        [ReflectorProperty("autoGetSource", Required = false)]
+        public bool AutoGetSource = false;
 
-		public Modification[] GetModifications(IIntegrationResult from, IIntegrationResult to)
-		{
-			DirectoryInfo root = new DirectoryInfo(from.BaseFromWorkingDirectory(RepositoryRoot));
-			ArrayList modifications = GetMods(root, from.StartTime);
-			return (Modification[]) modifications.ToArray(typeof (Modification));
-		}
+        public Modification[] GetModifications(IIntegrationResult from, IIntegrationResult to)
+        {
+            DirectoryInfo root = new DirectoryInfo(from.BaseFromWorkingDirectory(RepositoryRoot));
+            ArrayList modifications = GetMods(root, from.StartTime);
+            return (Modification[])modifications.ToArray(typeof(Modification));
+        }
 
-		private ArrayList GetMods(DirectoryInfo dir, DateTime from)
-		{
-			ArrayList mods = new ArrayList();
-			try
-			{
-				foreach (FileInfo file in dir.GetFiles())
-				{
-					if (IsLocalFileChanged(file, from))
-					{
-						mods.Add(CreateModification(file));
-					}
-				}
+        private ArrayList GetMods(DirectoryInfo dir, DateTime from)
+        {
+            ArrayList mods = new ArrayList();
+            try
+            {
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    if (IsLocalFileChanged(file, from))
+                    {
+                        mods.Add(CreateModification(file));
+                    }
+                }
 
-				foreach (DirectoryInfo sub in dir.GetDirectories())
-				{
-					mods.AddRange(GetMods(sub, from));
-				}
-			}
-			catch (DirectoryNotFoundException exc)
-			{
-				if (!IgnoreMissingRoot)
-				{
-					throw exc;
-				}
-			}
+                foreach (DirectoryInfo sub in dir.GetDirectories())
+                {
+                    mods.AddRange(GetMods(sub, from));
+                }
+            }
+            catch (DirectoryNotFoundException exc)
+            {
+                if (!IgnoreMissingRoot)
+                {
+                    throw exc;
+                }
+            }
 
-			return mods;
-		}
+            return mods;
+        }
 
-		private Modification CreateModification(FileInfo info)
-		{
-			Modification modification = new Modification();
-			modification.FileName = info.Name;
-			modification.ModifiedTime = info.LastWriteTime;
-			modification.FolderName = info.DirectoryName;
-			return modification;
-		}
+        private Modification CreateModification(FileInfo info)
+        {
+            Modification modification = new Modification();
+            modification.FileName = info.Name;
+            modification.FolderName = info.DirectoryName;
 
-		private bool IsLocalFileChanged(FileInfo reposFile, DateTime date)
-		{
-			return reposFile.LastWriteTime > date;
-		}
+            if (info.CreationTime > info.LastWriteTime)
+            {
+                modification.ModifiedTime = info.CreationTime;
+            }
+            else
+            {
+                modification.ModifiedTime = info.LastWriteTime;
+            }
 
-		public void LabelSourceControl(IIntegrationResult result)
-		{}
+            return modification;
+        }
 
-		public void GetSource(IIntegrationResult result)
-		{
+        private bool IsLocalFileChanged(FileInfo reposFile, DateTime date)
+        {
+            bool result = false;
+
+            if (reposFile.LastWriteTime > date) result = true;
+            if (reposFile.CreationTime > date) result = true;
+
+            return result;
+        }
+
+        public void LabelSourceControl(IIntegrationResult result)
+        { }
+
+        public void GetSource(IIntegrationResult result)
+        {
             result.BuildProgressInformation.SignalStartRunTask("Getting source from FileSourceControl");
 
-			if (AutoGetSource)
-				fileSystem.Copy(RepositoryRoot, result.WorkingDirectory);
-		}
+            if (AutoGetSource)
+                fileSystem.Copy(RepositoryRoot, result.WorkingDirectory);
+        }
 
-		public void Initialize(IProject project)
-		{}
+        public void Initialize(IProject project)
+        { }
 
-		public void Purge(IProject project)
-		{}
-	}
+        public void Purge(IProject project)
+        { }
+    }
 }
