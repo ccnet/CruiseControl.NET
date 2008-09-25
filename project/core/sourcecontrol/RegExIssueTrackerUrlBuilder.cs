79317fdcba1800ef915afae765c39eaf5d41f73b
Ruben Willems <ruben.willems@gmail.com>
2008-09-25 20:00:20 +0000

    CCNet-1262 Create a Regualar Expression IssueTrackUrl Builder

---
+++ project/core/sourcecontrol/RegExIssueTrackerUrlBuilder.cs
@@ -0,0 +1,43 @@
+using Exortech.NetReflector;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
+{
+    [ReflectorType("regexIssueTracker")]
+    public class RegExIssueTrackerUrlBuilder : IModificationUrlBuilder
+    {
+        private string _find;
+        private string _replace;
+
+        [ReflectorProperty("find")]
+        public string Find
+        {
+            get { return _find; }
+            set { _find = value; }
+        }
+
+
+        [ReflectorProperty("replace")]
+        public string Replace
+        {
+            get { return _replace; }
+            set { _replace = value; }
+        }
+
+        public void SetupModification(Modification[] modifications)
+        {
+            foreach (Modification mod in modifications)
+            {
+                if (mod.Comment.Length > 0)
+                {
+                    if (Regex.IsMatch(mod.Comment, _find))
+                    {
+                        mod.IssueUrl = Regex.Replace(mod.Comment, _find, _replace);
+                    }
+                }
+
+            }
+        }
+    }
+}
