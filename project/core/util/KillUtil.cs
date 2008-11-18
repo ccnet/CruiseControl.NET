80b71040717db95e0133d64905561fc7fe558db2
Ruben Willems <ruben.willems@gmail.com>
2008-11-18 19:03:29 +0000

    CCNet-1267 killutil.cs - killPid incorrectly identifies a Mac OSX as Unix patch provided by Alex Hutton

--- project/core/util/KillUtil.cs
+++ project/core/util/KillUtil.cs
@@ -14,7 +14,8 @@ namespace ThoughtWorks.CruiseControl.Core.Util
         public static void KillPid(int pid)
         {
             Process process = new Process();
-
+            string platform = string.Empty;
+           
             switch (Environment.OSVersion.Platform)
             {
                 case PlatformID.Win32NT:
@@ -26,6 +27,7 @@ namespace ThoughtWorks.CruiseControl.Core.Util
                         // instead.  It may not exist, but if it doesn't, at least if can be obtained.
                         process.StartInfo.FileName = string.Format("{0}\\kill.exe", WIN2K_SUPPORT_TOOLS_DIR);
                         process.StartInfo.Arguments = string.Format("-f {0}", pid);
+                        platform = "Windows";
                         break;
                     }
                     else
@@ -33,22 +35,54 @@ namespace ThoughtWorks.CruiseControl.Core.Util
                         process.StartInfo.FileName = string.Format("{0}\\taskkill.exe",
                                 Environment.GetFolderPath(Environment.SpecialFolder.System));
                         process.StartInfo.Arguments = string.Format("/pid {0} /t /f", pid);
+                        platform = "Windows";
                         break;
                     }
 
-                case PlatformID.Unix:
-                    process.StartInfo.FileName = "/usr/bin/pkill";
-                    process.StartInfo.Arguments = string.Format("-9 -g {0}", pid);
-                    break;
+               case PlatformID.Unix:
+                                      
+                    // need to execute uname -s to find out if it is a MAC or not
+                    Process unameprocess = new Process();
+                    process.StartInfo.FileName = "uname";
+                    process.StartInfo.Arguments = "-s";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardOutput = true;
 
+                    process.Start();
+                    process.WaitForExit();
+
+                    StreamReader soReader = process.StandardOutput;
+
+                    string output = soReader.ReadToEnd();
+
+                    int nRet = process.ExitCode;
+
+                    process.Close();                                        
+
+                    if ((nRet == 0) && (output.Contains("Darwin")))
+                    {
+                        process.StartInfo.FileName = "/bin/kill";
+                        process.StartInfo.Arguments = string.Format("-9 {0}", pid);
+                        platform = "Mac";
+
+                    }
+                    else
+                    {
+                        process.StartInfo.FileName = "/usr/bin/pkill";
+                        process.StartInfo.Arguments = string.Format("-9 -g {0}", pid);
+                        platform = "Unix";
+                    }
+
+                    break;  
+                
                 default:
                     throw new Exception("Unknown Operating System.");
             }
 
             if (!File.Exists(process.StartInfo.FileName))
             {
-                throw new Exception(string.Format("Kill command {0} not found.",
-                                                  process.StartInfo.FileName));
+                throw new Exception(string.Format("Kill command {0} not found on {1} OS. PID:{2}",
+                                                  process.StartInfo.FileName, platform, Convert.ToString(pid)));
             }
 
             process.Start();
