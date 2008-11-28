b9e3e568b33cc5d4efca062120a3ef4fb47e9492
Ruben Willems <ruben.willems@gmail.com>
2008-11-28 14:41:33 +0000

    CCNet1294 OK / Cancel buttons should respond to Enter/ESC on "Add Server..." dialog.

--- project/CCTrayLib/Presentation/AddBuildServer.cs
+++ project/CCTrayLib/Presentation/AddBuildServer.cs
@@ -67,180 +67,178 @@ namespace ThoughtWorks.CruiseControl.CCTrayLib.Presentation
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(AddBuildServer));
-			this.label1 = new System.Windows.Forms.Label();
-			this.rdoDashboard = new System.Windows.Forms.RadioButton();
-			this.rdoRemoting = new System.Windows.Forms.RadioButton();
-			this.rdoHttp = new System.Windows.Forms.RadioButton();
-			this.label2 = new System.Windows.Forms.Label();
-			this.txtDashboard = new System.Windows.Forms.TextBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.txtRemoting = new System.Windows.Forms.TextBox();
-			this.label5 = new System.Windows.Forms.Label();
-			this.txtHttp = new System.Windows.Forms.TextBox();
-			this.btnCancel = new System.Windows.Forms.Button();
-			this.btnOK = new System.Windows.Forms.Button();
-			this.lblFeedback = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(5, 10);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(490, 35);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "CCTray can monitor build servers in different ways.  Select how you want to monit" +
-				"or the server, then enter the required information.";
-			// 
-			// rdoDashboard
-			// 
-			this.rdoDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.rdoDashboard.Location = new System.Drawing.Point(10, 45);
-			this.rdoDashboard.Name = "rdoDashboard";
-			this.rdoDashboard.Size = new System.Drawing.Size(370, 24);
-			this.rdoDashboard.TabIndex = 1;
-			this.rdoDashboard.Text = "Via the CruiseControl.NET dashboard";
-			this.rdoDashboard.CheckedChanged += new System.EventHandler(this.rdoDashboard_CheckedChanged);
-			// 
-			// rdoRemoting
-			// 
-			this.rdoRemoting.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.rdoRemoting.Location = new System.Drawing.Point(10, 150);
-			this.rdoRemoting.Name = "rdoRemoting";
-			this.rdoRemoting.Size = new System.Drawing.Size(345, 24);
-			this.rdoRemoting.TabIndex = 2;
-			this.rdoRemoting.TabStop = true;
-			this.rdoRemoting.Text = "Connect directly using .NET remoting";
-			this.rdoRemoting.CheckedChanged += new System.EventHandler(this.rdoRemoting_CheckedChanged);
-			// 
-			// rdoHttp
-			// 
-			this.rdoHttp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.rdoHttp.Location = new System.Drawing.Point(10, 295);
-			this.rdoHttp.Name = "rdoHttp";
-			this.rdoHttp.Size = new System.Drawing.Size(345, 24);
-			this.rdoHttp.TabIndex = 3;
-			this.rdoHttp.Text = "Supply a custom HTTP URL";
-			this.rdoHttp.CheckedChanged += new System.EventHandler(this.rdoHttp_CheckedChanged);
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(30, 70);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(465, 45);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "This is the preferred way of monitoring build status.  In the box below, enter th" +
-				"e http address of the dashboard home directory in the box below, e.g. http://bui" +
-				"ld-server/ccnet.  The dashboard must be version 1.1 or later.";
-			// 
-			// txtDashboard
-			// 
-			this.txtDashboard.Location = new System.Drawing.Point(30, 116);
-			this.txtDashboard.Name = "txtDashboard";
-			this.txtDashboard.Size = new System.Drawing.Size(350, 20);
-			this.txtDashboard.TabIndex = 5;
-			this.txtDashboard.Text = "http://localhost/ccnet";
-			// 
-			// label3
-			// 
-			this.label3.Location = new System.Drawing.Point(30, 175);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(470, 45);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "CCTray will connect directly to the build server using .NET Remoting.  This is ho" +
-				"w CruiseControl.NET 1.0 worked, but it often forces you to install a new version" +
-				" of CCTray when the server is upgraded.";
-			// 
-			// label4
-			// 
-			this.label4.Location = new System.Drawing.Point(30, 220);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(470, 45);
-			this.label4.TabIndex = 7;
-			this.label4.Text = "In the box below, enter the name of the build server to monitor.  If you have con" +
-				"figured your build server to run on a port other than the default of 21234, you " +
-				"can follow the name of the build server with :port.";
-			// 
-			// txtRemoting
-			// 
-			this.txtRemoting.Location = new System.Drawing.Point(30, 265);
-			this.txtRemoting.Name = "txtRemoting";
-			this.txtRemoting.Size = new System.Drawing.Size(350, 20);
-			this.txtRemoting.TabIndex = 8;
-			this.txtRemoting.Text = "localhost";
-			// 
-			// label5
-			// 
-			this.label5.Location = new System.Drawing.Point(30, 325);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(465, 45);
-			this.label5.TabIndex = 9;
-			this.label5.Text = "Use this option to monitor a non-CruiseControl.NET build server (e.g. CruiseContr" +
-				"ol for Java).  CCTray will perform an HTTP GET on this URL, and expect to retrie" +
-				"ve an XML file as described at http://confluence.public.thoughtworks.org/display" +
-				"/CI/.";
-			// 
-			// txtHttp
-			// 
-			this.txtHttp.Location = new System.Drawing.Point(30, 370);
-			this.txtHttp.Name = "txtHttp";
-			this.txtHttp.Size = new System.Drawing.Size(350, 20);
-			this.txtHttp.TabIndex = 10;
-			this.txtHttp.Text = "http://localhost/cruisecontrol/xml.jsp";
-			// 
-			// btnCancel
-			// 
-			this.btnCancel.CausesValidation = false;
-			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.btnCancel.Location = new System.Drawing.Point(260, 450);
-			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.TabIndex = 12;
-			this.btnCancel.Text = "Cancel";
-			// 
-			// btnOK
-			// 
-			this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.btnOK.Location = new System.Drawing.Point(175, 450);
-			this.btnOK.Name = "btnOK";
-			this.btnOK.TabIndex = 11;
-			this.btnOK.Text = "OK";
-			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
-			// 
-			// lblFeedback
-			// 
-			this.lblFeedback.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.lblFeedback.Location = new System.Drawing.Point(30, 400);
-			this.lblFeedback.Name = "lblFeedback";
-			this.lblFeedback.Size = new System.Drawing.Size(455, 40);
-			this.lblFeedback.TabIndex = 13;
-			this.lblFeedback.Text = "lblFeedback";
-			this.lblFeedback.Visible = false;
-			// 
-			// AddBuildServer
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(502, 483);
-			this.Controls.Add(this.lblFeedback);
-			this.Controls.Add(this.btnCancel);
-			this.Controls.Add(this.btnOK);
-			this.Controls.Add(this.txtHttp);
-			this.Controls.Add(this.txtRemoting);
-			this.Controls.Add(this.txtDashboard);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.rdoHttp);
-			this.Controls.Add(this.rdoRemoting);
-			this.Controls.Add(this.rdoDashboard);
-			this.Controls.Add(this.label1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "AddBuildServer";
-			this.Text = "Build Server";
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddBuildServer));
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdoDashboard = new System.Windows.Forms.RadioButton();
+            this.rdoRemoting = new System.Windows.Forms.RadioButton();
+            this.rdoHttp = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDashboard = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRemoting = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHttp = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(5, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CCTray can monitor build servers in different ways.  Select how you want to monit" +
+                "or the server, then enter the required information.";
+            // 
+            // rdoDashboard
+            // 
+            this.rdoDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoDashboard.Location = new System.Drawing.Point(10, 45);
+            this.rdoDashboard.Name = "rdoDashboard";
+            this.rdoDashboard.Size = new System.Drawing.Size(370, 24);
+            this.rdoDashboard.TabIndex = 1;
+            this.rdoDashboard.Text = "Via the CruiseControl.NET dashboard";
+            this.rdoDashboard.CheckedChanged += new System.EventHandler(this.rdoDashboard_CheckedChanged);
+            // 
+            // rdoRemoting
+            // 
+            this.rdoRemoting.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoRemoting.Location = new System.Drawing.Point(10, 150);
+            this.rdoRemoting.Name = "rdoRemoting";
+            this.rdoRemoting.Size = new System.Drawing.Size(345, 24);
+            this.rdoRemoting.TabIndex = 2;
+            this.rdoRemoting.TabStop = true;
+            this.rdoRemoting.Text = "Connect directly using .NET remoting";
+            this.rdoRemoting.CheckedChanged += new System.EventHandler(this.rdoRemoting_CheckedChanged);
+            // 
+            // rdoHttp
+            // 
+            this.rdoHttp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoHttp.Location = new System.Drawing.Point(10, 295);
+            this.rdoHttp.Name = "rdoHttp";
+            this.rdoHttp.Size = new System.Drawing.Size(345, 24);
+            this.rdoHttp.TabIndex = 3;
+            this.rdoHttp.Text = "Supply a custom HTTP URL";
+            this.rdoHttp.CheckedChanged += new System.EventHandler(this.rdoHttp_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(465, 45);
+            this.label2.TabIndex = 4;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // txtDashboard
+            // 
+            this.txtDashboard.Location = new System.Drawing.Point(30, 116);
+            this.txtDashboard.Name = "txtDashboard";
+            this.txtDashboard.Size = new System.Drawing.Size(350, 20);
+            this.txtDashboard.TabIndex = 5;
+            this.txtDashboard.Text = "http://localhost/ccnet";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(30, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(470, 45);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CCTray will connect directly to the build server using .NET Remoting.  This is ho" +
+                "w CruiseControl.NET 1.0 worked, but it often forces you to install a new version" +
+                " of CCTray when the server is upgraded.";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(30, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(470, 45);
+            this.label4.TabIndex = 7;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // txtRemoting
+            // 
+            this.txtRemoting.Location = new System.Drawing.Point(30, 265);
+            this.txtRemoting.Name = "txtRemoting";
+            this.txtRemoting.Size = new System.Drawing.Size(350, 20);
+            this.txtRemoting.TabIndex = 8;
+            this.txtRemoting.Text = "localhost";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(30, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(465, 45);
+            this.label5.TabIndex = 9;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // txtHttp
+            // 
+            this.txtHttp.Location = new System.Drawing.Point(30, 370);
+            this.txtHttp.Name = "txtHttp";
+            this.txtHttp.Size = new System.Drawing.Size(350, 20);
+            this.txtHttp.TabIndex = 10;
+            this.txtHttp.Text = "http://localhost/cruisecontrol/xml.jsp";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.CausesValidation = false;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(260, 450);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            // 
+            // btnOK
+            // 
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOK.Location = new System.Drawing.Point(175, 450);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 11;
+            this.btnOK.Text = "OK";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFeedback.Location = new System.Drawing.Point(30, 400);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(455, 40);
+            this.lblFeedback.TabIndex = 13;
+            this.lblFeedback.Text = "lblFeedback";
+            this.lblFeedback.Visible = false;
+            // 
+            // AddBuildServer
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(502, 483);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtHttp);
+            this.Controls.Add(this.txtRemoting);
+            this.Controls.Add(this.txtDashboard);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rdoHttp);
+            this.Controls.Add(this.rdoRemoting);
+            this.Controls.Add(this.rdoDashboard);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddBuildServer";
+            this.Text = "Build Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
