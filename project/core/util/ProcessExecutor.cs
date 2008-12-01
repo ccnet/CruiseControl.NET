0fccae85d2353d74c68d3387d1fd9865775f8bfc
David Cameron <dave.bc@gmail.com>
2008-12-01 13:40:38 +0000

    Need to create ticket: Changed output logging to be independent of thread used. Reformatted whitespace to use tabs.

--- project/core/util/ProcessExecutor.cs
+++ project/core/util/ProcessExecutor.cs
@@ -14,160 +14,169 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 	/// </summary>
 	public class ProcessExecutor
 	{
-       	private RunnableProcess p;        
+		private RunnableProcess p;
 
 		public virtual ProcessResult Execute(ProcessInfo processInfo)
 		{
-			using (p = new RunnableProcess(processInfo))
+			string projectName = Thread.CurrentThread.Name;
+			using (p = new RunnableProcess(processInfo, projectName))
 			{
 				return p.Run();
 			}
 		}
-		
+
 		// BuildTasks receive a ProcessMonitor to monitor their build process
 		public virtual ProcessResult Execute(ProcessInfo processInfo, ProcessMonitor processMonitor)
 		{
-			using(p = new RunnableProcess(processInfo))
+			string projectName = Thread.CurrentThread.Name;
+			using (p = new RunnableProcess(processInfo, projectName))
 			{
-				processMonitor.MonitorNewProcess(p.process);
-				return p.Run();	
+				processMonitor.MonitorNewProcess(p.Process);
+				return p.Run();
 			}
 		}
 
-        public void Kill()
-        {
-            Log.Info(string.Format("The process will be killed: {0}", p));
-            try
-            {
-                using (p)
-                {
-                    p.Kill();
-                }
-            }
-            catch (InvalidOperationException)
-            {
-                Log.Info(string.Format("The process can't be killed because it got disposed already: {0}", p));
-            }
-        }
-		
+		public void Kill()
+		{
+			Log.Info(string.Format("The process will be killed: {0}", p));
+			try
+			{
+				using (p)
+				{
+					p.Kill();
+				}
+			}
+			catch (InvalidOperationException)
+			{
+				Log.Info(string.Format("The process can't be killed because it got disposed already: {0}", p));
+			}
+		}
+
 		private class RunnableProcess : IDisposable
 		{
-            private static string processName = string.Empty;
+			private readonly string projectName;
 			private readonly ProcessInfo processInfo;
-			public readonly Process process;
-            private StringBuilder stdOutput = new StringBuilder();
-            private StringBuilder stdError = new StringBuilder();
-            
-                                  
-			public RunnableProcess(ProcessInfo processInfo)
+			private readonly Process process;
+			private readonly StringBuilder stdOutput = new StringBuilder();
+			private readonly StringBuilder stdError = new StringBuilder();
+
+
+			public RunnableProcess(ProcessInfo processInfo, string projectName)
 			{
-                processName = Thread.CurrentThread.Name;
+				this.projectName = projectName;
 				this.processInfo = processInfo;
 				process = processInfo.CreateProcess();
 			}
 
 			public ProcessResult Run()
 			{
-                bool hasTimedOut = false;
-                bool failed = true;
-                bool hasExited = false;
-                int exitcode = 0;
-                                              
-                try
-                {
-                    process.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
-                    process.ErrorDataReceived += new DataReceivedEventHandler(SortErrorOutputHandler);
-                    Log.Debug(string.Format("Starting process [{0}] in working directory [{1}] with arguments [{2}]", process.StartInfo.FileName, process.StartInfo.WorkingDirectory, process.StartInfo.Arguments));
-                    bool isNewProcess = process.Start();
-                    if (!isNewProcess) Log.Warning("Reusing existing process...");
+				bool hasTimedOut = false;
+				bool failed = true;
+				bool hasExited = false;
+				int exitcode = 0;
 
-                    WriteToStandardInput();
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
+				try
+				{
+					process.OutputDataReceived += StandardOutputHandler;
+					process.ErrorDataReceived += ErrorOutputHandler;
+					Log.Debug(string.Format("Starting process [{0}] in working directory [{1}] with arguments [{2}]", process.StartInfo.FileName, process.StartInfo.WorkingDirectory, process.StartInfo.Arguments));
+					bool isNewProcess = process.Start();
+					if (!isNewProcess) Log.Warning("Reusing existing process...");
 
-                    try
-                    {
-                        hasExited = process.WaitForExit(processInfo.TimeOut);
-                    }
-                    catch (ThreadAbortException e)
-                    {
-                        process.CancelErrorRead();
-                        process.CancelOutputRead();
-                        Thread.ResetAbort();
-                    }                    
+					WriteToStandardInput();
+					process.BeginOutputReadLine();
+					process.BeginErrorReadLine();
 
-                    if (!hasExited)
-                    {
-                        hasTimedOut = true;
-                        Kill();
-                    }
+					try
+					{
+						hasExited = process.WaitForExit(processInfo.TimeOut);
+					}
+					catch (ThreadAbortException)
+					{
+						// Why are we catching ThreadAbortException. This is not a known throw from WaitForExit(int).
+						// If the ProjectIntegrator's thread is aborted we *will* end up here, but should we be calling ResetAbort()?
+						process.CancelErrorRead();
+						process.CancelOutputRead();
+						Thread.ResetAbort();
+					}
 
-                    exitcode = process.ExitCode;
+					if (!hasExited)
+					{
+						hasTimedOut = true;
+						Kill();
+					}
 
-                    failed = !processInfo.ProcessSuccessful(exitcode);
-                    
-                }
-                catch (Win32Exception e)
-                {
-                    string filename = Path.Combine(process.StartInfo.WorkingDirectory, process.StartInfo.FileName);
-                    string msg = string.Format("Unable to execute file [{0}].  The file may not exist or may not be executable.", filename);
-                    throw new IOException(msg, e);
-                }
-				
+					exitcode = process.ExitCode;
+
+					failed = !processInfo.ProcessSuccessful(exitcode);
+
+				}
+				catch (Win32Exception e)
+				{
+					string filename = Path.Combine(process.StartInfo.WorkingDirectory, process.StartInfo.FileName);
+					string msg = string.Format("Unable to execute file [{0}].  The file may not exist or may not be executable.", filename);
+					throw new IOException(msg, e);
+				}
+
+				// TODO: This WaitForExit() should really have a timeout. Otherwise, what if the process doesn't exit?
 				process.WaitForExit();
-                process.Close();
-				
+				process.Close();
+
 				return new ProcessResult(stdOutput.ToString(), stdError.ToString(), exitcode, hasTimedOut, failed);
 			}
-            
-            public void Kill()
-            {
-                try
-                {
-                    KillUtil.KillPid(process.Id);
-                    Log.Warning(string.Format("The process has been killed: {0}", process.Id));
-                }
-                catch (InvalidOperationException)
-                {
-                    Log.Warning(string.Format("Process has already exited before getting killed: {0}", process.Id));
-                }
-            }
 
-            private void WriteToStandardInput()
-            {
-                if (process.StartInfo.RedirectStandardInput)
-                {
-                    process.StandardInput.Write(processInfo.StandardInputContent);
-                    process.StandardInput.Flush();
-                    process.StandardInput.Close();
-                }
-            }
-
-            private void SortOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
-            {
-                // Collect the sort command output.
-                if (!String.IsNullOrEmpty(outLine.Data))
-                {
-                    if (Thread.CurrentThread.Name == null) Thread.CurrentThread.Name = processName;
-                    stdOutput.AppendLine(outLine.Data);
-                    Log.Debug(outLine.Data);
-                }
-            }
-
-            private void SortErrorOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
-            {
-                // Collect the sort command output.
-                if (!String.IsNullOrEmpty(outLine.Data))
-                {
-                    if (Thread.CurrentThread.Name == null) Thread.CurrentThread.Name = processName;
-                    stdError.AppendLine(outLine.Data);
-                    Log.Debug(outLine.Data);
-                }
-            }           
-
-            void IDisposable.Dispose()
+			public void Kill()
 			{
-				process.Dispose();       
+				try
+				{
+					KillUtil.KillPid(process.Id);
+					Log.Warning(string.Format("The process has been killed: {0}", process.Id));
+				}
+				catch (InvalidOperationException)
+				{
+					Log.Warning(string.Format("Process has already exited before getting killed: {0}", process.Id));
+				}
+			}
+
+			private void WriteToStandardInput()
+			{
+				if (process.StartInfo.RedirectStandardInput)
+				{
+					process.StandardInput.Write(processInfo.StandardInputContent);
+					process.StandardInput.Flush();
+					process.StandardInput.Close();
+				}
+			}
+
+			private void StandardOutputHandler(object sender, DataReceivedEventArgs outLine)
+			{
+				CollectOutput(sender, outLine, stdOutput);
+			}
+
+			private void ErrorOutputHandler(object sender, DataReceivedEventArgs outLine)
+			{
+				CollectOutput(sender, outLine, stdError);
+			}
+
+			private void CollectOutput(object sender, DataReceivedEventArgs outLine, StringBuilder collector)
+			{
+				Process sendingProcess = (Process)sender;
+				if (!String.IsNullOrEmpty(outLine.Data))
+				{
+					collector.AppendLine(outLine.Data);
+					Log.Debug(string.Format("[{0} {1}] {2}", projectName, sendingProcess.ProcessName, outLine.Data));
+				}
+			}
+
+			void IDisposable.Dispose()
+			{
+				process.Dispose();
+			}
+
+			// TODO: Smelly. ProcessMonitor doesn't seem like the right abstraction.
+			public Process Process
+			{
+				get { return process; }
 			}
 		}
 	}
