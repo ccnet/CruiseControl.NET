2bf05e985c493ec3d084b691a7c0e74db084257c
Ruben Willems <ruben.willems@gmail.com>
2008-12-22 20:33:59 +0000

    CCNet-1322 foresee a customtemplates folder for the dashboard when people have custom templates, it is best that these are stored in a separate folder, so they do not get overwritten on a upgrade of ccnet. This allows for easy customisation of the dashboard.

--- project/WebDashboard/MVC/View/LazilyInitialisingVelocityTransformer.cs
+++ project/WebDashboard/MVC/View/LazilyInitialisingVelocityTransformer.cs
@@ -10,53 +10,113 @@ using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
 
 namespace ThoughtWorks.CruiseControl.WebDashboard.MVC.View
 {
-	public class LazilyInitialisingVelocityTransformer : IVelocityTransformer
-	{
-		private readonly IPhysicalApplicationPathProvider physicalApplicationPathProvider;
+    public class LazilyInitialisingVelocityTransformer : IVelocityTransformer
+    {
+        private readonly IPhysicalApplicationPathProvider physicalApplicationPathProvider;
 
-		private VelocityEngine lazilyInitialisedEngine = null;
+        private VelocityEngine lazilyInitialisedEngine = null;
+        private VelocityEngine lazilyCustomInitialisedEngine = null;
+        private System.Collections.Generic.Dictionary<string, TemplateLocation> FoundTemplates = new System.Collections.Generic.Dictionary<string, TemplateLocation>();
 
-		public LazilyInitialisingVelocityTransformer(IPhysicalApplicationPathProvider physicalApplicationPathProvider)
-		{
-			this.physicalApplicationPathProvider = physicalApplicationPathProvider;
-		}
+        public enum TemplateLocation
+        {
+            Templates, CustomTemplates
+        }
 
-		public string Transform(string transformerFileName, Hashtable transformable)
-		{
-			string output = "";
-			using(TextWriter writer = new StringWriter())
-			{
-				try
-				{
-					VelocityEngine.MergeTemplate(transformerFileName, RuntimeConstants.ENCODING_DEFAULT, new VelocityContext(transformable), writer);
-				}
-				catch (Exception baseException)
-				{
-					throw new CruiseControlException(string.Format(@"Exception calling NVelocity for template: {0}
+
+
+        public LazilyInitialisingVelocityTransformer(IPhysicalApplicationPathProvider physicalApplicationPathProvider)
+        {
+            this.physicalApplicationPathProvider = physicalApplicationPathProvider;
+        }
+
+        public string Transform(string transformerFileName, Hashtable transformable)
+        {
+            string output = "";
+            using (TextWriter writer = new StringWriter())
+            {
+                try
+                {
+
+                    if (DetermineTemplateLocation(transformerFileName) == TemplateLocation.CustomTemplates )
+                    {
+                        VelocityEngineCustom.MergeTemplate(transformerFileName, RuntimeConstants.ENCODING_DEFAULT, new VelocityContext(transformable), writer);
+
+                    }
+                    else
+                    {
+                        VelocityEngine.MergeTemplate(transformerFileName, RuntimeConstants.ENCODING_DEFAULT, new VelocityContext(transformable), writer);
+                    }
+                }
+                catch (Exception baseException)
+                {
+                    throw new CruiseControlException(string.Format(@"Exception calling NVelocity for template: {0}
 Template path is {1}", transformerFileName, physicalApplicationPathProvider.GetFullPathFor("templates")), baseException);
-				}
-				output = writer.ToString();
-			}
-			return output;
-		}
+                }
+                output = writer.ToString();
+            }
+            return output;
+        }
 
-		private VelocityEngine VelocityEngine
-		{
-			get
-			{
-				lock (this)
-				{
-					if (lazilyInitialisedEngine == null)
-					{
-						lazilyInitialisedEngine = new VelocityEngine();
-						lazilyInitialisedEngine.SetProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "NVelocity.Runtime.Log.NullLogSystem");
-						lazilyInitialisedEngine.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, physicalApplicationPathProvider.GetFullPathFor("templates"));
-						lazilyInitialisedEngine.SetProperty(RuntimeConstants.RESOURCE_MANAGER_CLASS, "NVelocity.Runtime.Resource.ResourceManagerImpl");
-						lazilyInitialisedEngine.Init();
-					}
-				}
-				return lazilyInitialisedEngine;
-			}
-		}
-	}
+        private VelocityEngine VelocityEngine
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (lazilyInitialisedEngine == null)
+                    {
+                        lazilyInitialisedEngine = new VelocityEngine();
+                        lazilyInitialisedEngine.SetProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "NVelocity.Runtime.Log.NullLogSystem");
+                        lazilyInitialisedEngine.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, physicalApplicationPathProvider.GetFullPathFor("templates"));
+                        lazilyInitialisedEngine.SetProperty(RuntimeConstants.RESOURCE_MANAGER_CLASS, "NVelocity.Runtime.Resource.ResourceManagerImpl");
+                        lazilyInitialisedEngine.Init();
+                    }
+                }
+                return lazilyInitialisedEngine;
+            }
+        }
+
+
+
+        private VelocityEngine VelocityEngineCustom
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (lazilyCustomInitialisedEngine == null)
+                    {
+                        lazilyCustomInitialisedEngine = new VelocityEngine();
+                        lazilyCustomInitialisedEngine.SetProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "NVelocity.Runtime.Log.NullLogSystem");
+                        lazilyCustomInitialisedEngine.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, physicalApplicationPathProvider.GetFullPathFor("customtemplates"));
+                        lazilyCustomInitialisedEngine.SetProperty(RuntimeConstants.RESOURCE_MANAGER_CLASS, "NVelocity.Runtime.Resource.ResourceManagerImpl");
+                        lazilyCustomInitialisedEngine.Init();
+                    }
+                }
+                return lazilyCustomInitialisedEngine;
+            }
+        }
+
+
+        private TemplateLocation DetermineTemplateLocation(string transformerFileName)
+        {
+            if (!FoundTemplates.ContainsKey(transformerFileName))
+            {
+                string filelocation = System.IO.Path.Combine(physicalApplicationPathProvider.GetFullPathFor("customtemplates"),transformerFileName);
+                if (System.IO.File.Exists(filelocation))
+                {
+                    FoundTemplates.Add(transformerFileName, TemplateLocation.CustomTemplates);
+                }
+                else
+                {
+                    FoundTemplates.Add(transformerFileName, TemplateLocation.Templates);
+                }
+            }
+
+            return FoundTemplates[transformerFileName];
+        }
+
+
+    }
 }
