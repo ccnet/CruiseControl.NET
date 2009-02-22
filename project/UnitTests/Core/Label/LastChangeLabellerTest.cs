f89b53d49fc0fa3a8c051f1851952b81901d2259
Ruben Willems <ruben.willems@gmail.com>
2009-02-22 19:33:56 +0000

    CCNet-1383 updated last change labeller

--- project/UnitTests/Core/Label/LastChangeLabellerTest.cs
+++ project/UnitTests/Core/Label/LastChangeLabellerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Exortech.NetReflector;
+using NMock;
 using NUnit.Framework;
 using ThoughtWorks.CruiseControl.Core;
 using ThoughtWorks.CruiseControl.Core.Label;
@@ -7,71 +8,72 @@ using ThoughtWorks.CruiseControl.UnitTests.Core.Sourcecontrol;
 
 namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
 {
-	/// <remarks>
-	/// This code is based on code\label\DefaultLabeller.cs.
-	/// </remarks> 
-	[TestFixture]
-	public class LastChangeLabellerTest : CustomAssertion
-	{
-		private LastChangeLabeller labeller;
+    /// <remarks>
+    /// This code is based on code\label\DefaultLabeller.cs.
+    /// </remarks> 
+    [TestFixture]
+    public class LastChangeLabellerTest : IntegrationFixture
+    {
+        private LastChangeLabeller labeller;
 
-		[SetUp]
-		public void SetUp()
-		{
-			labeller = new LastChangeLabeller();
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            labeller = new LastChangeLabeller();
+        }
 
-		[Test]
-		public void GenerateLabel()
-		{
-			IntegrationResult result = CreateIntegrationResult();
-			AddModifications(result);
-			Assert.AreEqual("30", labeller.Generate(result));
-		}
+        [Test]
+        public void GenerateLabel()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            AddModifications(result);
+            Assert.AreEqual("30", labeller.Generate(result));
+        }
 
-		[Test]
-		public void GenerateLabelFromNoMods()
-		{
-			IntegrationResult result = CreateIntegrationResult();
-			Assert.AreEqual("unknown", labeller.Generate(result));
-		}
+        [Test]
+        public void GenerateLabelFromNoMods()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            Assert.AreEqual("unknown", labeller.Generate(result));
+        }
 
-		[Test]
-		public void GeneratePrefixedLabel()
-		{
-			IntegrationResult result = CreateIntegrationResult();
-			AddModifications(result);
-			labeller.LabelPrefix = "Sample";
-			Assert.AreEqual("Sample30", labeller.Generate(result));
-		}
+        [Test]
+        public void GeneratePrefixedLabel()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            AddModifications(result);
+            labeller.LabelPrefix = "Sample";
+            Assert.AreEqual("Sample30", labeller.Generate(result));
+        }
 
-		[Test]
-		public void GeneratePrefixedLabelWhenPrefixAndLastIntegrationLabelDontMatch()
-		{
-			IntegrationResult result = CreateIntegrationResult();
-			AddModifications(result);
-			labeller.LabelPrefix = "Sample";
-			result.LastSuccessfulIntegrationLabel = "SomethingElse23";
-			Assert.AreEqual("Sample30", labeller.Generate(result));
-		}
+        [Test]
+        public void GeneratePrefixedLabelWhenPrefixAndLastIntegrationLabelDontMatch()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            AddModifications(result);
+            labeller.LabelPrefix = "Sample";
+            result.LastSuccessfulIntegrationLabel = "SomethingElse23";
+            Assert.AreEqual("Sample30", labeller.Generate(result));
+        }
 
-		[Test]
-		public void GeneratePrefixedLabelWhenPrefixIsNumeric()
-		{
-			IntegrationResult result = CreateIntegrationResult();
-			AddModifications(result);
-			labeller.LabelPrefix = "R3SX";
-			result.LastSuccessfulIntegrationLabel = "R3SX23";
-			Assert.AreEqual("R3SX30", labeller.Generate(result));
-		}
+        [Test]
+        public void GeneratePrefixedLabelWhenPrefixIsNumeric()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            AddModifications(result);
+            labeller.LabelPrefix = "R3SX";
+            result.LastSuccessfulIntegrationLabel = "R3SX23";
+            Assert.AreEqual("R3SX30", labeller.Generate(result));
+        }
 
-		[Test]
-		public void PopulateFromConfiguration()
-		{
-			string xml = "<LastChangeLabeller prefix=\"foo\"/>";
-			NetReflector.Read(xml, labeller);
-			Assert.AreEqual("foo", labeller.LabelPrefix);
-		}
+        [Test]
+        public void PopulateFromConfiguration()
+        {
+            string xml = "<LastChangeLabeller prefix=\"foo\" allowDuplicateSubsequentLabels=\"false\" />";
+            NetReflector.Read(xml, labeller);
+            Assert.AreEqual("foo", labeller.LabelPrefix);
+            Assert.AreEqual(false, labeller.AllowDuplicateSubsequentLabels);
+        }
 
         [Test]
         public void PopulateFromMinimalConfiguration()
@@ -79,34 +81,77 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Label
             string xml = "<LastChangeLabeller/>";
             NetReflector.Read(xml, labeller);
             Assert.AreEqual(string.Empty, labeller.LabelPrefix);
+            Assert.AreEqual(true, labeller.AllowDuplicateSubsequentLabels);
         }
 
         [Test]
-		public void VerifyDefaultValues()
-		{
-			Assert.AreEqual(string.Empty, labeller.LabelPrefix);
+        public void VerifyDefaultValues()
+        {
+            Assert.AreEqual(string.Empty, labeller.LabelPrefix);
+            Assert.AreEqual(true, labeller.AllowDuplicateSubsequentLabels);
         }
 
-		private static IntegrationResult CreateIntegrationResult()
-		{
-			IntegrationResult result = IntegrationResultMother.CreateSuccessful();
-			return result;
-		}
-		
-		private static void AddModifications(IntegrationResult result) {
-			result.Modifications = new Modification[3];
-			result.Modifications[0] = ModificationMother.CreateModification("fileName", "folderName",
-																			new DateTime(), "userName",
-																			"comment", 10, "email@address",
-																			"http://url");
-			result.Modifications[1] = ModificationMother.CreateModification("fileName", "folderName",
-																			new DateTime(), "userName",
-																			"comment", 30, "email@address",
-																			"http://url");
-			result.Modifications[2] = ModificationMother.CreateModification("fileName", "folderName",
-			                                                                new DateTime(), "userName",
-																			"comment", 20, "email@address",
-																			"http://url");
-		}
-	}
+        private static IntegrationResult CreateIntegrationResult()
+        {
+            IntegrationResult result = IntegrationResultMother.CreateSuccessful();
+            return result;
+        }
+
+        private static void AddModifications(IntegrationResult result)
+        {
+            result.Modifications = new Modification[3];
+            result.Modifications[0] = ModificationMother.CreateModification("fileName", "folderName",
+                                                                            new DateTime(), "userName",
+                                                                            "comment", 10, "email@address",
+                                                                            "http://url");
+            result.Modifications[1] = ModificationMother.CreateModification("fileName", "folderName",
+                                                                            new DateTime(), "userName",
+                                                                            "comment", 30, "email@address",
+                                                                            "http://url");
+            result.Modifications[2] = ModificationMother.CreateModification("fileName", "folderName",
+                                                                            new DateTime(), "userName",
+                                                                            "comment", 20, "email@address",
+                                                                            "http://url");
+        }
+
+
+        [Test]
+        public void GenerateLabelFromNoModsIterative()
+        {
+            labeller.LabelPrefix = "DoesNotMatterForNoMods";
+            Assert.AreEqual("unknown", labeller.Generate(SuccessfulResult("unknown")));
+            Assert.AreEqual("30", labeller.Generate(SuccessfulResult("30")));
+            Assert.AreEqual("30.1", labeller.Generate(SuccessfulResult("30.1")));
+            Assert.AreEqual("Sample.30", labeller.Generate(SuccessfulResult("Sample.30")));
+            Assert.AreEqual("Sample.30.1", labeller.Generate(SuccessfulResult("Sample.30.1")));
+        }
+
+        [Test]
+        public void GenerateLabelFromNoModsIterativeWhenDuplicatesAreNotAllowed()
+        {
+            labeller.LabelPrefix = "DoesNotMatterForNoMods";
+            labeller.AllowDuplicateSubsequentLabels = false;
+            Assert.AreEqual("unknown.1", labeller.Generate(SuccessfulResult("unknown")));
+            Assert.AreEqual("30.1", labeller.Generate(SuccessfulResult("30")));
+            Assert.AreEqual("30.2", labeller.Generate(SuccessfulResult("30.1")));
+            Assert.AreEqual("Sample.30.1", labeller.Generate(SuccessfulResult("Sample.30")));
+            Assert.AreEqual("Sample.30.2", labeller.Generate(SuccessfulResult("Sample.30.1")));
+        }
+
+        [Test]
+        public void GeneratePrefixedLabelWhenDuplicatesAreNotAllowed()
+        {
+            IntegrationResult result = CreateIntegrationResult();
+            AddModifications(result);
+            labeller.LabelPrefix = "Sample";
+            labeller.AllowDuplicateSubsequentLabels = false;
+            Assert.AreEqual("Sample30.1", labeller.Generate(result));
+        }
+
+
+
+
+
+
+    }
 }
--- project/core/label/LastChangeLabeller.cs
+++ project/core/label/LastChangeLabeller.cs
@@ -1,45 +1,84 @@
+using System;
+using System.Text.RegularExpressions;
 using Exortech.NetReflector;
 using ThoughtWorks.CruiseControl.Core.Util;
 
 namespace ThoughtWorks.CruiseControl.Core.Label
 {
-	/// <summary>
-	/// Labeller for use with source code control systems that have numbered changes.
-	/// This labeller uses the last change number (IIntegrationResult.LastChangeNumber) 
-	/// as the build number, with an optional prefix.
-	/// </summary>
-	/// <remarks>
-	/// This code is based on code\label\DefaultLabeller.cs.
-	/// </remarks> 
-	[ReflectorType("lastChangeLabeller")]
-	public class LastChangeLabeller : ILabeller
-	{
-		/// <summary>
-		/// The string to be prepended onto the last change number.
-		/// </summary>
-		[ReflectorProperty("prefix", Required=false)]
-		public string LabelPrefix = string.Empty;
+    /// <summary>
+    /// Labeller for use with source code control systems that have numbered changes.
+    /// This labeller uses the last change number (IIntegrationResult.LastChangeNumber) 
+    /// as the build number, with an optional prefix.
+    /// </summary>
+    /// <remarks>
+    /// This code is based on code\label\DefaultLabeller.cs.
+    /// </remarks> 
+    [ReflectorType("lastChangeLabeller")]
+    public class LastChangeLabeller : ILabeller
+    {
+        /// <summary>
+        /// The string to be prepended onto the last change number.
+        /// </summary>
+        [ReflectorProperty("prefix", Required = false)]
+        public string LabelPrefix = string.Empty;
 
-		/// <summary>
-		/// Generate a label string from the last change number.
-		/// </summary>
-		/// <param name="resultFromThisBuild">IntegrationResult object for the current build</param>
-		/// <returns>the new label</returns>
-		public virtual string Generate(IIntegrationResult resultFromThisBuild)
-		{
-			int changeNumber = resultFromThisBuild.LastChangeNumber;
-			Log.Debug(string.Format("Last change number is \"{0}\"", changeNumber));
-			if (changeNumber != 0)
-				return LabelPrefix + changeNumber;
-			else
-				return LabelPrefix + "unknown";
-		}
-		
-		// Nothing seems to use this, but ILabellers are ITasks, and ITasks need a Run().  All the other
-		// labellers use this implementation, so we will too.
-		public void Run(IIntegrationResult result)
-		{
-			result.Label = Generate(result);
-		}
-	}
+        /// <summary>
+        /// Controls whether duplicate subsequent labels are permitted or not. If true, duplicate labels are left
+        /// intact. If false, the label will always be suffixed with ".n", where "n" is incremented for each
+        /// successive duplication. Defaults to true.
+        /// </summary>
+        [ReflectorProperty("allowDuplicateSubsequentLabels", Required = false)]
+        public bool AllowDuplicateSubsequentLabels = true;
+
+
+
+        /// <summary>
+        /// Generate a label string from the last change number.
+        /// If there is no valid change number (e.g. for a forced build without modifications),
+        /// then the last integration label is used.
+        /// </summary>
+        /// <param name="resultFromThisBuild">IntegrationResult object for the current build</param>
+        /// <returns>the new label</returns>
+        public virtual string Generate(IIntegrationResult resultFromThisBuild)
+        {
+            int changeNumber = resultFromThisBuild.LastChangeNumber;
+            IntegrationSummary lastIntegration = resultFromThisBuild.LastIntegration;
+            string changeLabel;
+
+            Log.Debug(string.Format("Last change number is \"{0}\"", changeNumber));
+            if (changeNumber != 0)
+                changeLabel = LabelPrefix + changeNumber;
+            else if (!lastIntegration.IsInitial() && lastIntegration.Label != null)
+                changeLabel = lastIntegration.Label;
+            else
+                changeLabel = LabelPrefix + "unknown";
+
+            if (!AllowDuplicateSubsequentLabels)
+            {
+                return IncrementLabel(changeLabel);
+            }
+            return changeLabel;
+        }
+
+        // Nothing seems to use this, but ILabellers are ITasks, and ITasks need a Run().  All the other
+        // labellers use this implementation, so we will too.
+        public void Run(IIntegrationResult result)
+        {
+            result.Label = Generate(result);
+        }
+
+        private string IncrementLabel(string label)
+        {
+            int current = 0;
+            Match match = Regex.Match(label, @"(.*\d+)\.(\d+)$");
+            if (match.Success && match.Groups.Count >= 3)
+            {
+                current = Int32.Parse(match.Groups[2].Value);
+                label = match.Groups[1].Value;
+            }
+            return String.Format("{0}.{1}", label, current + 1);
+        }
+
+
+    }
 }
