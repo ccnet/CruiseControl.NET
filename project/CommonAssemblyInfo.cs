fb0f11bc31066131f52819a5411a6da4a739bed1
David Cameron <dave.bc@gmail.com>
2009-03-14 09:35:52 +0000

    documentation, version number, copyright date updates for 1.4.3 (part 8)

--- project/CommonAssemblyInfo.cs
+++ project/CommonAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersionAttribute("0")]
-[assembly: AssemblyCopyrightAttribute("Copyright © 2008 ThoughtWorks Inc.")]
+[assembly: AssemblyCopyrightAttribute("Copyright © 2009 ThoughtWorks Inc.")]
 [assembly: AssemblyCompanyAttribute("ThoughtWorks")]
 [assembly: AssemblyProductAttribute("CruiseControl.NET")]
 
