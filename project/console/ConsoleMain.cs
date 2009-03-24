6d64845ef17d8e017105dd1de9f9a0f1184a7572
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-03-24 18:04:35 +0000

    CCNET-1409: Expand hit-swapping to include all DLLs and allow a "cool-down" period to reduce errors in the restart.

--- project/console/ConsoleMain.cs
+++ project/console/ConsoleMain.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 
 namespace ThoughtWorks.CruiseControl.Console
 {
@@ -11,26 +12,42 @@ namespace ThoughtWorks.CruiseControl.Console
 		{
             bool restart = true;
             int result = 0;
-            while (restart)
+            DateTime restartTime = DateTime.MinValue;
+            using (FileSystemWatcher watcher = new FileSystemWatcher(AppDomain.CurrentDomain.BaseDirectory, "*.dll"))
             {
-                using (FileSystemWatcher watcher = new FileSystemWatcher(AppDomain.CurrentDomain.BaseDirectory, "ThoughtWorks.CruiseControl.Core.dll"))
+                AppRunner runner = null;
+
+                // Start monitoring file changes
+                watcher.Changed += delegate(object sender, FileSystemEventArgs e)
+                {
+                    if (!restart) runner.Stop(e.Name + " has changed");
+                    restart = true;
+                    restartTime = DateTime.Now.AddSeconds(10);
+                };
+                watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.Size;
+                watcher.EnableRaisingEvents = true;
+
+                // Begin the main application loop
+                while (restart)
                 {
                     restart = false;
 
+                    // Load the domain and start the runner
                     AppDomain newDomain = AppDomain.CreateDomain("CC.Net",
                         null,
                         AppDomain.CurrentDomain.BaseDirectory,
                         AppDomain.CurrentDomain.RelativeSearchPath,
                         true);
-                    AppRunner runner = newDomain.CreateInstanceFromAndUnwrap(Assembly.GetExecutingAssembly().Location,
+                    runner = newDomain.CreateInstanceFromAndUnwrap(Assembly.GetExecutingAssembly().Location,
                         "ThoughtWorks.CruiseControl.Console.AppRunner") as AppRunner;
-
-                    watcher.Changed += delegate(object sender, FileSystemEventArgs e) { restart = true; runner.Stop(e.Name + " has changed"); };
-                    watcher.EnableRaisingEvents = true;
-                    watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.Size;
-
                     result = runner.Run(args);
                     AppDomain.Unload(newDomain);
+
+                    // Allow any change events to finish (i.e. if multiple files are copied)
+                    while (DateTime.Now < restartTime)
+                    {
+                        Thread.Sleep(500);
+                    }
                 }
             }
             return result;
