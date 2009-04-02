6a9b84ea35da576f4322d279958f8d3e36d9fdc4
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-02 23:59:16 +0000

    CCNET-1440: Tidied up Message to make it more usable by external parties, plus added unit tests for it.

---
+++ project/UnitTests/Remote/MessageTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ThoughtWorks.CruiseControl.Remote;
+using NUnit.Framework;
+
+namespace ThoughtWorks.CruiseControl.UnitTests.Remote
+{
+    [TestFixture]
+    public class MessageTests
+    {
+        #region Test methods
+        [Test]
+        public void StartANewBlankMessage()
+        {
+            Message value = new Message();
+            Assert.IsNull(value.Text);
+        }
+
+        [Test]
+        public void StartANewMessageWithText()
+        {
+            string expected = "Testing";
+            Message value = new Message(expected);
+            Assert.AreEqual(expected, value.Text);
+        }
+
+        [Test]
+        public void TextPropertyCanBeSet()
+        {
+            string expected = "Testing";
+            Message value = new Message();
+            Assert.IsNull(value.Text);
+            value.Text = expected;
+            Assert.AreEqual(expected, value.Text);
+        }
+
+        [Test]
+        public void ToStringReturnsMessage()
+        {
+            string expected = "Testing";
+            Message value = new Message(expected);
+            Assert.AreEqual(expected, value.ToString());
+        }
+        #endregion
+    }
+}
