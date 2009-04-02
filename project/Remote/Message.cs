7e1b7ba2ac9468b3d0dc9cc3ae378ae97c85d2e3
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-02 19:35:00 +0000

    CCNET-1440: Tidied up Message to make it more usable by external parties, plus added unit tests for it.

--- project/Remote/Message.cs
+++ project/Remote/Message.cs
@@ -2,19 +2,58 @@ using System;
 
 namespace ThoughtWorks.CruiseControl.Remote
 {
+    /// <summary>
+    /// A user-readable message.
+    /// </summary>
 	[Serializable]
 	public class Message
-	{
-		private string message;
+    {
+        #region Private fields
+        private string message;
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Initialise a new blank <see cref="Message"/>.
+        /// </summary>
+        public Message()
+        {
+        }
+
+        /// <summary>
+        /// Initialise a new <see cref="Message"/> with a message.
+        /// </summary>
+        /// <param name="message">The message.</param>
 		public Message(string message)
 		{
 			this.message = message;
-		}
+        }
+        #endregion
 
-		public override string ToString()
+        #region Public properties
+        #region Text
+        /// <summary>
+        /// The text of the message.
+        /// </summary>
+        public string Text
+        {
+            get { return message; }
+            set { message = value; }
+        }
+        #endregion
+        #endregion
+
+        #region Public methods
+        #region ToString()
+        /// <summary>
+        /// Returns the message text.
+        /// </summary>
+        /// <returns>The text of the message.</returns>
+        public override string ToString()
 		{
 			return message;
 		}
-	}
+        #endregion
+        #endregion
+    }
 }
