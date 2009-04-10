b194d75bcd085222167f1c44de50aa34c868ec79
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-10 07:36:49 +0000

    One last attempt to get the ManualResetEvent working for the unit tests.

--- project/UnitTests/Core/ProjectIntegratorTest.cs
+++ project/UnitTests/Core/ProjectIntegratorTest.cs
@@ -567,51 +567,51 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core
         //    Assert.AreEqual(IntegrationStatus.Success, status);
         //}
 
-        //[Test]
-        //public void IntegrationCanBeCancelled()
-        //{
-        //    string enforcer = "BuildForcer";
-        //    IntegrationResult result = new IntegrationResult();
-        //    result.ProjectName = (projectMock.MockInstance as IProject).Name;
-        //    result.Status = IntegrationStatus.Success;
-        //    // The following latch is needed to ensure the end assertions are not called before
-        //    // the events have been fired. Because of the multi-threaded nature of the integrators
-        //    // this can happen without any of the other latches being affected.
-        //    ManualResetEvent latch = new ManualResetEvent(false);
+        [Test]
+        public void IntegrationCanBeCancelled()
+        {
+            string enforcer = "BuildForcer";
+            IntegrationResult result = new IntegrationResult();
+            result.ProjectName = (projectMock.MockInstance as IProject).Name;
+            result.Status = IntegrationStatus.Success;
+            // The following latch is needed to ensure the end assertions are not called before
+            // the events have been fired. Because of the multi-threaded nature of the integrators
+            // this can happen without any of the other latches being affected.
+            ManualResetEvent latch = new ManualResetEvent(false);
 
-        //    bool eventIntegrationStartedFired = false;
-        //    bool eventIntegrationCompletedFired = false;
-        //    IntegrationStatus status = IntegrationStatus.Unknown;
-        //    integrator.IntegrationStarted += delegate(object o, IntegrationStartedEventArgs a)
-        //    {
-        //        eventIntegrationStartedFired = true;
-        //        a.Result = IntegrationStartedEventArgs.EventResult.Cancel;
-        //    };
-        //    integrator.IntegrationCompleted += delegate(object o, IntegrationCompletedEventArgs a)
-        //    {
-        //        eventIntegrationCompletedFired = true;
-        //        status = a.Status;
-        //        latch.Set();
-        //    };
+            bool eventIntegrationStartedFired = false;
+            bool eventIntegrationCompletedFired = false;
+            IntegrationStatus status = IntegrationStatus.Unknown;
+            integrator.IntegrationStarted += delegate(object o, IntegrationStartedEventArgs a)
+            {
+                eventIntegrationStartedFired = true;
+                a.Result = IntegrationStartedEventArgs.EventResult.Cancel;
+            };
+            integrator.IntegrationCompleted += delegate(object o, IntegrationCompletedEventArgs a)
+            {
+                eventIntegrationCompletedFired = true;
+                status = a.Status;
+                latch.Set();
+            };
 
-        //    integrationTriggerMock.ExpectNoCall("Fire");
-        //    projectMock.ExpectNoCall("Integrate", typeof(IntegrationRequest));
-        //    projectMock.Expect("NotifyPendingState");
-        //    projectMock.ExpectAndSignal("NotifySleepingState");
-        //    projectMock.ExpectNoCall("Integrate", typeof(IntegrationRequest));
-        //    projectMock.SetupResult("MaxSourceControlRetries", 5);
-        //    projectMock.SetupResult("SourceControlErrorHandling", ThoughtWorks.CruiseControl.Core.Sourcecontrol.Common.SourceControlErrorHandlingPolicy.ReportEveryFailure);
-        //    integrationTriggerMock.ExpectAndSignal("IntegrationCompleted");
-        //    integrator.ForceBuild(enforcer);
-        //    integrationTriggerMock.WaitForSignal();
-        //    projectMock.WaitForSignal();
-        //    VerifyAll();
+            integrationTriggerMock.ExpectNoCall("Fire");
+            projectMock.ExpectNoCall("Integrate", typeof(IntegrationRequest));
+            projectMock.Expect("NotifyPendingState");
+            projectMock.ExpectAndSignal("NotifySleepingState");
+            projectMock.ExpectNoCall("Integrate", typeof(IntegrationRequest));
+            projectMock.SetupResult("MaxSourceControlRetries", 5);
+            projectMock.SetupResult("SourceControlErrorHandling", ThoughtWorks.CruiseControl.Core.Sourcecontrol.Common.SourceControlErrorHandlingPolicy.ReportEveryFailure);
+            integrationTriggerMock.ExpectAndSignal("IntegrationCompleted");
+            integrator.ForceBuild(enforcer);
+            integrationTriggerMock.WaitForSignal();
+            projectMock.WaitForSignal();
+            VerifyAll();
 
-        //    latch.WaitOne(2000);
-        //    Assert.IsTrue(eventIntegrationStartedFired);
-        //    Assert.IsTrue(eventIntegrationCompletedFired);
-        //    Assert.AreEqual(IntegrationStatus.Cancelled, status);
-        //}
+            latch.WaitOne(2000, false);
+            Assert.IsTrue(eventIntegrationStartedFired);
+            Assert.IsTrue(eventIntegrationCompletedFired);
+            Assert.AreEqual(IntegrationStatus.Cancelled, status);
+        }
     }
 
 	public class HasForceBuildCondition : BaseConstraint
