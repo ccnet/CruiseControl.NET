aac5a668365e96e321e2525f714817a30580e723
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-14 09:28:01 +0000

    Changed the validator to use CommonAssemblyInfo instead of its own assembly information.

--- project/Validator/Properties/AssemblyInfo.cs
+++ project/Validator/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Validator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("healthAlliance")]
-[assembly: AssemblyProduct("Validator")]
-[assembly: AssemblyCopyright("Copyright © healthAlliance 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,19 +15,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a180ec8b-c031-4ed4-be5a-90499ecb8a31")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
