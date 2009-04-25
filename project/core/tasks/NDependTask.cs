49467950a48566ac70af3c1595f84a9dc058b7fa
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-25 08:48:16 +0000

    Changed the NDepend task so it publishes to its own folder in the artefacts folder (under the label).

--- project/core/tasks/NDependTask.cs
+++ project/core/tasks/NDependTask.cs
@@ -211,12 +211,12 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
             if (publish)
             {
                 // Check for any new files
-                FileInfo[] newFiles = ListFileDifferences(oldFiles, outputDirectory.GetFiles());
+                FileInfo[] newFiles = ListFileDifferences(oldFiles, outputDirectory);
 
                 if (newFiles.Length > 0)
                 {
                     // Copy all the new files over
-                    string publishDir = result.BaseFromArtifactsDirectory(result.Label);
+                    string publishDir = Path.Combine(result.BaseFromArtifactsDirectory(result.Label), "NDepend");
                     if (!Directory.Exists(publishDir)) Directory.CreateDirectory(publishDir);
                     foreach (FileInfo newFile in newFiles)
                     {
@@ -347,8 +347,11 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
         /// <param name="originalList"></param>
         /// <param name="newList"></param>
         /// <returns></returns>
-        private FileInfo[] ListFileDifferences(Dictionary<string, DateTime> originalList, FileInfo[] newList)
+        private FileInfo[] ListFileDifferences(Dictionary<string, DateTime> originalList, DirectoryInfo outputDirectory)
         {
+            FileInfo[] newList = {};
+            if (outputDirectory.Exists) newList = outputDirectory.GetFiles();
+
             List<FileInfo> differenceList = new List<FileInfo>();
 
             // For each new file, see if it is in the old file list
@@ -381,11 +384,14 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
         /// <returns></returns>
         private Dictionary<string, DateTime> GenerateOriginalFileList(DirectoryInfo outputDirectory)
         {
-            FileInfo[] oldFiles = outputDirectory.GetFiles();
             Dictionary<string, DateTime> originalFiles = new Dictionary<string, DateTime>();
-            foreach (FileInfo oldFile in oldFiles)
+            if (outputDirectory.Exists)
             {
-                originalFiles.Add(oldFile.Name, oldFile.LastWriteTime);
+                FileInfo[] oldFiles = outputDirectory.GetFiles();
+                foreach (FileInfo oldFile in oldFiles)
+                {
+                    originalFiles.Add(oldFile.Name, oldFile.LastWriteTime);
+                }
             }
             return originalFiles;
         }
