c53dec85b55c44623b38ec532e800e6f1f6b6e7b
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-04-27 23:34:46 +0000

    Fixed a bug where the packages were being downloaded from working folder instead of artefacts.

--- project/CCTrayLib/Presentation/PackagesListForm.cs
+++ project/CCTrayLib/Presentation/PackagesListForm.cs
@@ -186,7 +186,7 @@ namespace ThoughtWorks.CruiseControl.CCTrayLib.Presentation
                 Stream outputStream = File.Create(Path.Combine(downloadLocation, package.Name + ".zip"));
                 try
                 {
-                    IFileTransfer fileTransfer = project.RetrieveFileTransfer(package.FileName, FileTransferSource.Working);
+                    IFileTransfer fileTransfer = project.RetrieveFileTransfer(package.FileName, FileTransferSource.Artefact);
                     fileTransfer.Download(outputStream);
                 }
                 finally
