9983f69aa82a9e696d914fc3fd5bfb6f9d9d2084
Daniel Nauck <d.nauck@nauck-it.de>
2009-05-05 19:59:14 +0000

    * build-all.bat: call all target  * ccnet.build: include new build files, add showHelp as new default target

--- build-all.bat
+++ build-all.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
-Tools\NAnt\NAnt.exe -buildfile:ccnet.build -D:codemetrics.output.type=HtmlFile -nologo -logfile:nant-build-all.log.txt %*
+Tools\NAnt\NAnt.exe all -buildfile:ccnet.build -D:codemetrics.output.type=HtmlFile -nologo -logfile:nant-build-all.log.txt %*
 echo %time% %date%
 pause
