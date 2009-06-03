3013821983e5a4e9d53125f3747e5728d01443f6
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-06-03 02:51:17 +0000

    CCNET-1091: Added RoboCopy source control system (original patch by Len Holgate.)

---
+++ project/core/sourcecontrol/RobocopyHistoryParser.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
+{
+	public class RobocopyHistoryParser : IHistoryParser
+	{
+		public Modification[] Parse(TextReader reader, DateTime from, DateTime to)
+		{
+			ArrayList mods = new ArrayList();
+
+			string currentLine = reader.ReadLine();
+
+			while (currentLine != null)
+			{
+				// TODO - do it all with a regex?
+
+				if (currentLine.Contains(DELETED_DIR_TAG))
+				{
+					mods.Add(ParseDeletedDirectory(currentLine));
+				}
+				else if (currentLine.Contains(DELETED_FILE_TAG))
+				{
+					mods.Add(ParseDeletedFile(currentLine));
+				}
+				else if (currentLine.Contains(ADDED_FILE_TAG))
+				{
+					mods.Add(ParseAddedFile(currentLine));
+				}
+				else if (currentLine.Contains(UPDATED_FILE_TAG))
+				{
+					mods.Add(ParseUpdatedFile(currentLine));
+				}
+
+				currentLine = reader.ReadLine();
+			}
+
+			return (Modification[]) mods.ToArray(typeof (Modification));
+		}
+
+		private static string DELETED_DIR_TAG = "*EXTRA Dir";
+		private static string DELETED_FILE_TAG = "*EXTRA File";
+		private static string ADDED_FILE_TAG = "New File";
+		private static string UPDATED_FILE_TAG = "Newer";
+
+		private static readonly Regex ParseDeletedDirectoryRegex = new Regex(@"\s+\*EXTRA Dir\s+(?'Path'.*)");
+
+		// To match this
+		//	*EXTRA Dir  	E:\copytest\dst\dir2\
+
+		Modification ParseDeletedDirectory(
+			string logLine)
+		{
+			Match match = ParseDeletedDirectoryRegex.Match(logLine);
+
+			if (match.Success)
+			{
+				if (match.Groups.Count == 2)
+				{
+					string path = match.Groups["Path"].Captures[0].ToString();
+
+					Modification mod = new Modification();
+
+					mod.Type = "deleted";
+
+					mod.FolderName = Path.GetDirectoryName(path);
+
+					return mod;
+				}
+			}
+
+			throw new Exception("Failed to match regex");
+		}
+
+		private static readonly Regex ParseDeletedFileRegex = new Regex(@"\s+\*EXTRA File\s+(?'Date'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2})\s+(?'Path'.*)");
+		// To match this
+		// 	  *EXTRA File 		     2008/02/06 09:31:37	E:\copytest\dst\dir2\deleted.txt
+
+		Modification ParseDeletedFile(
+			string logLine)
+		{
+			Match match = ParseDeletedFileRegex.Match(logLine);
+
+			if (match.Success)
+			{
+				if (match.Groups.Count == 3)
+				{
+					string path = match.Groups["Path"].Captures[0].ToString();
+
+					Modification mod = new Modification();
+						
+					mod.Type = "deleted";
+
+					mod.FileName = Path.GetFileName(path);
+					mod.FolderName = Path.GetDirectoryName(path);
+
+					return mod;
+				}
+			}
+
+			throw new Exception("Failed to match regex");
+		}
+
+		private static readonly Regex ParseAddedFileRegex = new Regex(@"\s+New File\s+(?'Date'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2})\s+(?'Path'.*)");
+
+		// To match this
+		// 	    New File  		 2008/02/06 09:16:49	E:\copytest\src\file2.txt
+
+		Modification ParseAddedFile(
+			string logLine)
+		{
+			Match match = ParseAddedFileRegex.Match(logLine);
+
+			if (match.Success)
+			{
+				if (match.Groups.Count == 3)
+				{
+					string date = match.Groups["Date"].Captures[0].ToString();
+					string path = match.Groups["Path"].Captures[0].ToString();
+
+					Modification mod = new Modification();
+
+					mod.Type = "added";
+
+					mod.FileName = Path.GetFileName(path);
+					mod.FolderName = Path.GetDirectoryName(path);
+
+					mod.ModifiedTime = CreateDate(date);
+
+					return mod;
+				}
+			}
+
+			throw new Exception("Failed to match regex");
+		}
+
+		private static readonly Regex ParseUpdatedFileRegex = new Regex(@"\s+Newer\s+(?'Date'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2})\s+(?'Path'.*)");
+
+		// To match this
+		// 	    Newer     		 2008/02/06 09:35:50	E:\copytest\src\file3.txt
+
+		Modification ParseUpdatedFile(
+			string logLine)
+		{
+			Match match = ParseUpdatedFileRegex.Match(logLine);
+
+			if (match.Success)
+			{
+				if (match.Groups.Count == 3)
+				{
+					string date = match.Groups["Date"].Captures[0].ToString();
+					string path = match.Groups["Path"].Captures[0].ToString();
+
+					Modification mod = new Modification();
+
+					mod.Type = "modified";
+
+					mod.FileName = Path.GetFileName(path);
+					mod.FolderName = Path.GetDirectoryName(path);
+
+					mod.ModifiedTime = CreateDate(date);
+
+					return mod;
+				}
+			}
+
+			throw new Exception("Failed to match regex");
+		}
+
+		private DateTime CreateDate(
+			string dateTimeString)
+		{
+			DateTime date = DateTime.ParseExact(dateTimeString, "yyyy/MM/dd HH:mm:ss", DateTimeFormatInfo.GetInstance(CultureInfo.InvariantCulture));
+
+			return date;
+		}
+	}
+}
---
+++ project/core/sourcecontrol/RobocopySourceControl.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.IO;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
+{
+	[ReflectorType("robocopy")]
+	public class RobocopySourceControl : ProcessSourceControl
+	{
+		private static int[] GenerateExitCodes()
+		{
+			int[] exitCodes = new int[4];
+
+			exitCodes[0] = 0;			// All OK, nothing to do
+			exitCodes[1] = 1;			// Some files copied
+			exitCodes[2] = 2;			// Some extra files in destination tree
+			exitCodes[3] = 3;			// Copied and some extra files in destination tree
+
+			// Note that we COULD want to have 4-7 as valid success codes, but i've not been 
+			// able to cause them to occur yet and so havent included them here.
+			
+			return exitCodes;
+		}
+
+		private static readonly int[] successExitCodes = GenerateExitCodes();
+
+		public RobocopySourceControl() : this(new RobocopyHistoryParser(), new ProcessExecutor())
+		{}
+
+		public RobocopySourceControl(IHistoryParser parser, ProcessExecutor executor) : base(parser, executor)
+		{}
+
+		[ReflectorProperty("executable", Required = false)]
+		public string Executable = "C:\\Windows\\System32\\robocopy.exe";	 
+
+		[ReflectorProperty("repositoryRoot")]
+		public string RepositoryRoot;
+
+		[ReflectorProperty("autoGetSource", Required = false)]
+		public bool AutoGetSource = false;
+
+		[ReflectorProperty("workingDirectory", Required = false)]
+		public string WorkingDirectory = string.Empty;
+
+		[ReflectorProperty("additionalArguments", Required = false)]
+		public string AdditionalArguments = string.Empty;
+
+		public override Modification[] GetModifications(IIntegrationResult from, IIntegrationResult to)
+		{
+			string destinationDirectory = from.BaseFromWorkingDirectory(WorkingDirectory);
+
+			ProcessArgumentBuilder builder = new ProcessArgumentBuilder();
+
+			AddStandardArguments(builder, destinationDirectory);
+
+			builder.AddArgument("/L");
+
+			Modification[] modifications = GetModifications(new ProcessInfo(Executable, builder.ToString(), null, successExitCodes), from.StartTime, to.StartTime);
+
+			return modifications;
+		}
+
+		public override void LabelSourceControl(IIntegrationResult result)
+		{}
+
+		public override void GetSource(IIntegrationResult result)
+		{
+			if (AutoGetSource)
+			{
+				string destinationDirectory = result.BaseFromWorkingDirectory(WorkingDirectory);
+		
+				ProcessArgumentBuilder builder = new ProcessArgumentBuilder();
+
+				AddStandardArguments(builder, destinationDirectory);
+
+				Execute(new ProcessInfo(Executable, builder.ToString(), null, successExitCodes));
+			}
+		}
+
+		// /MIR - MIRror a directory tree (equivalent to /E plus /PURGE).
+		// /NP	- No Progress - don't display % copied.
+		// /X	- Report all eXtra files, not just those selected.
+		// /TS	- Include source file Time Stamps in the output.
+		// /FP	- Include Full Pathname of files in the output.
+		// /NDL	- No Directory List - don't log directory names.
+		// /NS	- No Size - don't log file sizes.
+		// /NJH	- No Job Header.
+		// /NJS	- No Job Summary.
+
+		private readonly static string standardArguments = " /MIR /NP /X /TS /FP /NDL /NS /NJH /NJS ";
+
+		private void AddStandardArguments(
+			ProcessArgumentBuilder builder,
+			string destinationDirectory)
+		{
+			builder.AddArgument(RepositoryRoot);
+			builder.AddArgument(destinationDirectory);
+			builder.Append(standardArguments);
+			builder.Append(AdditionalArguments);
+		}
+	}
+}
