aed43107e0590d0add512d8f30f6d416f8881f27
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-06-13 10:37:42 +0000

    Expanded the tests to also cover default values.

--- project/UnitTests/Core/Tasks/ReplacementDynamicValueTests.cs
+++ project/UnitTests/Core/Tasks/ReplacementDynamicValueTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using ThoughtWorks.CruiseControl.Core.Tasks;
 using ThoughtWorks.CruiseControl.Remote;
+using ThoughtWorks.CruiseControl.Remote.Parameters;
 
 namespace ThoughtWorks.CruiseControl.UnitTests.Core.Tasks
 {
@@ -19,18 +20,25 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Tasks
             Assert.AreEqual("test parameter", value.FormatValue, "FormatValue not being get/set correctly");
             value.PropertyName = "test property";
             Assert.AreEqual("test property", value.PropertyName, "PropertyName not being get/set correctly");
+            var parameters = new NameValuePair[] 
+            {
+                new NameValuePair("name", "value")
+            };
+            value.Parameters = parameters;
+            Assert.AreSame(parameters, value.Parameters);
         }
 
         [Test]
         public void ApplyTo()
         {
             MsBuildTask testTask = new MsBuildTask();
-            ReplacementDynamicValue value = new ReplacementDynamicValue("{0}\\Happy", "workingDirectory", 
-                new NameValuePair("newDir", null));
+            ReplacementDynamicValue value = new ReplacementDynamicValue("{0}\\Happy - {1}", "workingDirectory", 
+                new NameValuePair("newDir", null),
+                new NameValuePair("oldDir", "default"));
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters.Add("newDir", "a location");
             value.ApplyTo(testTask, parameters, null);
-            Assert.AreEqual("a location\\Happy", testTask.WorkingDirectory, "Value has not been correctly set");
+            Assert.AreEqual("a location\\Happy - default", testTask.WorkingDirectory, "Value has not been correctly set");
         }
     }
 }
