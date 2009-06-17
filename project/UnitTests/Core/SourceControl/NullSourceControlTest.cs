d98d2ecd70fe096ea396f60b6d0472ed7229450d
Ruben Willems <ruben.willems@gmail.com>
2009-06-17 11:07:48 +0000

    CCNet-1573 null source control should have settings for simulating failures in the different stages

--- project/UnitTests/Core/SourceControl/NullSourceControlTest.cs
+++ project/UnitTests/Core/SourceControl/NullSourceControlTest.cs
@@ -29,5 +29,28 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Sourcecontrol
 			sourceControl.Purge(null);
 			sourceControl.LabelSourceControl(null);
 		}
-	}
+
+        [Test, ExpectedException("System.Exception", "Failing GetModifications")]
+        public void ShouldFailGetModsWhenFailModsIsTrue()
+        {
+            sourceControl.FailGetModifications = true;
+            sourceControl.GetModifications(null,null);
+        }
+
+        [Test, ExpectedException("System.Exception", "Failing getting the source")]
+        public void ShouldFailGetSourceWhenFailGetSourceIsTrue()
+        {
+            sourceControl.FailGetSource = true;
+            sourceControl.GetSource(null);
+        }
+
+        [Test, ExpectedException("System.Exception", "Failing label source control")]
+        public void ShouldFailLabelSourceWhenFailLabelSourceIsTrue()
+        {
+            sourceControl.FailLabelSourceControl = true;
+            sourceControl.LabelSourceControl(null);
+        }
+  
+    }
+
 }
--- project/core/sourcecontrol/NullSourceControl.cs
+++ project/core/sourcecontrol/NullSourceControl.cs
@@ -5,17 +5,44 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
 	[ReflectorType("nullSourceControl")]
 	public class NullSourceControl : ISourceControl
 	{
+
+        [ReflectorProperty("failGetModifications", Required = false)]
+        public bool FailGetModifications = false;
+
+        [ReflectorProperty("failLabelSourceControl", Required = false)]
+        public bool FailLabelSourceControl = false;
+
+        [ReflectorProperty("failGetSource", Required = false)]
+        public bool FailGetSource = false;
+
+
+
 		public Modification[] GetModifications(IIntegrationResult from, IIntegrationResult to)
 		{
-			return new Modification[0];
+            if (FailGetModifications)
+            {
+                throw new System.Exception("Failing GetModifications");
+            }
+            else
+            {
+                return new Modification[0];
+            }
 		}
 
 		public void LabelSourceControl(IIntegrationResult result) 
 		{
+            if (FailLabelSourceControl)
+            {
+                throw new System.Exception("Failing label source control");
+            }
 		}
 
 		public void GetSource(IIntegrationResult result)
 		{
+            if (FailGetSource)
+            {
+                throw new System.Exception("Failing getting the source");
+            }
 			
 		}
 
