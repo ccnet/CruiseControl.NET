5de4a6a511391fb6dd8c737fd3ce83788bb765ae
Ruben Willems <ruben.willems@gmail.com>
2009-06-24 11:52:58 +0000

    CCNET-1575 - add debugging

--- project/core/publishers/BuildPublisher.cs
+++ project/core/publishers/BuildPublisher.cs
@@ -100,6 +100,8 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
         private void KeepLastXSubDirs(string targetFolder, int amountToKeep, string buildLogDirectory)
         {
+            Util.Log.Debug("Deleting Subdirs of {0}",targetFolder);
+
             System.Collections.Generic.List<string> sortNames = new System.Collections.Generic.List<string>();
             const string dateFormat = "yyyyMMddHHmmssffffff";
 
@@ -121,6 +123,8 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
         private void DeleteSubDirsOlderThanXDays(string targetFolder, int daysToKeep, string buildLogDirectory)
         {
+            Util.Log.Debug("Deleting Subdirs of {0}", targetFolder);
+
             System.DateTime cutoffDate = System.DateTime.Now.Date.AddDays(-daysToKeep);
 
             foreach (string folder in Directory.GetDirectories(targetFolder))
@@ -133,6 +137,8 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
         private void DeleteFolder(string folderName)
         {
+            Util.Log.Debug("    Deleting {0}", folderName);
+
             SetFilesToNormalAttributeAndDelete(folderName);
             Directory.Delete(folderName);
         }
