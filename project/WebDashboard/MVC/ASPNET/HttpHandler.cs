783b594e12ecf4a1cfb38b247525faa93e92fc95
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-06-25 03:13:20 +0000

    Removed the automatic migration - this will now be done by a separate migration tool (in development).

--- project/WebDashboard/MVC/ASPNET/HttpHandler.cs
+++ project/WebDashboard/MVC/ASPNET/HttpHandler.cs
@@ -11,39 +11,20 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.MVC.ASPNET
 	public class HttpHandler : IHttpHandler
 	{
 		private const string RESOLVED_TYPE_MAP = "ResolvedTypeMap";
-		
-		public void ProcessRequest(HttpContext context)
-		{
-            // The following check has been added to handle conversion from 1.4.4 to 1.5.0
-            // In 1.5.0, the configuration file has been moved from the program files folder to program data
-            var configPath = ProgramDataFolder.MapPath("dashboard.config");
-            if (!File.Exists(configPath))
-            {
-                var file = context.Server.MapPath("~/DataMigration.htm");
-                if ((context.Request.HttpMethod == "POST") &&
-                    (context.Request.Form["Upgrade"] == "Yes"))
-                {
-                    if (!Directory.Exists(ProgramDataFolder.Location)) Directory.CreateDirectory(ProgramDataFolder.Location);
-                    File.Copy(context.Server.MapPath("dashboard.config"), configPath);
-                    file = context.Server.MapPath("~/DataMigrationDone.htm");
-                }
-                context.Response.WriteFile(file);
-            }
-            else
-            {
-                ObjectionStore objectionStore = new ObjectionStore(
-                    new CachingImplementationResolver(new NMockAwareImplementationResolver(), new CachedTypeMap(context.Cache, RESOLVED_TYPE_MAP)),
-                    new MaxLengthConstructorSelectionStrategy());
-                ObjectSource objectSource = new CruiseObjectSourceInitializer(objectionStore).SetupObjectSourceForRequest(context);
 
-                context.Response.AppendHeader("X-CCNet-Version",
-                    string.Format("CruiseControl.NET/{0}", Assembly.GetExecutingAssembly().GetName().Version));
-                Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public void ProcessRequest(HttpContext context)
+        {
+            ObjectionStore objectionStore = new ObjectionStore(
+                new CachingImplementationResolver(new NMockAwareImplementationResolver(), new CachedTypeMap(context.Cache, RESOLVED_TYPE_MAP)),
+                new MaxLengthConstructorSelectionStrategy());
+            ObjectSource objectSource = new CruiseObjectSourceInitializer(objectionStore).SetupObjectSourceForRequest(context);
 
-                IResponse response = ((RequestController)objectSource.GetByType(typeof(RequestController))).Do();
-                response.Process(context.Response);
-            }
-            context.Response.Flush();
+            context.Response.AppendHeader("X-CCNet-Version",
+                string.Format("CruiseControl.NET/{0}", Assembly.GetExecutingAssembly().GetName().Version));
+            Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            IResponse response = ((RequestController)objectSource.GetByType(typeof(RequestController))).Do();
+            response.Process(context.Response);
         }
 
 		public bool IsReusable
