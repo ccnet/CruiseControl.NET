44004ec2fc388fdd83dbdd30af7f10bfe7ba4cad
Ruben Willems <ruben.willems@gmail.com>
2009-10-09 17:54:30 +0000

    CCNet-1603 SECURITY: LDAP user does not work.

---
+++ project/core/util/LdapUserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThoughtWorks.CruiseControl.Core.Util
+{
+    /// <summary>
+    /// Information retrieved about a user from the LDAP service
+    /// </summary>
+    public class LdapUserInfo
+    {
+        public string MailAddress {get;set;}
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string CommonName { get; set; }
+        public string GivenName { get; set; }
+        public string DisplayName { get; set; }
+        public string MailNickName { get; set; }
+
+    }
+}
