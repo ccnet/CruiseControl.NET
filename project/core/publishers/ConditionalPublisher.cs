42eeaf2bf546cdb09c8c7917406fc223baf220cd
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-10-16 20:51:41 +0000

    CCNET-1723: Fix bug in the conditional publisher (checking Conditions instead of Tasks for the loop length) - patch by Mike Jansen.

--- project/core/publishers/ConditionalPublisher.cs
+++ project/core/publishers/ConditionalPublisher.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Exortech.NetReflector;
-using ThoughtWorks.CruiseControl.Core.Util;
-using ThoughtWorks.CruiseControl.Remote;
-using ThoughtWorks.CruiseControl.Core.Tasks;
-using ThoughtWorks.CruiseControl.Remote.Parameters;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ConditionalPublisher.cs" company="CruiseControl.NET">
+//     Copyright (c) 2009 CruiseControl.NET. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace ThoughtWorks.CruiseControl.Core.Publishers
 {
+    using System;
+    using Exortech.NetReflector;
+    using ThoughtWorks.CruiseControl.Core.Tasks;
+    using ThoughtWorks.CruiseControl.Core.Util;
+    using ThoughtWorks.CruiseControl.Remote;
+
     /// <summary>
     /// A container publisher that only executes the child publishers when the 
     /// condition (e.g. build status) is met.
@@ -20,7 +23,7 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
         #region Public properties
         #region Publishers
         /// <summary>
-        /// The publishers to run if the condition is met.
+        /// Gets or sets the publishers to run if the condition is met.
         /// </summary>
         [ReflectorProperty("publishers")]
         public override ITask[] Tasks
@@ -32,7 +35,7 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
         #region Conditions
         /// <summary>
-        /// The conditions to test for.
+        /// Gets or sets the conditions to test for.
         /// </summary>
         [ReflectorProperty("conditions", Required = true)]
         public IntegrationStatus[] Conditions { get; set; }
@@ -40,7 +43,7 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
         #region Logger
         /// <summary>
-        /// The logger to use.
+        /// Gets or sets the logger to use.
         /// </summary>
         public ILogger Logger { get; set; }
         #endregion
@@ -51,11 +54,12 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
         /// <summary>
         /// Runs the task, given the specified <see cref="IIntegrationResult"/>, in the specified <see cref="IProject"/>.
         /// </summary>
-        /// <param name="result"></param>
+        /// <param name="result">The results of the current build.</param>
+        /// <returns><c>true</c> if the execution is successful; <c>false</c> otherwise.</returns>
         protected override bool Execute(IIntegrationResult result)
         {
             // Initialise the publisher
-            var logger = Logger ?? new DefaultLogger();
+            var logger = this.Logger ?? new DefaultLogger();
             result.BuildProgressInformation.SignalStartRunTask(!string.IsNullOrEmpty(Description)
                 ? Description
                 : "Checking conditional publishers");
@@ -63,18 +67,18 @@ namespace ThoughtWorks.CruiseControl.Core.Publishers
 
             // Check the conditions
             var runPublishers = false;
-            foreach (var condition in Conditions ?? new IntegrationStatus[0])
+            foreach (var condition in this.Conditions ?? new IntegrationStatus[0])
             {
-                runPublishers |= (condition == result.Status);
+                runPublishers |= condition == result.Status;
             }
 
             if (runPublishers)
             {
                 // Run each publisher
                 logger.Info("Conditions met - running publishers");
-                for (var loop = 0; loop < Conditions.Length; loop++)
+                for (var loop = 0; loop < this.Tasks.Length; loop++)
                 {
-                    var publisher = Tasks[loop];
+                    var publisher = this.Tasks[loop];
                     logger.Debug("Running publisher #{0}", loop);
                     try
                     {
