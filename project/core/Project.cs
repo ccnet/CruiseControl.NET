2a3bd04651b086ca1887aac293987337c90ec30c
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-10-22 18:41:05 +0000

    Fixed a bug where the parameters were not being passed to prebuild or publisher tasks.

--- project/core/Project.cs
+++ project/core/Project.cs
@@ -432,7 +432,9 @@ namespace ThoughtWorks.CruiseControl.Core
 
         public void Prebuild(IIntegrationResult result)
         {
-            Prebuild(result, new Dictionary<string, string>());
+            var parameters = new Dictionary<string, string>();
+            if (result.Parameters != null) parameters = NameValuePair.ToDictionary(result.Parameters);
+            Prebuild(result, parameters);
         }
 
         public void Prebuild(IIntegrationResult result, Dictionary<string, string> parameterValues)
@@ -501,7 +503,9 @@ namespace ThoughtWorks.CruiseControl.Core
 
         public void PublishResults(IIntegrationResult result)
         {
-            PublishResults(result, new Dictionary<string, string>());
+            var parameters = new Dictionary<string, string>();
+            if (result.Parameters != null) parameters = NameValuePair.ToDictionary(result.Parameters);
+            PublishResults(result, parameters);
         }
 
 		public void PublishResults(IIntegrationResult result, Dictionary<string, string> parameterValues)
