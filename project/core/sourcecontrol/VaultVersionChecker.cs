d51c6e85f915c8efdf4d766aa099954050f2a506
Craig Sutherland <craig.j.sutherland@gmail.com>
2009-12-17 21:49:36 +0000

    Documentation and XSD for Vault.

--- project/core/sourcecontrol/VaultVersionChecker.cs
+++ project/core/sourcecontrol/VaultVersionChecker.cs
@@ -5,7 +5,111 @@ using ThoughtWorks.CruiseControl.Core.Util;
 
 namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol {
 
-	[ReflectorType("vault")]
+    /// <summary>
+    /// SourceGear Vault Source Control Block.
+    /// </summary>
+    /// <title>SourceGear Vault Source Control Block</title>
+    /// <version>1.0</version>
+    /// <key name="type">
+    /// <description>The type of source control block.</description>
+    /// <value>vault</value>
+    /// </key>
+    /// <example>
+    /// <code title="Minimal example">
+    /// &lt;sourcecontrol type="vault" /&gt;
+    /// </code>
+    /// <code title="Full example">
+    /// &lt;sourcecontrol type="vault" autoGetSource="true" applyLabel="true"&gt;
+    /// &lt;executable&gt;c:\program files\sourcegear\vault client\vault.exe&lt;/executable&gt;
+    /// &lt;username&gt;my_username&lt;/username&gt;
+    /// &lt;password&gt;my_password&lt;/password&gt;
+    /// &lt;host&gt;my_buildserver&lt;/host&gt;
+    /// &lt;repository&gt;my_repository&lt;/repository&gt;
+    /// &lt;folder&gt;$&lt;/folder&gt;
+    /// &lt;ssl&gt;true&lt;/ssl&gt;
+    /// &lt;timeout units="minutes"&gt;10&lt;/timeout&gt;
+    /// &lt;useWorkingDirectory&gt;true&lt;/useWorkingDirectory&gt;
+    /// &lt;workingDirectory&gt;project/src&lt;/workingDirectory&gt;
+    /// &lt;/sourcecontrol&gt;
+    /// </code>
+    /// </example>
+    /// <remarks>
+    /// <heading>Plugin available for Vault 4.1+ (or Fortress 1.1+)</heading>
+    /// <para>
+    /// SourceGear has released a plugin that offers better performance and accuracy by interacting directly with Vault
+    /// via its API, rather than the command line. The configuration format is almost identical to this one, making
+    /// migration easy. The plugin and its documentation can be downloaded from SourceGear's site
+    /// (http://www.sourcegear.com/downloads.html).
+    /// </para>
+    /// <heading>Vault Working Folder Defined</heading>
+    /// <para>
+    /// Most version control systems have distinct commands for "get me the source" and "get me the source into a folder
+    /// where I may make changes." Vault is no exception. A working folder is a folder where Vault will keep track of
+    /// your changes. If you're using CC.NET 1.1.0.2172+, the useWorkingFolder setting determines whether Vault
+    /// retrieves source into a working folder. For build purposes, there are typically two situations where you want to
+    /// retrieve source into a working folder:
+    /// </para>
+    /// <para>
+    /// 1. Your build script changes source and checks in the change
+    /// </para>
+    /// <para>
+    /// 2. It's taking longer than you'd like to retrieve the source
+    /// </para>
+    /// <para>
+    /// Because of the additional state information kept by Vault for working folders, retrieving source into a working
+    /// folder is usually faster than into a non-working folder. The trade-off is that more disk space will be used for
+    /// cache and state data.
+    /// </para>
+    /// <heading>Filtering out Label Changes</heading>
+    /// <para>
+    /// If you are using Vault 3.x or later, labels will automatically be filtered. However, if you are using an earlier
+    /// version of Vault and you apply a label as part of your build process, this will kick off another build. you will
+    /// need to use a <link>Filtered Source Control Block</link> to get around this. If your build server uses a
+    /// specific user id to integrate with Vault, you can set up a UserFilter to filter out all changes made by that 
+    /// user.
+    /// </para>
+    /// <heading>Problems with CCService and Vault 3.0.2+</heading>
+    /// <para>
+    /// If you are experiencing problems detecting modifications using CCService after upgrading to Vault 3.0.2, it may
+    /// be related to the enhanced security features of the Vault server. You should try the following process to fix
+    /// this issue:
+    /// </para>
+    /// <para>
+    /// 1. reinstall the Vault Server to have the IIS Process Model run as System (ie. not Machine)
+    /// </para>
+    /// <para>
+    /// 2. change the CCService to run as an actual user, not as LocalSystem.
+    /// </para>
+    /// <heading>Problems with releases prior to Vault 3.0</heading>
+    /// <para>
+    /// For versions of Vault prior to 3.0, the -excludeactions argument is not supported. To get around this problem
+    /// you should explicitly specify the &lt;historyArgs&gt; element in your ccnet.config file so that it does not
+    /// contain that argument (ie. set it to &lt;historyArgs&gt;-rowlimit 0&lt;/historyArgs&gt;.
+    /// </para>
+    /// <heading>NAnt Vault Tasks</heading>
+    /// <para>
+    /// SourceGear has produced some NAnt tasks for Vault that can be downloaded from
+    /// http://www.sourcegear.com/vault/downloads.html.
+    /// </para>
+    /// <heading>Turning off the creation of _sgbak folders</heading>
+    /// <para>
+    /// Using CC.NET 1.1.0.2172+ and Vault 3.1.7+, _sgbak folders are never created.
+    /// </para>
+    /// <para>
+    /// Using older versions, the use of the _sgbak folder can be turned off using the GUI client. This is a user and
+    /// machine-specific setting, so you need to launch the GUI client on the CCNet machine and log in as the same Vault
+    /// user that CCNet is using (as specified it ccnet.config).
+    /// </para>
+    /// <para>
+    /// Tools|Options -&gt; Local Files -&gt; Cache/Backup Locations -&gt; Un-check "Save files in backup folder before
+    /// overwriting"
+    /// </para>
+    /// <heading>Contributions</heading>
+    /// <para>
+    /// Code contributed by Ryan Duffield, Leo von Wyss and Ian Olsen.
+    /// </para>
+    /// </remarks>
+    [ReflectorType("vault")]
 	public class VaultVersionChecker 
         : SourceControlBase
 	{
@@ -28,38 +132,92 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol {
 		private Vault3 _vaultSourceControl = null;
 		private EForcedVaultVersion _forcedVaultVersion = EForcedVaultVersion.None;
 
-
+        /// <summary>
+        /// Vault user id that CCNet should use to authenticate.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
 		[ReflectorProperty("username", Required=false)]
 		public string Username;
 
-		[ReflectorProperty("password", Required=false)]
+        /// <summary>
+        /// Password for the Vault user.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("password", Required = false)]
 		public string Password;
 
-		[ReflectorProperty("host", Required=false)]
+        /// <summary>
+        /// The name of the Vault server.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("host", Required = false)]
 		public string Host;
 
-		[ReflectorProperty("repository", Required=false)]
+        /// <summary>
+        /// The name of the Vault repository to monitor. .
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("repository", Required = false)]
 		public string Repository;
 
-		[ReflectorProperty("folder", Required=false)]
+        /// <summary>
+        /// The root folder to be monitored by CCNet.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>$</default>
+        [ReflectorProperty("folder", Required = false)]
 		public string Folder = DefaultFolder;
 
-		[ReflectorProperty("executable", Required=false)]
+        /// <summary>
+        /// The location of the Vault command-line executable.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>C:\Program Files\SourceGear\Vault Client\vault.exe</default>
+        [ReflectorProperty("executable", Required = false)]
 		public string Executable = DefaultExecutable;
 
-		[ReflectorProperty("ssl", Required=false)]
+        /// <summary>
+        /// Should SSL be used to communicate with the Vault server.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>false</default>
+        [ReflectorProperty("ssl", Required = false)]
 		public bool Ssl = false;
 
-		[ReflectorProperty("autoGetSource", Required=false)]
+        /// <summary>
+        /// Specifies if CCNet should automatically retrieve the latest version of the source from the repository.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>true</default>
+        [ReflectorProperty("autoGetSource", Required = false)]
 		public bool AutoGetSource = true;
 
-		[ReflectorProperty("applyLabel", Required=false)]
+        /// <summary>
+        /// Specifies if CCNet should apply the build label to the repository.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>false</default>
+        [ReflectorProperty("applyLabel", Required = false)]
 		public bool ApplyLabel = false;
 
-		[ReflectorProperty("historyArgs", Required=false)]
+        /// <summary>
+        /// Extra arguments to be included in the history commandline.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>-excludeactions label,obliterate -rowlimit 0</default>
+        [ReflectorProperty("historyArgs", Required = false)]
 		public string HistoryArgs = DefaultHistoryArgs;
 
-		[ReflectorProperty("timeout", typeof(TimeoutSerializerFactory), Required = false)]
+        /// <summary>
+        /// Sets the timeout period for the source control operation.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>10 minutes</default>
+        [ReflectorProperty("timeout", typeof(TimeoutSerializerFactory), Required = false)]
 		public Timeout Timeout
 		{
 			get
@@ -76,43 +234,117 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol {
 			
 		}
 
-		[ReflectorProperty("useWorkingDirectory", Required=false)]
+        /// <summary>
+        /// <b>CC.NET 1.0</b>: Determines the working directory into which Vault files will be retrieved. Supply true if
+        /// you want CCNet to use the Vault folder working directory created for the build user using the Vault GUI
+        /// (recommended). Supply false if CCNet should use the CCNet working directory.
+        /// <b>CC.NET 1.1</b>: Determines if the source will be retrieved into a Vault Working Folder. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>true</default>
+        [ReflectorProperty("useWorkingDirectory", Required = false)]
 		public bool UseVaultWorkingDirectory = true;
 
-		[ReflectorProperty("workingDirectory", Required=false)]
+        /// <summary>
+        /// The root folder where the latest source will retrieved from Vault. This path can either be absolute or it
+        /// can be relative to the CCNet project working directory.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>Project Working Directory</default>
+        /// <remarks>
+        /// <b>CC.NET 1.1</b>: If this element is missing or empty, Vault will attempt to use the directory set as the
+        /// user's working folder. Note that this is simply the destination path on disk. Whether or not this location
+        /// is a Vault Working Folder is determined by the useWorkingFolder element. To use the same path as the 
+        /// project, it is necessary to use "." (without the quotes) rather than leaving this empty, as you could in 
+        /// CC.NET 1.0.
+        /// </remarks>
+        [ReflectorProperty("workingDirectory", Required = false)]
 		public string WorkingDirectory;
 
-		// A more sensible default here would be "current" IMHO, but "checkin" was hard-coded before the property was added, 
-		// so I'm leaving that as the default so the behavior doesn't change on people when they upgrade.
-		[ReflectorProperty("setFileTime", Required=false)]
-		public string setFileTime = DefaultFileTime; 
+        /// <summary>
+        /// The modification date that retrieved source files will have. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>checkin</default>
+        /// <remarks>
+        /// Must be one of:
+        /// *  checkin - the date/time the file was checked in
+        /// * current - the date/time the file was retrieved from Vault
+        /// * modification - the date/time the file was last modified
+        /// </remarks>
+        [ReflectorProperty("setFileTime", Required = false)]
+		public string setFileTime = DefaultFileTime;
 
-		[ReflectorProperty("cleanCopy", Required = false)]
+        /// <summary>
+        /// If true, the source path will be emptied before retrieving new source. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>false</default>
+        [ReflectorProperty("cleanCopy", Required = false)]
 		public bool CleanCopy = false;
 
-		[ReflectorProperty("proxyServer", Required=false)]
+        /// <summary>
+        /// The host name of the HTTP proxy Vault should use. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("proxyServer", Required = false)]
 		public string proxyServer;
 
-		[ReflectorProperty("proxyPort", Required=false)]
+        /// <summary>
+        /// The port on the HTTP proxy Vault should use. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("proxyPort", Required = false)]
 		public string proxyPort;
 
-		[ReflectorProperty("proxyUser", Required=false)]
+        /// <summary>
+        /// The user name for the HTTP proxy Vault should use. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("proxyUser", Required = false)]
 		public string proxyUser;
 
-		[ReflectorProperty("proxyPassword", Required=false)]
+        /// <summary>
+        /// The password for the HTTP proxy Vault should use. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("proxyPassword", Required = false)]
 		public string proxyPassword;
 
-		[ReflectorProperty("proxyDomain", Required=false)]
+        /// <summary>
+        /// The Windows domain of the HTTP proxy Vault should use.  
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("proxyDomain", Required = false)]
 		public string proxyDomain;
 
-		// Anything provided here will be included in every vault command
-		[ReflectorProperty("otherVaultArguments", Required=false)]
+        /// <summary>
+        /// Any other aruuments to pass into the executable.
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>None</default>
+        [ReflectorProperty("otherVaultArguments", Required = false)]
 		public string otherVaultArguments;
 
-		[ReflectorProperty("pollRetryWait", Required=false)]
+        /// <summary>
+        /// The number of seconds to wait between retries when a check for modifications fails. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>5</default>
+        [ReflectorProperty("pollRetryWait", Required = false)]
 		public int pollRetryWait = DefaultPollRetryWait;
 
-		[ReflectorProperty("pollRetryAttempts", Required=false)]
+        /// <summary>
+        /// The number of automatic retries when failing to check for modifications before an exception is thrown. 
+        /// </summary>
+        /// <version>1.0</version>
+        /// <default>5</default>
+        [ReflectorProperty("pollRetryAttempts", Required = false)]
 		public int pollRetryAttempts = DefaultPollRetryAttempts;
 
 		public Vault3 VaultSourceControl
