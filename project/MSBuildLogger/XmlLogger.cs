8288ae149086744e923b56de8a91bbe4d1f60c14
Ruben Willems <ruben.willems@gmail.com>
2009-12-29 09:02:56 +0000

    CCNET-1776 Timestamps in msbuild logger

--- project/MSBuildLogger/XmlLogger.cs
+++ project/MSBuildLogger/XmlLogger.cs
@@ -12,33 +12,33 @@ using System.Xml;
 
 namespace ThoughtWorks.CruiseControl.MSBuild
 {
-	/// <summary>
-	/// Implements an XML logger for MSBuild.
-	/// </summary>
-	public class XmlLogger : Logger
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="XmlLogger"/> class.
-		/// </summary>
-		public XmlLogger()
-		{}
+    /// <summary>
+    /// Implements an XML logger for MSBuild.
+    /// </summary>
+    public class XmlLogger : Logger
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlLogger"/> class.
+        /// </summary>
+        public XmlLogger()
+        { }
 
         private string outputPath;
-		private XmlDocument outputDoc;
-		private XmlElement currentElement;
+        private XmlDocument outputDoc;
+        private XmlElement currentElement;
 
-		/// <summary>
-		/// Initializes the logger by attaching events and parsing command line.
-		/// </summary>
-		/// <param name="eventSource">The event source.</param>
-		public override void Initialize(IEventSource eventSource)
-		{
+        /// <summary>
+        /// Initializes the logger by attaching events and parsing command line.
+        /// </summary>
+        /// <param name="eventSource">The event source.</param>
+        public override void Initialize(IEventSource eventSource)
+        {
             outputPath = this.Parameters;
 
-			this.outputDoc = new XmlDocument();
-			this.outputDoc.AppendChild(this.outputDoc.CreateXmlDeclaration("1.0", "utf-8", "yes"));
-			this.outputDoc.AppendChild(this.outputDoc.CreateElement(XmlLoggerElements.Build));
-			this.currentElement = this.outputDoc.DocumentElement;
+            this.outputDoc = new XmlDocument();
+            this.outputDoc.AppendChild(this.outputDoc.CreateXmlDeclaration("1.0", "utf-8", "yes"));
+            this.outputDoc.AppendChild(this.outputDoc.CreateElement(XmlLoggerElements.Build));
+            this.currentElement = this.outputDoc.DocumentElement;
 
             eventSource.ErrorRaised += new BuildErrorEventHandler(eventSource_ErrorRaised);
             eventSource.WarningRaised += new BuildWarningEventHandler(eventSource_WarningRaised);
@@ -47,7 +47,7 @@ namespace ThoughtWorks.CruiseControl.MSBuild
             eventSource.BuildFinished += new BuildFinishedEventHandler(eventSource_BuildFinishedHandler);
 
             if (Verbosity != LoggerVerbosity.Quiet) // minimal and above
-			{
+            {
                 eventSource.MessageRaised += new BuildMessageEventHandler(eventSource_MessageHandler);
                 eventSource.CustomEventRaised += new CustomBuildEventHandler(eventSource_CustomBuildEventHandler);
 
@@ -55,73 +55,73 @@ namespace ThoughtWorks.CruiseControl.MSBuild
                 eventSource.ProjectFinished += new ProjectFinishedEventHandler(eventSource_ProjectFinishedHandler);
 
                 if (Verbosity != LoggerVerbosity.Minimal) // normal and above
-				{
+                {
                     eventSource.TargetStarted += new TargetStartedEventHandler(eventSource_TargetStartedHandler);
                     eventSource.TargetFinished += new TargetFinishedEventHandler(eventSource_TargetFinishedHandler);
 
                     if (Verbosity != LoggerVerbosity.Normal) // only detailed and diagnostic
-					{
+                    {
                         eventSource.TaskStarted += new TaskStartedEventHandler(eventSource_TaskStartedHandler);
                         eventSource.TaskFinished += new TaskFinishedEventHandler(eventSource_TaskFinishedHandler);
-					}
-				}
-			}
-		}
+                    }
+                }
+            }
+        }
 
-		public override void Shutdown()
-		{
-			if (String.IsNullOrEmpty(this.outputPath))
-			{
-				// Output to console.
-				Console.WriteLine(this.outputDoc.OuterXml);
-			}
-			else
-			{
-				this.outputDoc.Save(this.outputPath);
-			}
-		}
+        public override void Shutdown()
+        {
+            if (String.IsNullOrEmpty(this.outputPath))
+            {
+                // Output to console.
+                Console.WriteLine(this.outputDoc.OuterXml);
+            }
+            else
+            {
+                this.outputDoc.Save(this.outputPath);
+            }
+        }
 
-		#region Event Handlers
+        #region Event Handlers
 
-		private void eventSource_BuildStartedHandler(object sender, BuildStartedEventArgs e)
-		{
-			LogStageStarted(XmlLoggerElements.Build, "", "", e.Timestamp);
-		}
+        private void eventSource_BuildStartedHandler(object sender, BuildStartedEventArgs e)
+        {
+            LogStageStarted(XmlLoggerElements.Build, "", "", e.Timestamp);
+        }
 
-		private void eventSource_BuildFinishedHandler(object sender, BuildFinishedEventArgs e)
-		{
-			LogStageFinished(e.Succeeded, e.Timestamp);
-		}
+        private void eventSource_BuildFinishedHandler(object sender, BuildFinishedEventArgs e)
+        {
+            LogStageFinished(e.Succeeded, e.Timestamp);
+        }
 
-		private void eventSource_ProjectStartedHandler(object sender, ProjectStartedEventArgs e)
-		{
+        private void eventSource_ProjectStartedHandler(object sender, ProjectStartedEventArgs e)
+        {
             LogStageStarted(XmlLoggerElements.Project, e.TargetNames, e.ProjectFile, e.Timestamp);
-		}
+        }
 
-		private void eventSource_ProjectFinishedHandler(object sender, ProjectFinishedEventArgs e)
-		{
-			LogStageFinished(e.Succeeded, e.Timestamp);
-		}
+        private void eventSource_ProjectFinishedHandler(object sender, ProjectFinishedEventArgs e)
+        {
+            LogStageFinished(e.Succeeded, e.Timestamp);
+        }
 
-		private void eventSource_TargetStartedHandler(object sender, TargetStartedEventArgs e)
-		{
+        private void eventSource_TargetStartedHandler(object sender, TargetStartedEventArgs e)
+        {
             LogStageStarted(XmlLoggerElements.Target, e.TargetName, "", e.Timestamp);
-		}
+        }
 
-		private void eventSource_TargetFinishedHandler(object sender, TargetFinishedEventArgs e)
-		{
-			LogStageFinished(e.Succeeded, e.Timestamp);
-		}
+        private void eventSource_TargetFinishedHandler(object sender, TargetFinishedEventArgs e)
+        {
+            LogStageFinished(e.Succeeded, e.Timestamp);
+        }
 
-		private void eventSource_TaskStartedHandler(object sender, TaskStartedEventArgs e)
-		{
+        private void eventSource_TaskStartedHandler(object sender, TaskStartedEventArgs e)
+        {
             LogStageStarted(XmlLoggerElements.Task, e.TaskName, e.ProjectFile, e.Timestamp);
-		}
+        }
 
-		private void eventSource_TaskFinishedHandler(object sender, TaskFinishedEventArgs e)
-		{
-			LogStageFinished(e.Succeeded, e.Timestamp);
-		}
+        private void eventSource_TaskFinishedHandler(object sender, TaskFinishedEventArgs e)
+        {
+            LogStageFinished(e.Succeeded, e.Timestamp);
+        }
 
         void eventSource_WarningRaised(object sender, BuildWarningEventArgs e)
         {
@@ -133,60 +133,54 @@ namespace ThoughtWorks.CruiseControl.MSBuild
             LogErrorOrWarning(XmlLoggerElements.Error, e.Message, e.Code, e.File, e.LineNumber, e.ColumnNumber, e.Timestamp);
         }
 
-		private void eventSource_MessageHandler(object sender, BuildMessageEventArgs e)
-		{
+        private void eventSource_MessageHandler(object sender, BuildMessageEventArgs e)
+        {
             LogMessage(XmlLoggerElements.Message, e.Message, e.Importance, e.Timestamp);
         }
 
-		private void eventSource_CustomBuildEventHandler(object sender, CustomBuildEventArgs e)
-		{
+        private void eventSource_CustomBuildEventHandler(object sender, CustomBuildEventArgs e)
+        {
             LogMessage(XmlLoggerElements.Custom, e.Message, MessageImportance.Normal, e.Timestamp);
-		}
+        }
 
-		#endregion
+        #endregion
 
-		#region Logging
+        #region Logging
 
-		private void LogStageStarted(string elementName, string stageName, string file, DateTime timeStamp)
-		{
-			// use the default root for the build element
-			if (elementName != XmlLoggerElements.Build)
-			{
-				XmlElement stageElement = this.outputDoc.CreateElement(elementName);
-				this.currentElement.AppendChild(stageElement);
-				this.currentElement = stageElement;
-			}
+        private void LogStageStarted(string elementName, string stageName, string file, DateTime timeStamp)
+        {
+            // use the default root for the build element
+            if (elementName != XmlLoggerElements.Build)
+            {
+                XmlElement stageElement = this.outputDoc.CreateElement(elementName);
+                this.currentElement.AppendChild(stageElement);
+                this.currentElement = stageElement;
+            }
 
             SetAttribute(currentElement, stageName, XmlLoggerAttributes.Name);
             SetAttribute(currentElement, file, XmlLoggerAttributes.File);
+            SetAttribute(currentElement, timeStamp, XmlLoggerAttributes.StartTime);
 
-            if (currentElement.Name == XmlLoggerElements.Build || Verbosity == LoggerVerbosity.Diagnostic)
-            {
-                SetAttribute(currentElement, timeStamp, XmlLoggerAttributes.StartTime);
-            }
-		}
+        }
 
-		private void LogStageFinished(bool succeded, DateTime timeStamp)
-		{
-            if (currentElement.Name == XmlLoggerElements.Build || Verbosity == LoggerVerbosity.Diagnostic)
-            {
-                DateTime startTime = DateTime.Parse(currentElement.GetAttribute(XmlLoggerAttributes.StartTime), DateTimeFormatInfo.InvariantInfo);
-                SetAttribute(currentElement, timeStamp - startTime, XmlLoggerAttributes.ElapsedTime);
-            }
-           
+        private void LogStageFinished(bool succeded, DateTime timeStamp)
+        {
+            DateTime startTime = DateTime.Parse(currentElement.GetAttribute(XmlLoggerAttributes.StartTime), DateTimeFormatInfo.InvariantInfo);
+            SetAttribute(currentElement, timeStamp - startTime, XmlLoggerAttributes.ElapsedTime);
+
             SetAttribute(currentElement, succeded, XmlLoggerAttributes.Success);
 
             if (this.currentElement.ParentNode is XmlElement)
             {
                 XmlElement parentElement = (XmlElement)this.currentElement.ParentNode;
-              
+
                 // don't put element's that don't contain any messages
                 if (!currentElement.HasChildNodes && Verbosity != LoggerVerbosity.Detailed && Verbosity != LoggerVerbosity.Diagnostic)
                     parentElement.RemoveChild(currentElement);
 
                 this.currentElement = parentElement;
             }
-		}
+        }
 
         private void LogErrorOrWarning(string messageType, string message, string code, string file, int lineNumber, int columnNumber, DateTime timestamp)
         {
@@ -197,19 +191,18 @@ namespace ThoughtWorks.CruiseControl.MSBuild
             SetAttribute(messageElement, lineNumber, XmlLoggerAttributes.LineNumber);
             SetAttribute(messageElement, columnNumber, XmlLoggerAttributes.ColumnNumber);
 
-            if (Verbosity == LoggerVerbosity.Diagnostic)
-                SetAttribute(messageElement, timestamp, XmlLoggerAttributes.TimeStamp);
+            SetAttribute(messageElement, timestamp, XmlLoggerAttributes.TimeStamp);
 
-             // Escape < and > if this is not a "Properties" message.  This is because in a Properties
-             // message, we want the ability to insert legal XML, but otherwise we can get malformed
-             // XML that will cause the parser to fail.
+            // Escape < and > if this is not a "Properties" message.  This is because in a Properties
+            // message, we want the ability to insert legal XML, but otherwise we can get malformed
+            // XML that will cause the parser to fail.
             WriteMessage(messageElement, message, code != "Properties");
 
             this.currentElement.AppendChild(messageElement);
         }
 
-		private void LogMessage(string messageType, string message, MessageImportance importance, DateTime timestamp)
-		{
+        private void LogMessage(string messageType, string message, MessageImportance importance, DateTime timestamp)
+        {
             if (importance == MessageImportance.Low
                 && Verbosity != LoggerVerbosity.Detailed
                 && Verbosity != LoggerVerbosity.Diagnostic)
@@ -220,17 +213,17 @@ namespace ThoughtWorks.CruiseControl.MSBuild
                     || Verbosity == LoggerVerbosity.Quiet))
                 return;
 
-			XmlElement messageElement = this.outputDoc.CreateElement(messageType);
+            XmlElement messageElement = this.outputDoc.CreateElement(messageType);
 
-			SetAttribute(messageElement, importance, XmlLoggerAttributes.Importance);
+            SetAttribute(messageElement, importance, XmlLoggerAttributes.Importance);
 
             if (Verbosity == LoggerVerbosity.Diagnostic)
-				SetAttribute(messageElement, timestamp, XmlLoggerAttributes.TimeStamp);
+                SetAttribute(messageElement, timestamp, XmlLoggerAttributes.TimeStamp);
 
             WriteMessage(messageElement, message, false);
 
-			this.currentElement.AppendChild(messageElement);
-		}
+            this.currentElement.AppendChild(messageElement);
+        }
 
         private void WriteMessage(XmlElement element, string message, bool escapeLtGt)
         {
@@ -247,24 +240,24 @@ namespace ThoughtWorks.CruiseControl.MSBuild
             }
         }
 
-		private void SetAttribute(XmlElement element, object obj, string name)
-		{
-			if (obj == null)
-				return;
+        private void SetAttribute(XmlElement element, object obj, string name)
+        {
+            if (obj == null)
+                return;
 
-			Type t = obj.GetType();
-			if (t == typeof(int))
-			{
-				if (Int32.Parse(obj.ToString()) > 0)	//????
-				{
-					element.SetAttribute(name, obj.ToString());
-				}
-			}
-			else if (t == typeof(DateTime))
-			{
-				DateTime dateTime = (DateTime)obj;
-				element.SetAttribute(name, dateTime.ToString("G", DateTimeFormatInfo.InvariantInfo));
-			}
+            Type t = obj.GetType();
+            if (t == typeof(int))
+            {
+                if (Int32.Parse(obj.ToString()) > 0)	//????
+                {
+                    element.SetAttribute(name, obj.ToString());
+                }
+            }
+            else if (t == typeof(DateTime))
+            {
+                DateTime dateTime = (DateTime)obj;
+                element.SetAttribute(name, dateTime.ToString("G", DateTimeFormatInfo.InvariantInfo));
+            }
             else if (t == typeof(TimeSpan))
             {
                 // format TimeSpan to show only integral seconds
@@ -274,7 +267,7 @@ namespace ThoughtWorks.CruiseControl.MSBuild
                 //if (rest!=0)
                 //    element.SetAttribute(name, whole.ToString()+"."+rest.ToString(NumberFormatInfo.InvariantInfo));
                 //else
-                    element.SetAttribute(name, whole.ToString());
+                element.SetAttribute(name, whole.ToString());
             }
             else if (t == typeof(Boolean))
             {
@@ -292,47 +285,47 @@ namespace ThoughtWorks.CruiseControl.MSBuild
                     element.SetAttribute(name, obj.ToString());
                 }
             }
-		}
+        }
 
-		#endregion
+        #endregion
 
-		#region Constants
+        #region Constants
 
-		internal sealed class XmlLoggerElements
-		{
-			private XmlLoggerElements()
-			{ }
+        internal sealed class XmlLoggerElements
+        {
+            private XmlLoggerElements()
+            { }
 
-			public const string Build = "msbuild";
-			public const string Error = "error";
-			public const string Warning = "warning";
-			public const string Message = "message";
-			public const string Project = "project";
-			public const string Target = "target";
-			public const string Task = "task";
-			public const string Custom = "custom";
-		}
+            public const string Build = "msbuild";
+            public const string Error = "error";
+            public const string Warning = "warning";
+            public const string Message = "message";
+            public const string Project = "project";
+            public const string Target = "target";
+            public const string Task = "task";
+            public const string Custom = "custom";
+        }
 
-		internal sealed class XmlLoggerAttributes
-		{
-			private XmlLoggerAttributes()
-			{ }
+        internal sealed class XmlLoggerAttributes
+        {
+            private XmlLoggerAttributes()
+            { }
 
-			public const string Name = "name";
-			public const string File = "file";
-			public const string StartTime = "startTime";
+            public const string Name = "name";
+            public const string File = "file";
+            public const string StartTime = "startTime";
             public const string ElapsedTime = "elapsedTime";
-			public const string TimeStamp = "timeStamp";
-			public const string Code = "code";
-			public const string LineNumber = "line";
-			public const string ColumnNumber = "column";
-			public const string Importance = "level";
-			public const string Processor = "processor";
-			public const string HelpKeyword = "help";
-			public const string SubCategory = "category";
-			public const string Success = "success";
-		}
+            public const string TimeStamp = "timeStamp";
+            public const string Code = "code";
+            public const string LineNumber = "line";
+            public const string ColumnNumber = "column";
+            public const string Importance = "level";
+            public const string Processor = "processor";
+            public const string HelpKeyword = "help";
+            public const string SubCategory = "category";
+            public const string Success = "success";
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
