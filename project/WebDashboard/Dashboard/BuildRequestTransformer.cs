4aa509cf96a030dabadfbf1c2d6ef92afc5a8f68
Craig Sutherland <craig.j.sutherland@gmail.com>
2010-01-18 02:40:42 +0000

    Removed the log caching from the dashboard.

--- project/WebDashboard/Dashboard/BuildRequestTransformer.cs
+++ project/WebDashboard/Dashboard/BuildRequestTransformer.cs
@@ -6,6 +6,7 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
     using System.Web.Caching;
     using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
     using ThoughtWorks.CruiseControl.Core.Util;
+    using System.Configuration;
 
     public class BuildRequestTransformer 
         : IBuildLogTransformer
@@ -38,65 +39,8 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
         /// <returns>The transformed content.</returns>
         public string Transform(IBuildSpecifier buildSpecifier, string[] transformerFileNames, Hashtable xsltArgs, string sessionToken)
 		{
-            var log = this.RetrieveLogData(buildSpecifier, sessionToken);
-            return transformer.Transform(log, transformerFileNames, xsltArgs);
-        }
-        #endregion
-        #endregion
-
-        #region Private methods
-        #region RetrieveLogData()
-        /// <summary>
-        /// Retrieves the log data.
-        /// </summary>
-        /// <param name="buildSpecifier">The build specifier to retrieve the log for.</param>
-        /// <param name="sessionToken">The session token.</param>
-        /// <returns>The data for the log.</returns>
-        /// <exception cref="ApplicationException">Thrown if the data for the log could not be retrieved.</exception>
-        private string RetrieveLogData(IBuildSpecifier buildSpecifier, string sessionToken)
-        {
-            var cache = HttpRuntime.Cache;
-
-            // Generate the log and report keys
-            var logKey = buildSpecifier.ProjectSpecifier.ServerSpecifier.ServerName +
-                buildSpecifier.ProjectSpecifier.ProjectName +
-                buildSpecifier.BuildName +
-                (sessionToken ?? string.Empty);
-
-            // Check if the log has already been cached
-            var logData = cache[logKey] as SynchronisedData;
-            if (logData == null)
-            {
-                // Add the new log data and load it
-                logData = new SynchronisedData();
-                cache.Add(
-                    logKey,
-                    logData,
-                    null,
-                    Cache.NoAbsoluteExpiration,
-                    new TimeSpan(1, 0, 0),
-                    CacheItemPriority.AboveNormal,
-                    null);
-                logData.LoadData(() =>
-                {
-                    var buildLog = buildRetriever.GetBuild(buildSpecifier, sessionToken).Log;
-                    return buildLog;
-                });
-            }
-            else
-            {
-                // Wait for the data to load
-                logData.WaitForLoad(10000);
-            }
-
-            // Raise an error if there is no log data
-            if (logData.Data == null)
-            {
-                cache.Remove(logKey);
-                throw new ApplicationException("Unable to retrieve log data");
-            }
-
-            return logData.Data as string;
+            var buildLog = buildRetriever.GetBuild(buildSpecifier, sessionToken).Log;
+            return transformer.Transform(buildLog, transformerFileNames, xsltArgs);
         }
         #endregion
         #endregion
