bede4f20809bb4542afe691e7d7978772496ba3f
Ruben Willems <ruben.willems@gmail.com>
2010-02-26 20:05:45 +0000

    CCNET-1826 : replace default password for admin page with empty one, and check on empty password add test that pasword is empty for distribution

--- project/UnitTests/CCnetDeploymentTests.cs
+++ project/UnitTests/CCnetDeploymentTests.cs
@@ -17,17 +17,18 @@ namespace ThoughtWorks.CruiseControl.UnitTests
         [Test]
         public void TestForAdminPackageOfWebDashboardIsEmpty()
         {
-#if Build
+#if BUILD
             string configFile = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\Webdashboard\dashboard.config");
 #else
             string configFile = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"..\..\..\Webdashboard\dashboard.config");
-#endif            
+#endif           
+
             Assert.IsTrue(System.IO.File.Exists(configFile), "Dashboard.config not found at {0}", configFile);
 
             System.Xml.XmlDocument xdoc = new System.Xml.XmlDocument();
             xdoc.Load(configFile);
 
-            var adminPluginNode =  xdoc.SelectSingleNode("/dashboard/plugins/farmPlugins/administrationPlugin");
+            var adminPluginNode = xdoc.SelectSingleNode("/dashboard/plugins/farmPlugins/administrationPlugin");
 
             Assert.IsNotNull(adminPluginNode, "Admin package configuration not found in dashboard.config at {0}", configFile);
 
