a9d48f2eaac416b5d231804d1d8ac788a5cf654c
Craig Sutherland <craig.j.sutherland@gmail.com>
2010-03-11 20:50:23 +0000

    Added unit tests for CruiseServerControlTask. Expanded CruiseServerControlTask so it can use wildcard mappings for specifying the projects. Added additional logging to CruiseServerControlTask.

--- project/core/tasks/CruiseServerControlTask.cs
+++ project/core/tasks/CruiseServerControlTask.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Exortech.NetReflector;
-using ThoughtWorks.CruiseControl.Remote;
-
-namespace ThoughtWorks.CruiseControl.Core.Tasks
+﻿namespace ThoughtWorks.CruiseControl.Core.Tasks
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Exortech.NetReflector;
+    using ThoughtWorks.CruiseControl.Core.Util;
+    using ThoughtWorks.CruiseControl.Remote;
+    using ThoughtWorks.CruiseControl.Core.Config;
+
     /// <summary>
     /// Sends a management task to a CruiseControl.NET server.
     /// </summary>
@@ -22,8 +24,12 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
     /// </example>
     [ReflectorType("cruiseServerControl")]
     public class CruiseServerControlTask
-        : TaskBase
+        : TaskBase, IConfigurationValidation
     {
+        #region Private fields
+        private List<string> cachedProjects = new List<string>();
+        #endregion
+
         #region Public properties
         #region Server
         /// <summary>
@@ -51,6 +57,32 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
         /// </summary>
         public ICruiseServerClientFactory ClientFactory { get; set; }
         #endregion
+
+        #region Logger
+        /// <summary>
+        /// Gets or sets the logger.
+        /// </summary>
+        /// <value>The logger.</value>
+        public ILogger Logger { get; set; }
+        #endregion
+        #endregion
+
+        #region Public methods
+        #region Validate()
+        /// <summary>
+        /// Checks the internal validation of the item.
+        /// </summary>
+        /// <param name="configuration">The entire configuration.</param>
+        /// <param name="parent">The parent item for the item being validated.</param>
+        /// <param name="errorProcesser">The error processer to use.</param>
+        public virtual void Validate(IConfiguration configuration, object parent, IConfigurationErrorProcesser errorProcesser)
+        {
+            if ((this.Actions == null) || (this.Actions.Length == 0))
+            {
+                errorProcesser.ProcessWarning("This task will not do anything - no actions specified");
+            }
+        }
+        #endregion
         #endregion
 
         #region Protected methods
@@ -62,24 +94,137 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
         /// <returns></returns>
         protected override bool Execute(IIntegrationResult result)
         {
-            if (ClientFactory == null) ClientFactory = new CruiseServerClientFactory();
-            var client = ClientFactory.GenerateClient(Server ?? "tcp://localhost:21234");
+            // Initialise the task
+            var logger = this.Logger ?? new DefaultLogger();
+            var factory = this.ClientFactory ?? new CruiseServerClientFactory();
+            result.BuildProgressInformation.SignalStartRunTask(!string.IsNullOrEmpty(Description)
+                ? Description
+                : "Performing server actions");
+            logger.Info("Performing server actions");
 
-            foreach (var action in Actions)
+            // Initialise the client and cache the project names
+            logger.Debug("Initialising client");
+            var client = factory.GenerateClient(Server ?? "tcp://localhost:21234");
+            this.CacheProjectNames(logger, client);
+
+            // Perform each action
+            var count = 0;
+            foreach (var action in Actions ?? new CruiseServerControlTaskAction[0])
             {
-                // Perform each action
-                switch (action.Type)
+                // Get the projects
+                var projects = this.ListProjects(action.Project);
+                logger.Info("Found " + projects.Count + " project(s) for pattern '" + action.Project + "'");
+
+                // Determine the action to perform
+                Action<string> projectAction = RetrieveAction(logger, client, action);
+
+                // Perform the action on each projec
+                if (projectAction != null)
                 {
-                    case CruiseServerControlTaskActionType.StartProject:
-                        client.StartProject(action.Project);
-                        break;
-                    case CruiseServerControlTaskActionType.StopProject:
-                        client.StopProject(action.Project);
-                        break;
+                    foreach (var project in projects)
+                    {
+                        logger.Debug("Sending action to " + project);
+                        count++;
+                        projectAction(project);
+                    }
+                }
+                else
+                {
+                    throw new CruiseControlException("Unknown action specified: " + action.Type.ToString());
                 }
             }
 
+            logger.Info("Server actions completed: " + count + " command(s) sent");
             return true;
+        }
+        #endregion
+        #endregion
+
+        #region Private methods
+        #region CacheProjectNames()
+        /// <summary>
+        /// Caches the project names.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="client">The client.</param>
+        private void CacheProjectNames(ILogger logger, CruiseServerClientBase client)
+        {
+            logger.Info("Retrieving projects from server");
+            this.cachedProjects.Clear();
+            var serverProjects = client.GetProjectStatus();
+            foreach (var serverProject in serverProjects)
+            {
+                this.cachedProjects.Add(serverProject.Name);
+            }
+
+            logger.Debug(this.cachedProjects.Count + " project(s) retrieved");
+        }
+        #endregion
+
+        #region RetrieveAction()
+        /// <summary>
+        /// Retrieves the action to perform.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="client">The client.</param>
+        /// <param name="action">The action definition.</param>
+        /// <returns>The action to perform.</returns>
+        private static Action<string> RetrieveAction(ILogger logger, CruiseServerClientBase client, CruiseServerControlTaskAction action)
+        {
+            Action<string> projectAction = null;
+            switch (action.Type)
+            {
+                case CruiseServerControlTaskActionType.StartProject:
+                    logger.Info("Performing start project action");
+                    projectAction = p =>
+                    {
+                        client.StartProject(p);
+                    };
+                    break;
+                case CruiseServerControlTaskActionType.StopProject:
+                    logger.Info("Performing stop project action");
+                    projectAction = p =>
+                    {
+                        client.StopProject(p);
+                    };
+                    break;
+            }
+            return projectAction;
+        }
+        #endregion
+
+        #region ListProjects()
+        /// <summary>
+        /// Lists the projects.
+        /// </summary>
+        /// <param name="projectPattern">The project pattern.</param>
+        /// <returns>The project names that match the pattern.</returns>
+        private IList<string> ListProjects(string projectPattern)
+        {
+            var list = new List<string>();
+
+            if (projectPattern.Contains("*") || projectPattern.Contains("?"))
+            {
+                // Check all the projects to see if any match
+                var pattern = projectPattern
+                    .Replace("*", ".*")
+                    .Replace("?", ".");
+                var regex = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                foreach (var project in this.cachedProjects)
+                {
+                    if (regex.IsMatch(project))
+                    {
+                        list.Add(project);
+                    }
+                }
+            }
+            else
+            {
+                // Just add the pattern
+                list.Add(projectPattern);
+            }
+
+            return list;
         }
         #endregion
         #endregion
