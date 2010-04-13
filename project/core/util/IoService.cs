b6dff39ee3d096dadea5d869d4207977cc295caa
Daniel Nauck <d.nauck@nauck-it.de>
2010-04-13 11:04:59 +0000

    CCNET-1861: IOService do not support deleting paths longer then 260 chars
     * use DictionaryInfo.Delete(true)
     * remove recursively ReadOnly file and dictionary attributes
     * catch PathTooLongException and use a fall back method to delete those folders/files on windows: run 'cmd.exe /C RD /S /Q' to delete the path

--- project/core/util/IoService.cs
+++ project/core/util/IoService.cs
@@ -1,69 +1,112 @@
-
+using System;
 using System.IO;
 
 namespace ThoughtWorks.CruiseControl.Core.Util
 {
-	public class IoService : IFileDirectoryDeleter
-	{
-		public void DeleteIncludingReadOnlyObjects(string filename)
-		{
-			DeleteDirectoryEvenIfReadOnly(filename);
-			DeleteFileEvenIfReadOnly(filename);
-		}
+    public class IoService : IFileDirectoryDeleter
+    {
+        private readonly IExecutionEnvironment executionEnvironment = new ExecutionEnvironment();
 
-		/// <summary>
-		/// Deletes all contents of a directory, even those that are read-only, without deleting the directory itself.
-		/// </summary>
-		/// <param name="directoryPath"></param>
-		public void EmptyDirectoryIncludingReadOnlyObjects(string directoryPath)
-		{
-			if (Directory.Exists(directoryPath))
-			{
-				foreach (string subdir in Directory.GetDirectories(directoryPath))
-				{
-					DeleteDirectoryEvenIfReadOnly(Path.Combine(directoryPath, subdir));
-				}
-
-				foreach (string file in Directory.GetFiles(directoryPath))
-				{
-					DeleteFileEvenIfReadOnly(Path.Combine(directoryPath, file));
-				}
-			}
-		}
-
-		private void DeleteDirectoryEvenIfReadOnly(string directoryPath)
-		{
-			if (Directory.Exists(directoryPath))
-			{
-				EmptyDirectoryIncludingReadOnlyObjects(directoryPath);
-				File.SetAttributes(directoryPath, FileAttributes.Normal);
-				Directory.Delete(directoryPath, true);
-			}
-
-		}
-
-		public void DeleteFileEvenIfReadOnly(string filename)
-		{
-			if (File.Exists(filename))
-			{
-                // This try/catch block craziness is to handle symbolic links on Unix based systems.
-                // For Some reason Exists returns true on Symlinks, but SetAttributes throws a 
-                // FileNotFoundException on them (?) as a hack we catch that and still call delete, since
-                // that also actually works. Be great to eliminate setattributes, but we need it for ReadOnly
-                try
+        public void DeleteIncludingReadOnlyObjects(string path)
+        {
+            try
+            {
+                // check whether path is a file or directory
+                if (File.Exists(path))
                 {
-                    File.SetAttributes(filename, FileAttributes.Normal);
-                    File.Delete(filename);
+                    File.SetAttributes(path, FileAttributes.Normal);
+                    File.Delete(path);
                 }
-                catch (System.IO.FileNotFoundException)
+                else if (Directory.Exists(path))
                 {
-                    File.Delete(filename);
+                    var dirInfo = new DirectoryInfo(path);
+                    SetReadOnlyRecursive(dirInfo);
+                    dirInfo.Delete(true);
                 }
-                catch (System.Exception)
+                else
+                {
+                    Log.Warning("[IoService] File or directory not found: '{0}'", path);
+                }
+            }
+            catch (PathTooLongException pathTooLongEx)
+            {
+                Log.Error("[IoService] Unable to delete path '{0}'.{1}{2}", path, Environment.NewLine, pathTooLongEx);
+                
+                if (executionEnvironment.IsRunningOnWindows)
+                {
+                    DeleteDirectoryWithLongPath(path);
+                }
+                else
                 {
                     throw;
-                }                
-			}
-		}
-	}
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a directory on Windows with a commandline call.
+        /// 
+        /// Reason:
+        /// .NET only supports filenames up to 260 characters long for backwards compability
+        /// read more at: http://blogs.msdn.com/bclteam/archive/2007/02/13/long-paths-in-net-part-1-of-3-kim-hamilton.aspx
+        /// this is a Windows only limitation
+        /// </summary>
+        /// <param name="path">Path to delete.</param>
+        static void DeleteDirectoryWithLongPath(string path)
+        {
+            Log.Info("[IoService] Try running 'cmd.exe /C RD /S /Q' to delete '{0}'", path);
+
+            // call a commandline delete as fallback
+            var executor = new ProcessExecutor();
+            var processInfo = new ProcessInfo("cmd.exe",
+                string.Concat("/C RD /S /Q ", StringUtil.AutoDoubleQuoteString(path)));
+
+            var pr = executor.Execute(processInfo);
+            if (pr.Failed)
+                throw new CruiseControlException(string.Format("Unable to delete path '{0}'.", path));
+        }
+
+        /// <summary>
+        /// Sets directory and file attributes to "normal" recursive
+        /// </summary>
+        /// <param name="path">Root path to start from.</param>
+        static void SetReadOnlyRecursive(DirectoryInfo path)
+        {
+
+            foreach (var dirInfo in path.GetDirectories())
+            {
+                try
+                {
+                    dirInfo.Attributes = FileAttributes.Normal;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("[IoService] Unable to remove read-only attribute from '{0}'.", dirInfo.FullName);
+                    Log.Error(ex);
+                    throw;
+                }
+
+                SetReadOnlyRecursive(dirInfo);
+            }
+
+            foreach (var fileInfo in path.GetFiles())
+            {
+                try
+                {
+                    fileInfo.Attributes = FileAttributes.Normal;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("[IoService] Unable to remove read-only attribute from '{0}'.", fileInfo.FullName);
+                    Log.Error(ex);
+                    throw;
+                }
+            }
+        }
+    }
 }
