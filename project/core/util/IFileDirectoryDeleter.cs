a66b6584fd291d2b495ac5482370f7cc9145e3f1
Daniel Nauck <d.nauck@nauck-it.de>
2010-04-13 11:46:20 +0000

    Backport from trunk r7144
    CCNET-1861: IOService do not support deleting paths longer then 260 chars
    
     * use DictionaryInfo.Delete(true)
     * remove recursively ReadOnly file and dictionary attributes
     * catch PathTooLongException and use a fall back method to delete those folders/files on windows: run 'cmd.exe /C RD /S /Q' to delete the path

--- project/core/util/IFileDirectoryDeleter.cs
+++ project/core/util/IFileDirectoryDeleter.cs
@@ -3,11 +3,11 @@ namespace ThoughtWorks.CruiseControl.Core.Util
 	public interface IFileDirectoryDeleter
 	{
 		/// <summary>
-		/// Deletes any file or directory called filename.
-		/// If filename is a directory, deletes recursively
+        /// Deletes any file or directory called path.
+        /// If path is a directory, deletes recursively
 		/// All readonly objects encountered are also deleted
 		/// </summary>
-		/// <param name="filename"></param>
-		void DeleteIncludingReadOnlyObjects(string filename);
+        /// <param name="path"></param>
+        void DeleteIncludingReadOnlyObjects(string path);
 	}
 }
