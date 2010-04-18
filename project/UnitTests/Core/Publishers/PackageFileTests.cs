87d0c5866e169054b6f900225939c41027bb5f15
Craig Sutherland <craig.j.sutherland@gmail.com>
2010-04-18 21:36:36 +0000

    Refactored PackageFile to make it more testable. Added a test for handling the working directory name being longer than the file name.

---
+++ project/UnitTests/Core/Publishers/PackageFileTests.cs
@@ -0,0 +1,44 @@
+﻿namespace ThoughtWorks.CruiseControl.UnitTests.Core.Publishers
+{
+    using ICSharpCode.SharpZipLib.Zip;
+    using NUnit.Framework;
+    using Rhino.Mocks;
+    using ThoughtWorks.CruiseControl.Core;
+    using ThoughtWorks.CruiseControl.Core.Publishers;
+    using ThoughtWorks.CruiseControl.Core.Util;
+    using System.IO;
+    using System.Text;
+
+    [TestFixture]
+    public class PackageFileTests
+    {
+        private MockRepository mocks;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.mocks = new MockRepository();
+        }
+
+        [Test]
+        public void HandlesShortDirectoryName()
+        {
+            var testFile = @"C:\Somewhere.txt";
+            var result = mocks.StrictMock<IIntegrationResult>();
+            SetupResult.For(result.WorkingDirectory).Return(@"C:\OnceUponATime\InALandFarFarWayFromHere\ThereLivedABeautifulPrincess");
+            var fileSystem = mocks.StrictMock<IFileSystem>();
+            Expect.Call(fileSystem.FileExists(testFile)).Return(true);
+            var inputStream = new MemoryStream(Encoding.UTF8.GetBytes("This is some test data"));
+            Expect.Call(fileSystem.GetFileLength(testFile)).Return(inputStream.Length);
+            Expect.Call(fileSystem.OpenInputStream(testFile)).Return(inputStream);
+
+            var file = new PackageFile(testFile);
+            file.FileSystem = fileSystem;
+            var outputStream = mocks.DynamicMock<ZipOutputStream>();
+
+            mocks.ReplayAll();
+            file.Package(result, outputStream);
+            mocks.VerifyAll();
+        }
+    }
+}
