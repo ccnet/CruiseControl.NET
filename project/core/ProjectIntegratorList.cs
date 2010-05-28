fd2abaabfcf9dcc2fead1cecccf9bc01117b2229
Daniel Nauck <d.nauck@nauck-it.de>
2010-05-28 09:49:24 +0000

    use generic dictionary and avoid casts

--- project/core/ProjectIntegratorList.cs
+++ project/core/ProjectIntegratorList.cs
@@ -1,11 +1,12 @@
 
 using System.Collections;
+using System.Collections.Generic;
 
 namespace ThoughtWorks.CruiseControl.Core
 {
 	public class ProjectIntegratorList : IProjectIntegratorList
 	{
-		private Hashtable integrators = new Hashtable();
+	    private readonly Dictionary<string, IProjectIntegrator> integrators = new Dictionary<string, IProjectIntegrator>();
 
 		public void Add(IProjectIntegrator integrator)
 		{
@@ -19,7 +20,7 @@ namespace ThoughtWorks.CruiseControl.Core
 
 		public IProjectIntegrator this[string projectName]
 		{
-			get { return integrators[projectName] as IProjectIntegrator; }
+			get { return integrators[projectName]; }
 		}
 
 		public int Count
