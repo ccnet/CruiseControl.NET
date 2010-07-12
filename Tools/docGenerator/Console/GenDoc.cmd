ab3e1b9c81ce3025a2a77b51001295a774bbf35f
Daniel Nauck <d.nauck@nauck-it.de>
2010-07-12 11:07:38 +0000

    fix path

--- Tools/docGenerator/Console/GenDoc.cmd
+++ Tools/docGenerator/Console/GenDoc.cmd
@@ -1,5 +1,5 @@
 ﻿@echo off
-Console.exe -c=generate -s=..\..\..\..\..\project\Remote\bin\Debug\ThoughtWorks.CruiseControl.Remote.dll -o=documentation -xsd
-Console.exe -c=generate -s=..\..\..\..\..\project\core\bin\Debug\ThoughtWorks.CruiseControl.Core.dll -o=documentation -xsd
-Console.exe -c=generate -s=..\..\..\..\..\project\WebDashboard\bin\ThoughtWorks.CruiseControl.WebDashboard.dll -o=documentation -xsd
+Console.exe -c=generate -s=..\..\..\..\..\Build\Remote\ThoughtWorks.CruiseControl.Remote.dll -o=documentation -xsd
+Console.exe -c=generate -s=..\..\..\..\..\Build\Core\ThoughtWorks.CruiseControl.Core.dll -o=documentation -xsd
+Console.exe -c=generate -s=..\..\..\..\..\Build\WebDashboard\ThoughtWorks.CruiseControl.WebDashboard.dll -o=documentation -xsd
 pause
