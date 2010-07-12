eaf8b1f7ece3b5762441238745dc8dcf599b6d07
Daniel Nauck <d.nauck@nauck-it.de>
2010-07-12 09:20:31 +0000

    Implement first unit test

--- project/UnitTests/Core/Tasks/FakeTaskTest.cs
+++ project/UnitTests/Core/Tasks/FakeTaskTest.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Exortech.NetReflector;
 using NUnit.Framework;
 using Rhino.Mocks;
+using ThoughtWorks.CruiseControl.Core.Tasks;
 
 namespace ThoughtWorks.CruiseControl.UnitTests.Core.Tasks
 {
@@ -16,6 +18,25 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Tasks
         public void Setup()
         {
             mocks = new MockRepository();
+        }
+
+        [Test]
+        public void PopulateFromReflector()
+        {
+            var task = new FakeTask();
+            const string xml = @"
+    <fake>
+    	<executable>C:\FAKE.exe</executable>
+    	<baseDirectory>C:\</baseDirectory>
+		<buildFile>mybuild.fx</buildFile>
+		<description>Test description</description>
+    </fake>";
+
+            NetReflector.Read(xml, task);
+            Assert.AreEqual(@"C:\FAKE.exe", task.Executable);
+            Assert.AreEqual(@"C:\", task.ConfiguredBaseDirectory);
+            Assert.AreEqual("mybuild.fx", task.BuildFile);
+            Assert.AreEqual("Test description", task.Description);
         }
     }
 }
