14879af13a7c6750ecc0af680300fe43c17d3a78
Ruben Willems <ruben.willems@gmail.com>
2010-07-14 20:41:39 +0000

    CCNET-1506 : fixedtime trigger

---
+++ project/UnitTests/Core/Triggers/CronTriggerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using ThoughtWorks.CruiseControl.Core.Triggers;
+
+
+namespace ThoughtWorks.CruiseControl.UnitTests.Core.Triggers
+{
+    [TestFixture]
+    public class CronTriggerTest
+    {
+        [Test]
+        public void TestX()
+        {
+            var c = new CronTrigger();
+
+            c.CronExpression = "* * 1 1 *"; // first januari of each year
+
+            c.Fire();
+
+            DateTime expected;
+            if (DateTime.Now.DayOfYear == 1)
+            {
+                expected = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            else
+            {
+                expected = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            }
+
+            Assert.AreEqual(expected, c.NextBuild);
+
+        }
+    }
+}
