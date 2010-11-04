061ca47516327c982b4850e1d88b60c6aeef7d2b
Ruben Willems <ruben.willems@gmail.com>
2010-11-04 13:43:02 +0000

    CCNET-1053 : Exception when Vault workingDirectory doesn't exist during check for modifications CCNET-1960 : Directory does not exist: exception with vault clean build CCNET-1961 : it cleans project working folder not the subfolder specified in vault sourcecontrol block if working folder does not exist, exception was thrown attempt 2

--- project/core/sourcecontrol/Vault317.cs
+++ project/core/sourcecontrol/Vault317.cs
@@ -106,7 +106,7 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
 
                 Log.Debug("Cleaning out source folder: " + cleanCopyWorkingFolder);
                 fileDirectoryDeleter.DeleteIncludingReadOnlyObjects(cleanCopyWorkingFolder);
-                
+
                 System.IO.Directory.CreateDirectory(cleanCopyWorkingFolder);
             }
 
@@ -206,6 +206,23 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
                 _lastTxID = long.Parse(txIdAttr.Value);
                 Log.Debug("Most recent TxID: " + _lastTxID);
             }
+
+
+
+            //hack : attempt to fix CCNET-1053
+            string realWorkingFolder = null;
+            if (string.IsNullOrEmpty(_shim.WorkingDirectory))
+            {
+                realWorkingFolder = GetVaultWorkingFolder(from);
+                if (string.IsNullOrEmpty(realWorkingFolder))
+                    throw new VaultException(
+                        string.Format("Vault user {0} has no working folder set for {1} in repository {2} and no working directory has been specified.",
+                                      _shim.Username, _shim.Folder, _shim.Repository));
+            }
+            else
+                realWorkingFolder = from.BaseFromWorkingDirectory(_shim.WorkingDirectory);
+
+            System.IO.Directory.CreateDirectory(realWorkingFolder);
 
             return bFoundChanges;
         }
