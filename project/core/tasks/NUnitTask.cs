f1120b14816db9a4ab767a2d42985299532ad575
Ruben Willems <ruben.willems@gmail.com>
2010-11-07 06:43:55 +0000

    CCNET-1985 : Sometimes nunit-agent.exe processes remain running after using <nunit> task patch by Jozef Izso reworked a bit so a mono implementation is easier

--- project/core/tasks/NUnitTask.cs
+++ project/core/tasks/NUnitTask.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace ThoughtWorks.CruiseControl.Core.Tasks
 {
     using System.Diagnostics;
@@ -45,20 +48,21 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
     /// </code>
     /// </example>
     [ReflectorType("nunit")]
-	public class NUnitTask
+    public class NUnitTask
         : TaskBase
-	{
-		public const string DefaultPath = @"nunit-console";
-		public const int DefaultTimeout = 600;
-		private const string DefaultOutputFile = "nunit-results.xml";
-		private readonly ProcessExecutor executor;
+    {
+        public const string DefaultPath = @"nunit-console";
+        public const int DefaultTimeout = 600;
+        private const string DefaultOutputFile = "nunit-results.xml";
+        private readonly ProcessExecutor executor;
 
-		public NUnitTask() : this(new ProcessExecutor())
-		{}
+        public NUnitTask()
+            : this(new ProcessExecutor())
+        { }
 
-		public NUnitTask(ProcessExecutor exec)
-		{
-			executor = exec;
+        public NUnitTask(ProcessExecutor exec)
+        {
+            executor = exec;
             this.Assemblies = new string[0];
             this.NUnitPath = DefaultPath;
             this.OutputFile = DefaultOutputFile;
@@ -141,36 +145,168 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
         #endregion
 
         protected override bool Execute(IIntegrationResult result)
-		{
-            result.BuildProgressInformation.SignalStartRunTask(!string.IsNullOrEmpty(Description) ? Description : "Executing NUnit"); 
+        {
+            result.BuildProgressInformation.SignalStartRunTask(!string.IsNullOrEmpty(Description) ? Description : "Executing NUnit");
 
-			string outputFile = result.BaseFromArtifactsDirectory(OutputFile);
+            string outputFile = result.BaseFromArtifactsDirectory(OutputFile);
 
-			ProcessResult nunitResult = executor.Execute(NewProcessInfo(outputFile, result));
-		    result.AddTaskResult(new ProcessTaskResult(nunitResult, true));
-			if (File.Exists(outputFile))
-			{
-				result.AddTaskResult(new FileTaskResult(outputFile));				
-			}
-			else
-			{
-				Log.Warning(string.Format("NUnit test output file {0} was not created", outputFile));
-			}
+            ProcessResult nunitResult = executor.Execute(NewProcessInfo(outputFile, result));
+            DeleteNUnitAgentProcesses();
+
+            result.AddTaskResult(new ProcessTaskResult(nunitResult, true));
+            if (File.Exists(outputFile))
+            {
+                result.AddTaskResult(new FileTaskResult(outputFile));
+            }
+            else
+            {
+                Log.Warning(string.Format("NUnit test output file {0} was not created", outputFile));
+            }
             return !nunitResult.Failed;
-		}
+        }
 
-		private ProcessInfo NewProcessInfo(string outputFile, IIntegrationResult result)
-		{
+        private ProcessInfo NewProcessInfo(string outputFile, IIntegrationResult result)
+        {
             NUnitArgument nunitArgument = new NUnitArgument(Assemblies, outputFile);
             nunitArgument.ExcludedCategories = ExcludedCategories;
             nunitArgument.IncludedCategories = IncludedCategories;
             string args = nunitArgument.ToString();
 
-			Log.Debug(string.Format("Running unit tests: {0} {1}", NUnitPath, args));
-	
-			ProcessInfo info = new ProcessInfo(NUnitPath, args, result.WorkingDirectory, Priority);
-			info.TimeOut = Timeout * 1000;
-			return info;
-		}
-	}
+            Log.Debug(string.Format("Running unit tests: {0} {1}", NUnitPath, args));
+
+            ProcessInfo info = new ProcessInfo(NUnitPath, args, result.WorkingDirectory, Priority);
+            info.TimeOut = Timeout * 1000;
+            return info;
+        }
+
+
+        private void DeleteNUnitAgentProcesses()
+        {
+            var processes = Process.GetProcessesByName("nunit-agent");
+            foreach (var process in processes)
+            {
+                if (!HasParentProcess(process.Id))
+                {
+                    Log.Warning("Detected leaked nunit-agent.exe process with Id={0}.", process.Id);
+                    try
+                    {
+                        process.Kill();
+                        process.WaitForExit(2000);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e);
+                    }
+                }
+            }
+        }
+
+        private static bool HasParentProcess(int pid)
+        {
+            ExecutionEnvironment ee = new ExecutionEnvironment();
+            if (ee.IsRunningOnWindows)
+                return HasParentProcessWindows(pid);
+
+            return HasParentProcessNonWindows(pid);
+        }
+
+
+        private static bool HasParentProcessNonWindows(int pid)
+        {
+            //todo make implementation for non windows
+            // there do not appears to be a managed way of getting the parent procesid
+            return false;
+        }
+
+        #region HasParentProcessWindows
+        private static bool HasParentProcessWindows(int pid)
+        {
+            IntPtr handleToSnapshot = IntPtr.Zero;
+            try
+            {
+                PROCESSENTRY32 procEntry = new PROCESSENTRY32();
+                procEntry.dwSize = (UInt32)Marshal.SizeOf(typeof(PROCESSENTRY32));
+                handleToSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.Process, (uint)0);
+                if (Process32First(handleToSnapshot, ref procEntry))
+                {
+                    do
+                    {
+                        if (pid == procEntry.th32ProcessID)
+                        {
+                            try
+                            {
+                                Process parentProc = Process.GetProcessById((int)procEntry.th32ParentProcessID);
+                                return true;
+                            }
+                            catch (ArgumentException)
+                            {
+                                return false;
+                            }
+                        }
+                    } while (Process32Next(handleToSnapshot, ref procEntry));
+                }
+                else
+                {
+                    Log.Error("Failed to load process infomration with error code {0}.", Marshal.GetLastWin32Error());
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Can't get list of processes. Exception: {0}", ex.Message);
+            }
+            finally
+            {
+                // Must clean up the snapshot object!
+                CloseHandle(handleToSnapshot);
+            }
+            return false;
+        }
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
+
+        [DllImport("kernel32", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern bool Process32First([In]IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern bool Process32Next([In]IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle([In] IntPtr hObject);
+
+
+        [Flags]
+        private enum SnapshotFlags : uint
+        {
+            HeapList = 0x00000001,
+            Process = 0x00000002,
+            Thread = 0x00000004,
+            Module = 0x00000008,
+            Module32 = 0x00000010,
+            Inherit = 0x80000000,
+            All = 0x0000001F
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct PROCESSENTRY32
+        {
+            const int MAX_PATH = 260;
+            internal UInt32 dwSize;
+            internal UInt32 cntUsage;
+            internal UInt32 th32ProcessID;
+            internal IntPtr th32DefaultHeapID;
+            internal UInt32 th32ModuleID;
+            internal UInt32 cntThreads;
+            internal UInt32 th32ParentProcessID;
+            internal Int32 pcPriClassBase;
+            internal UInt32 dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH)]
+            internal string szExeFile;
+        }
+
+
+        #endregion
+    }
 }
