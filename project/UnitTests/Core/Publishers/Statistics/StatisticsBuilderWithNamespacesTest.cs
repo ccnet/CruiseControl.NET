71dc9f0069d5947ef496121fb26cbb1a4514c725
rubenwillems <ruben.willems@gmail.com>
2011-09-10 17:54:51 +0200

    fix reflector checks

--- project/UnitTests/Core/Publishers/Statistics/StatisticsBuilderWithNamespacesTest.cs
+++ project/UnitTests/Core/Publishers/Statistics/StatisticsBuilderWithNamespacesTest.cs
@@ -24,43 +24,57 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Publishers.Statistics
 
             StatisticsBuilder builder = new StatisticsBuilder();
 
-            var msb = new FirstMatch();
-            msb.Name = "build";
-            msb.Xpath = @"//build/@buildtime";
-            builder.Add(msb);
-
-
-            FirstMatch info = new FirstMatch();
-            info.Name = "MS-Test";
-            info.Xpath = @"//mstest:TestRun/@name";
-            info.NameSpaces = new StatisticsNamespaceMapping[1];
-            info.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+  
+            Statistic info = new Statistic();
+            info.Name = "Statistic TestsTotalCount no ns";
+            info.Xpath = @"//TestRun/ResultSummary/Counters/@total";
             builder.Add(info);
 
+
             FirstMatch info2 = new FirstMatch();
-            info2.Name = "TestsTotalCount";
-            info2.Xpath = @"//mstest:TestRun/mstest:ResultSummary/mstest:Counters/@total";
-            info2.NameSpaces = new StatisticsNamespaceMapping[1];           
-            info2.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+            info2.Name = "FirstMatch TestsTotalCount no ns";
+            info2.Xpath = @"//TestRun/ResultSummary/Counters/@total";
             builder.Add(info2);
 
-
-            // seems impossible in xsl 1.0
-            Statistic info3 = new Statistic();
-            info3.Name = "TestsExecutedCount";
-            info3.Xpath = @"/xmlns:cruisecontrol/mstest:TestRun/mstest:ResultSummary/mstest:Counters/@executed";
-            info3.NameSpaces = new StatisticsNamespaceMapping[1];
-            info3.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
             
+            FirstMatch info1 = new FirstMatch();
+            info1.Name = "FirstMatch TestsTotalCount with ns";
+            info1.Xpath = @"//mstest:TestRun/mstest:ResultSummary/mstest:Counters/@total";
+            info1.NameSpaces = new StatisticsNamespaceMapping[1];           
+            info1.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+            builder.Add(info1);
+
+
+
+            Statistic info3 = new Statistic();
+            info3.Name = "Statistic sum(TestsTotalCount) no ns";
+            info3.Xpath = @"sum(//TestRun/ResultSummary/Counters/@total)";
             builder.Add(info3);
 
             Statistic info4 = new Statistic();
-            info4.Name = "TestsExecutedCount2";
-            info4.Xpath = @"/cruisecontrol/mstest:TestRun/mstest:ResultSummary/mstest:Counters/@executed";
+            info4.Name = "Statistic sum(TestsTotalCount) with ns";
+            info4.Xpath = @"sum(//mstest:TestRun/mstest:ResultSummary/mstest:Counters/@total)";
             info4.NameSpaces = new StatisticsNamespaceMapping[1];
             info4.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
-
             builder.Add(info4);
+
+
+            // seems impossible in xsl 1.0
+            Statistic imp01 = new Statistic();
+            imp01.Name = "impossible : Statistic TestsExecutedCount xmlns";
+            imp01.Xpath = @"/xmlns:cruisecontrol/mstest:TestRun/mstest:ResultSummary/mstest:Counters/@executed";
+            imp01.NameSpaces = new StatisticsNamespaceMapping[1];
+            imp01.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+            
+            builder.Add(imp01);
+
+            Statistic imp02 = new Statistic();
+            imp02.Name = "impossible : Statistic TestsExecutedCount2 not namespace";
+            imp02.Xpath = @"/cruisecontrol/mstest:TestRun/mstest:ResultSummary/mstest:Counters/@executed";
+            imp02.NameSpaces = new StatisticsNamespaceMapping[1];
+            imp02.NameSpaces[0] = new StatisticsNamespaceMapping("mstest", @"http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+
+            builder.Add(imp02);
 
             
 
@@ -69,7 +83,7 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Publishers.Statistics
 
             foreach (var x in results)
             {
-                Console.WriteLine("Statistic {0} : {1}", x.StatName, x.Value);
+                Console.WriteLine("Result {0} : {1}", x.StatName, x.Value);
             }
             
         
