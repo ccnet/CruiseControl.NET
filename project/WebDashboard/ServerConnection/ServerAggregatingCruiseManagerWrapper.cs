5fc2814f777996457b81ff0764df74f0cea0b1bb
Jaap de Haan <jaap.dehaan@color-of-code.de>
2011-12-30 13:28:06 +0100

    Fix: Nasty cross session bug: one session could view the project list with the rights of another user
    -> The SessionToken of the last freshly logged in user that made a round-trip to the server is used for filtering the project list.
    -> A few commands (GetServerVersion, GetRSSFeed) now use the required SessionToken.
    
    Not sure if this is sufficient to completely solve the issue. There might be an race issue left due to client caching, but I don't know if requests can be handled in parallel for several sessions. If yes, then caching shall be disabled or the SessionToken property removed from the client.

--- project/WebDashboard/ServerConnection/ServerAggregatingCruiseManagerWrapper.cs
+++ project/WebDashboard/ServerConnection/ServerAggregatingCruiseManagerWrapper.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using ThoughtWorks.CruiseControl.Core;
@@ -201,9 +201,9 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.ServerConnection
                 .StopProject(projectSpecifier.ProjectName);
 		}
 
-		public string GetServerVersion(IServerSpecifier serverSpecifier)
+		public string GetServerVersion(IServerSpecifier serverSpecifier, string sessionToken)
 		{
-            var response = GetCruiseManager(serverSpecifier, null)
+            var response = GetCruiseManager(serverSpecifier, sessionToken)
                 .GetServerVersion();
             return response;
 		}
@@ -257,9 +257,9 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.ServerConnection
             return response;
         }
 
-        public string GetRSSFeed(IProjectSpecifier projectSpecifier)
+        public string GetRSSFeed(IProjectSpecifier projectSpecifier, string sessionToken)
         {
-            var response = GetCruiseManager(projectSpecifier, null)
+            var response = GetCruiseManager(projectSpecifier, sessionToken)
                 .GetRSSFeed(projectSpecifier.ProjectName);
             return response;
         }
@@ -282,7 +282,7 @@ namespace ThoughtWorks.CruiseControl.WebDashboard.ServerConnection
                 {
                     BackwardsCompatable = config.BackwardCompatible
                 });
-            if (!string.IsNullOrEmpty(sessionToken)) manager.SessionToken = sessionToken;
+            manager.SessionToken = sessionToken;
             return manager;
 		}
 
