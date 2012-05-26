71f853c9657abae5c88ebd48415e86370862867d
rubenwillems <ruben.willems@gmail.com>
2012-05-26 07:26:34 +0200

    allow others to adjust prefix of default labeler (Feature #133)

--- project/core/label/DefaultLabeller.cs
+++ project/core/label/DefaultLabeller.cs
@@ -47,6 +47,8 @@ namespace ThoughtWorks.CruiseControl.Core.Label
             this.InitialBuildLabel = INITIAL_LABEL;
             this.IncrementOnFailed = false;
             this.LabelFormat = "0";
+            this.LabelPrefixFile = string.Empty;
+            this.LabelPrefixsFileSearchPattern = string.Empty;
         }
         #endregion
 
@@ -92,6 +94,31 @@ namespace ThoughtWorks.CruiseControl.Core.Label
         [ReflectorProperty("labelFormat", Required = false)]
         public string LabelFormat { get; set; }
 
+
+        /// <summary>
+        /// The location to a file which contains the prefix to be used.
+        /// If this is filled in, the LabelPrefix setting is ignored, and the value from the file is used instead.
+        /// This allows other people to set the prefix, without them needing to adjust CCNet.config, 
+        /// or bother a person who has access. Tip : place this file in a separate folder that is shared, 
+        /// so that others do not have direct access to the build server.
+        /// </summary>
+        /// <version>1.7</version>
+        /// <default>None</default>
+        [ReflectorProperty("labelPrefixFile", Required = false)]
+        public string LabelPrefixFile { get; set; }
+
+
+        /// <summary>
+        /// A reg-ex expression to enforce the data of the contents in LabelPrefixFile. The first match is taken.
+        /// This allows a bit more userfriendlyness. (skip whitespace, extra lines and so)
+        /// example : \d+\.\d+\.\d+\.
+        /// This will enforce a layout of 3 numbers separated by a period
+        /// ideal for a version prefix
+        /// </summary>
+        [ReflectorProperty("labelPrefixFileSearchPattern", Required = false)]
+        public string LabelPrefixsFileSearchPattern { get; set; }
+
+
         /// <summary>
         /// Generates the specified integration result.	
         /// </summary>
@@ -100,10 +127,16 @@ namespace ThoughtWorks.CruiseControl.Core.Label
         /// <remarks></remarks>
         public override string Generate(IIntegrationResult integrationResult)
         {
+            if (!string.IsNullOrEmpty(LabelPrefixFile))
+            {
+                ThoughtWorks.CruiseControl.Core.Util.Log.Debug("Reading prefix from file : " + LabelPrefixFile);
+                LabelPrefix = GetPrefixFromFile();
+            }
+
             IntegrationSummary lastIntegration = integrationResult.LastIntegration;
             if (integrationResult == null || lastIntegration.IsInitial())
             {
-				return LabelPrefix + InitialBuildLabel.ToString(LabelFormat, CultureInfo.CurrentCulture) + LabelPostfix;
+                return LabelPrefix + InitialBuildLabel.ToString(LabelFormat, CultureInfo.CurrentCulture) + LabelPostfix;
             }
             else if (ShouldIncrementLabel(lastIntegration))
             {
@@ -114,6 +147,59 @@ namespace ThoughtWorks.CruiseControl.Core.Label
                 return integrationResult.LastIntegration.Label;
             }
         }
+
+
+        /// <summary>
+        /// Read the contents from the prefix file, using the LabelPrefixsFileSearchPattern if specififed
+        /// </summary>
+        /// <returns></returns>
+        private string GetPrefixFromFile()
+        {
+            string errorMessage;
+            string prefix = null;
+
+            if (!System.IO.File.Exists(LabelPrefixFile))
+            {
+                errorMessage = "File " + LabelPrefixFile + " does not exist";
+                throw new ThoughtWorks.CruiseControl.Core.Config.ConfigurationException(errorMessage);
+            }
+
+            var fr = new System.IO.StreamReader(LabelPrefixFile);
+            string line;
+
+            while (!fr.EndOfStream)
+            {
+                line = fr.ReadLine();
+
+                if (!string.IsNullOrEmpty(LabelPrefixsFileSearchPattern))
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(line, LabelPrefixsFileSearchPattern))
+                    {
+                        var m = System.Text.RegularExpressions.Regex.Match(line, LabelPrefixsFileSearchPattern);
+                        prefix = m.Value;
+                        continue;
+                    }
+                }
+                else
+                {
+                    prefix = line;
+                }
+            }
+
+            fr.Close();
+            fr.Dispose();
+
+            if (string.IsNullOrEmpty(prefix))
+            {
+                errorMessage = "No valid prefix data found in file : " + LabelPrefixFile;
+                throw new ThoughtWorks.CruiseControl.Core.Config.ConfigurationException(errorMessage);
+            }
+
+            ThoughtWorks.CruiseControl.Core.Util.Log.Debug("Read prefix {0} from file : {1}",  prefix, LabelPrefixFile);
+            return prefix;
+        }
+
+
 
         private bool ShouldIncrementLabel(IntegrationSummary previousResult)
         {
