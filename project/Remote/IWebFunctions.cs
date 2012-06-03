e520f2b2bd72ba8b15ccd22e45b5b46d340504b6
Adam Wendt <adam@acehostingllc.com>
2012-06-03 02:50:17 -0700

    Minor documentation fix

--- project/Remote/IWebFunctions.cs
+++ project/Remote/IWebFunctions.cs
@@ -8,8 +8,7 @@ namespace ThoughtWorks.CruiseControl.Remote
         /// <summary>
         /// Sets credentials on client if address contains user info.
         /// </summary>
-        /// <param name="webFunctions"> </param>
-        /// <param name="webClient"> </param>
+        /// <param name="webClient">The <see cref="WebClient"/> to set credentials on.</param>
         /// <param name="address">The address to check for user info.</param>
         /// <param name="forceAuthorization">Whether to force an Authorization header or allow WebClient credentials to handle it.</param>
         void SetCredentials(WebClient webClient, Uri address, bool forceAuthorization);
