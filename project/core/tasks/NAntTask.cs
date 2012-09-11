3b3ca4a1b81c02de9948ff83ad33536443b208da
Matthew Vukomanovic <matthew.vukomanovic@server>
2012-09-11 16:27:56 +0800

    Change integration properties to use correct encoding (Bug #191)
    Done for all the places I could find that would need it:
    FakeTasK;
    FinalBuilderTask;
    MsBuildTask;
    NAntTask;

--- project/core/tasks/NAntTask.cs
+++ project/core/tasks/NAntTask.cs
@@ -395,7 +395,7 @@ namespace ThoughtWorks.CruiseControl.Core.Tasks
 			{
 				object value = result.IntegrationProperties[key];
 				if (value != null)
-					buffer.AppendArgument(string.Format(System.Globalization.CultureInfo.CurrentCulture,"-D:{0}={1}", key, StringUtil.AutoDoubleQuoteString(StringUtil.RemoveTrailingPathDelimeter(StringUtil.IntegrationPropertyToString(value)))));
+					buffer.AppendArgument(string.Format(System.Globalization.CultureInfo.CurrentCulture,"-D:{0}={1}", key, StringUtil.StripThenEncodeParameterArgument(StringUtil.RemoveTrailingPathDelimeter(StringUtil.IntegrationPropertyToString(value)))));
 			}
 		}
 
