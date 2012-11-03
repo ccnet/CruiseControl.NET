d2d7f6c3a41a20e4b9d5365f7b128acaee04e3d2
RubenWillems <ruben.willems@gmail.com>
2012-11-03 13:06:20 +0100

    CCTray Exec: Spaces in path not supported, error: Unable to execute command (Bug #222) patch by Ron Vander Wal

--- project/CCTrayLib/Presentation/BuildTransitionExecRunner.cs
+++ project/CCTrayLib/Presentation/BuildTransitionExecRunner.cs
@@ -8,83 +8,96 @@ using ThoughtWorks.CruiseControl.CCTrayLib.Monitoring;
 
 namespace ThoughtWorks.CruiseControl.CCTrayLib.Presentation
 {
-	public class BuildTransitionExecRunner
-	{
-		private IProjectMonitor monitor;
-		private ExecCommands configuration;
-		private ProjectState status = null;
+    public class BuildTransitionExecRunner
+    {
+        private IProjectMonitor monitor;
+        private ExecCommands configuration;
+        private ProjectState status = null;
 
-		public BuildTransitionExecRunner(IProjectMonitor monitor, ExecCommands configuration)
-		{
-			this.monitor = monitor;
-			this.configuration = configuration;
+        public BuildTransitionExecRunner(IProjectMonitor monitor, ExecCommands configuration)
+        {
+            this.monitor = monitor;
+            this.configuration = configuration;
 
-			monitor.Polled += Monitor_Polled;
-		}
+            monitor.Polled += Monitor_Polled;
+        }
 
-		private void Monitor_Polled(object sender, MonitorPolledEventArgs args)
-		{
-			Status = monitor.ProjectState;
-		}
+        private void Monitor_Polled(object sender, MonitorPolledEventArgs args)
+        {
+            Status = monitor.ProjectState;
+        }
 
-		private ProjectState Status
-		{
-			set
-			{
-				if (status != value)
-				{
-					if (value == ProjectState.Broken)
-					{
-						Exec(configuration.BrokenCommand);
-					}
-					else if (value == ProjectState.BrokenAndBuilding)
-					{
-						Exec(configuration.BrokenAndBuildingCommand);
-					}
-					else if (value == ProjectState.Building)
-					{
-						Exec(configuration.BuildingCommand);
-					}
-					else if (value == ProjectState.NotConnected)
-					{
-						Exec(configuration.NotConnectedCommand);
-					}
-					else if (value == ProjectState.Success)
-					{
-						Exec(configuration.SuccessCommand);
-					}
-				}
-				status = value;
-			}
-		}
+        private ProjectState Status
+        {
+            set
+            {
+                if (status != value)
+                {
+                    if (value == ProjectState.Broken)
+                    {
+                        Exec(configuration.BrokenCommand);
+                    }
+                    else if (value == ProjectState.BrokenAndBuilding)
+                    {
+                        Exec(configuration.BrokenAndBuildingCommand);
+                    }
+                    else if (value == ProjectState.Building)
+                    {
+                        Exec(configuration.BuildingCommand);
+                    }
+                    else if (value == ProjectState.NotConnected)
+                    {
+                        Exec(configuration.NotConnectedCommand);
+                    }
+                    else if (value == ProjectState.Success)
+                    {
+                        Exec(configuration.SuccessCommand);
+                    }
+                }
+                status = value;
+            }
+        }
 
-		private static void Exec(string command)
-		{
-			if (!string.IsNullOrEmpty(command))
-			{
-				try
-				{
-					ProcessStartInfo lProc = new ProcessStartInfo();
-					lProc.UseShellExecute = false;
-					lProc.CreateNoWindow = true;
+        private static void Exec(string command)
+        {
+            if (!string.IsNullOrEmpty(command))
+            {
+                string fileName = string.Empty;
+                string arguments = string.Empty;
+                try
+                {
+                    ProcessStartInfo lProc = new ProcessStartInfo();
+                    lProc.UseShellExecute = false;
+                    lProc.CreateNoWindow = true;
+                    if (command.StartsWith("\""))
+                    {
+                        command = command.TrimStart(new[] { '\"' });
+                        fileName = command.Substring(0, command.IndexOf('\"'));
+                        arguments = command.Substring(command.IndexOf('\"')).Trim();
+                    }
 
-					if (command.Contains(" "))
-					{
-						lProc.FileName = command.Substring(0, command.IndexOf(' '));
-						lProc.Arguments = command.Substring(command.IndexOf(' '));
-					}
-					else
-					{
-						lProc.FileName = command;
-					}
-					Process.Start(lProc);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e);
-					MessageBox.Show("Unable to execute command \"" + command + "\": " + e);
-				}
-			}
-		}
-	}
+                    else if (command.Contains(" "))
+                    {
+                        fileName = command.Substring(0, command.IndexOf(' '));
+                        arguments = command.Substring(command.IndexOf(' '));
+                    }
+                    else
+                    {
+                        fileName = command;
+                    }
+                    lProc.FileName = fileName;
+                    lProc.Arguments = arguments;
+                    Process.Start(lProc);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    string message = string.Format(
+                                    "Unable to execute command \"{0}\" FileName: \"{1}\" Arguments: \"{2}\" {3}",
+                                    command, fileName, arguments, e);
+                    MessageBox.Show(message);
+                }
+            }
+        }
+    }
 }
