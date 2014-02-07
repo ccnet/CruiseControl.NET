a1456c90e1abec7faf4960053e5ef4796bacab88
Alexander Skrinnik <askrinnik@metratech.com>
2014-02-07 09:17:50 -0500

    Git Source control section has been extended with a new configuration parameter cleanUntrackedFiles. The purpose of this parameter is to avoid invocation of the command "git clean -d -f -x" during continue integration builds. You may want to use this trick for incremental builds when you don't want to delete compiled files after each sequential project. Default value is true. It corresponds to default behavior

--- project/core/sourcecontrol/Git.cs
+++ project/core/sourcecontrol/Git.cs
@@ -139,6 +139,14 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
         public bool AutoGetSource { get; set; }
 
         /// <summary>
+        /// Remove untracked files from the working tree
+        /// </summary>
+        /// <version>1.5</version>
+        /// <default>true</default>
+        [ReflectorProperty("cleanUntrackedFiles", Required = false)]
+        public bool CleanUntrackedFiles { get; set; }
+
+        /// <summary>
         /// The location of the Git executable. 
         /// </summary>
         /// <version>1.5</version>
@@ -267,6 +275,7 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
             _fileSystem = fileSystem;
             _fileDirectoryDeleter = fileDirectoryDeleter;
             this.AutoGetSource = true;
+            this.CleanUntrackedFiles = true;
             this.Executable = "git";
             this.Branch = "master";
             this.TagCommitMessage = "CCNet Build {0}";
@@ -327,7 +336,8 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
                 GitUpdateSubmodules(result);
 
             // clean up the local working copy
-            GitClean(result);
+      if (CleanUntrackedFiles)
+			  GitClean(result);
         }
 
         /// <summary>
