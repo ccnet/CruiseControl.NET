e0dc3cb5c0f772c0576d57439921dadadd5e4ac0
PabloJHM <pablohmarabo@hotmail.es>
2016-04-05 09:33:27 +0200

    Create DataGridRow.cs
    Object which contains data related to the builds

---
+++ project/WebDashboard/Dashboard/DataGridRow.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Drawing;
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using ThoughtWorks.CruiseControl.WebDashboard.Resources;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+
+    public class DataGridRow
+    {
+        private readonly string buildStatus;
+        private readonly string date;
+        private readonly string runningTime;
+        private readonly string link;
+
+        public DataGridRow(string buildStatus, string date, string runningTime, string link)
+        {
+            this.buildStatus = buildStatus;
+            this.date = date;
+            this.runningTime = runningTime;
+            this.link = link;
+        }
+
+        public string BuildStatus
+        {
+            get { return buildStatus; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+        }
+
+        public string RunningTime
+        {
+            get { return runningTime; }
+        }
+
+        public string Link
+        {
+            get { return link; }
+        }
+    }
+}
