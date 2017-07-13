0ff87064c469429f1a18bfbf0d1cf0ef4b882033
Ruben Willems <ruben.willems@gmail.com>
2017-07-13 09:45:51 +0200

    add registry keys for Visual Studio 2015 for tfs source control

--- project/core/sourcecontrol/Vsts.cs
+++ project/core/sourcecontrol/Vsts.cs
@@ -33,11 +33,13 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
     public class Vsts : ProcessSourceControl, IConfigurationValidation
     {
         #region Constants
+        private const string VS2015_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\14.0";
         private const string VS2013_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\12.0";
         private const string VS2012_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\11.0";
         private const string VS2010_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\10.0";
         private const string VS2008_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\9.0";
         private const string VS2005_32_REGISTRY_PATH = @"Software\Microsoft\VisualStudio\8.0";
+        private const string VS2015_64_REGISTRY_PATH = @"Software\Wow6432Node\Microsoft\VisualStudio\14.0";
         private const string VS2013_64_REGISTRY_PATH = @"Software\Wow6432Node\Microsoft\VisualStudio\12.0";
         private const string VS2012_64_REGISTRY_PATH = @"Software\Wow6432Node\Microsoft\VisualStudio\11.0";
         private const string VS2010_64_REGISTRY_PATH = @"Software\Wow6432Node\Microsoft\VisualStudio\10.0";
@@ -589,7 +591,13 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
 
         private string ReadTfFromRegistry()
         {
-            string registryValue = registry.GetLocalMachineSubKeyValue(VS2013_64_REGISTRY_PATH, VS_REGISTRY_KEY);
+            string registryValue = registry.GetLocalMachineSubKeyValue(VS2015_64_REGISTRY_PATH, VS_REGISTRY_KEY);
+
+            if (registryValue == null)
+            {
+                registryValue = registry.GetLocalMachineSubKeyValue(VS2013_64_REGISTRY_PATH, VS_REGISTRY_KEY);
+            }
+
 
             if (registryValue == null)
             {
@@ -609,6 +617,11 @@ namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
             if (registryValue == null)
             {
                 registryValue = registry.GetLocalMachineSubKeyValue(VS2005_64_REGISTRY_PATH, VS_REGISTRY_KEY);
+            }
+
+            if (registryValue == null)
+            {
+                registryValue = registry.GetLocalMachineSubKeyValue(VS2015_32_REGISTRY_PATH, VS_REGISTRY_KEY);
             }
 
             if (registryValue == null)
