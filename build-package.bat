b13e7e5e47a775713aa003adbd71e978a2f678f2
jun_lin <jun_lin@mail.gss.com.tw>
2017-08-16 16:28:58 +0800

    git sourcecontrol add SparseCheckout support

--- build-package.bat
+++ build-package.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
-Tools\NAnt\NAnt.exe package -buildfile:ccnet.build -D:CCNetLabel=1.5.0.0 -nologo -logfile:nant-build-package.log.txt %*
+Tools\NAnt\NAnt.exe package -buildfile:ccnet.build -D:CCNetLabel=1.8.5.0 -nologo -logfile:nant-build-package.log.txt %*
 echo %time% %date%
 pause
