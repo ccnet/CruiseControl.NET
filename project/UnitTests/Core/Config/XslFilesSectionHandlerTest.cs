0ef73bee88e98fa05e0590c55a37fcec5673917a
OBones <obones@free.fr>
2021-01-21 15:11:40 +0100

    IConfigurationSectionHandler is deprecated, a section handler should inherit from ConfigurationSection and implement the hiearchy via collections and elements

--- project/UnitTests/Core/Config/XslFilesSectionHandlerTest.cs
+++ project/UnitTests/Core/Config/XslFilesSectionHandlerTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Configuration;
 using NUnit.Framework;
+using ThoughtWorks.CruiseControl.Core.Config;
 
 namespace ThoughtWorks.CruiseControl.UnitTests.Core.Config
 {
@@ -10,8 +11,10 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Config
         [Test]
         public void GetConfig()
         {
-            IList list = (IList) ConfigurationManager.GetSection("xslFiles");
-            Assert.IsNotNull(list);
+            var section = ConfigurationManager.GetSection("xslFiles") as XslFilesSectionHandler;
+            Assert.IsNotNull(section, "section should not be null");
+            IList list = section.FileNames;
+            Assert.IsNotNull(list, "file names list should not be null");
             Assert.AreEqual(5, list.Count);
             Assert.AreEqual(@"xsl\header.xsl", list[0]);
             Assert.AreEqual(@"xsl\compile.xsl", list[1]);
